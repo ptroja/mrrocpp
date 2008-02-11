8ca472d931400f92063c9ad164fb6a5a9d0a2354
Piotr Trojanek <piotr.trojanek@gmail.com>
2008-02-11 14:23:31 +0000

    mp_t_multiplayer fixed
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1031 4c39dba3-d726-0410-8099-c98d187771e6

--- src/mp/mp_t_multiplayer.cc
+++ src/mp/mp_t_multiplayer.cc
@@ -7,17 +7,12 @@
 
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <map>
-
-#include "common/typedefs.h"
-#include "common/impconst.h"
-#include "common/com_buf.h"
 
 #include "lib/srlib.h"
 #include "mp/mp.h"
 #include "mp/mp_t_multiplayer.h"
+#include "ecp_mp/ecp_mp_tr_player.h"
 
 mp_task* return_created_mp_task (void)
 {
@@ -29,35 +24,35 @@ void mp_task_multiplayer::task_initialization(void)
 {
 	// Powolanie czujnikow
 	transmitter_m[TRANSMITTER_PLAYER] = 
-		new ecp_mp_schunk_sensor (SENSOR_FORCE_ON_TRACK, "[vsp_force_irp6ot]", *this);
+		new player_transmitter (TRANSMITTER_PLAYER, "[pcm1]", *this,
+		"localhost", 6665, "position", 3, 'a');
 		
 	sr_ecp_msg->message("MP multiplayer task loaded");
-};
+}
  
 void mp_task_multiplayer::main_task_algorithm(void)
 {
 
 	break_state = false;
 
-  	mp_multiplayer_generator mp_h_gen(*this, 10); 
+	/*
+  	mp_playerpos_generator mp_playerpos_gen(*this, 10); 
    	mp_h_gen.robot_m = robot_m;
    	mp_h_gen.sensor_m[SENSOR_FORCE_ON_TRACK] = sensor_m[SENSOR_FORCE_ON_TRACK];
    	mp_h_gen.sensor_m[SENSOR_FORCE_POSTUMENT] = sensor_m[SENSOR_FORCE_POSTUMENT];
+	*/
 
-	 // printf("przed wait for start \n");
-      // Oczekiwanie na zlecenie START od UI  
+	// Oczekiwanie na zlecenie START od UI  
 	sr_ecp_msg->message("MP multiplayer device - press start");
 	wait_for_start ();
 	// Wyslanie START do wszystkich ECP 
 	start_all (robot_m);
 
-	for (;;)
-	{  // Wewnetrzna petla
+	for (;;) { 
 
-       	// Zlecenie wykonania kolejnego makrokroku
-		// printf("po start all \n");
-		 for(;;)
-		{
+		// Zlecenie wykonania kolejnego makrokroku
+		for(;;) {
+			/*
 			sr_ecp_msg->message("New series");
 			for (std::map <SENSOR_ENUM, sensor*>::iterator sensor_m_iterator = sensor_m.begin();
 				 sensor_m_iterator != sensor_m.end(); sensor_m_iterator++)
@@ -66,29 +61,26 @@ void mp_task_multiplayer::main_task_algorithm(void)
 				sensor_m_iterator->second->configure_sensor();
 			}
 
-
-			// mp_h_gen.sensor_m = sensor_m;
 			mp_h_gen.configure(1, 0);
+
 		    	sr_ecp_msg->message("Track podatny do czasu wcisniecia mp_trigger");
 			if (Move ( mp_h_gen)) {
 		        	break_state = true;
 		        	break;
-	         }
-
-      	}
-	 	
-	 	
-		if (break_state) 
-		{
+			}
+			*/
+		}
+	 		 	
+		if (break_state) {
 			break;
+	
 		}	
 		
-        // Oczekiwanie na STOP od UI
-	  //          printf("w mp przed wait for stop\n");
-        wait_for_stop (MP_THROW);// by Y - wlaczony tryb
-  
-        // Wyslanie STOP do wszystkich ECP po zakonczeniu programu uzytkownika
-        terminate_all (robot_m);
-        break; 
-      } // koniec: for(;;) - wewnetrzna petla
+	        // Oczekiwanie na STOP od UI
+	        wait_for_stop (MP_THROW);// by Y - wlaczony tryb
+	  
+	        // Wyslanie STOP do wszystkich ECP po zakonczeniu programu uzytkownika
+	        terminate_all (robot_m);
+	        break; 
+	} // koniec: for(;;) - wewnetrzna petla
 }
