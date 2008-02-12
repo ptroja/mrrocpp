c3f3e2e0dce06a3bcaef7de449385b7b4bbc0e25
Janusz Nowacki <janusz.nowacki@capgemini.com>
2008-02-12 18:51:13 +0000

    1) Creation date and developer who created it - added to all files 2) Technical documentation section - revised, max. simplified and normalised for all files 3) INDEX-OFF comments deleted - rethink this when using indent 4) New version of Doxygen used - new configuration files
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1036 4c39dba3-d726-0410-8099-c98d187771e6

--- cleanDox.bat
+++ cleanDox.bat
@@ -1,58 +1,36 @@
 @echo off
-
-rem // *INDENT-OFF*
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
+rem ////////////////////////////////////////////////////////////////////////////
 rem //
-rem //                                        TECHNICAL SPECIFICATION
+rem //                              TECHNICAL SPECIFICATION
 rem //
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
+rem ////////////////////////////////////////////////////////////////////////////
 rem // 
 rem // $Id$
 rem //
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
-rem //File              : $HeadURL$
-rem //Revision          : $LastChangedRevision$
-rem //Application       : MRROC++
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
-rem // Creation date    : 2007-01-12
-rem // Last modification: $LastChangedDate$
-rem // Developer        : $LastChangedBy$
-rem //      (elka email): jnowacki@elka.pw.edu.pl
-rem //      (nick)      : jarosz 
-rem //      (real name) : Janusz Nowacki
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
-rem // File modification history:
-rem // 
-rem // Date         User              Description
-rem // ==========   ==============    =======================================================================
-rem // 2007-08-18   janusz.nowacki    1) technical specification added 
-rem // ----------   --------------    -----------------------------------------------------------------------
-rem // 2007-10-03   janusz.nowacki    1) layout changes
-rem //                                2) deletion of Mainpage.dox added, which gets 
-rem //                                   created by SubWCRev
-rem //                                3) SVN keywords added to file properties
-rem // ----------   --------------    -----------------------------------------------------------------------
-rem // 2007-10-04   janusz.nowacki    1) retab done in Vim, TAB key set to 2 spaces
-rem //                                2) more layout changes
-rem //                                  
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
+rem ////////////////////////////////////////////////////////////////////////////
+rem // $URL$
+rem // Revision  : $LastChangedRevision$
+rem // Project   : MRROC++
+rem ////////////////////////////////////////////////////////////////////////////
+rem // Created   : 2007-05-12
+rem // Developer : janusz.nowacki 
+rem // Modified  : $LastChangedDate$
+rem // By        : $LastChangedBy$
+rem ////////////////////////////////////////////////////////////////////////////
 rem // File description:
 rem // 
 rem //  Clean file - deleting (in Windows system) MRROC++ Doxygen documentation.
 rem // 
-rem /////////////////////////////////////////////////////////////////////////////////////////////////////////
-rem // *INDENT-ON*
-
+rem ////////////////////////////////////////////////////////////////////////////
 
 echo Program: %0
 echo Date: %date% 
 echo Time: %time% 
 echo Cleaning the Doxygen documentation for MRROC++ ...
 
-rem ----------------------------------------------------------------
-rem                             Settings
-rem ----------------------------------------------------------------
-
+rem ----------------------------------------------------------------------------
+rem                           Settings
+rem ----------------------------------------------------------------------------
 set CLEAN=cleanDox.bat
 set DOX_DIR=dox
 set TMP=*.tmp
@@ -62,9 +40,9 @@ set LOG=*.log
 set MAINPAGE=Mainpage.dox
 
 
-rem ################################################################
+rem ############################################################################
 echo Cleaning the documentation directories ...
-rem ################################################################
+rem ############################################################################
 
   if not exist %TMP%  (echo No %TMP% found) else (for %%i in (%TMP%) do del %%i) 
   
