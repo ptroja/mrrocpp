baf03f11518a90aae5d9e4de2d079b0c860a78de
Janusz Nowacki <janusz.nowacki@capgemini.com>
2008-02-12 22:00:02 +0000

    1) SVN ID tag removed from Technical Documentation as redundant. 2) Technical Documentation is different for C++ files (.cc, .h) - combined with Doxygen comments (a separate Tech. Doc. was redundant)
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1037 4c39dba3-d726-0410-8099-c98d187771e6

--- dox/PL/cleanDox.bat
+++ dox/PL/cleanDox.bat
@@ -1,15 +1,39 @@
 @echo off
+rem ////////////////////////////////////////////////////////////////////////////
+rem //
+rem //                              TECHNICAL SPECIFICATION
+rem //
+rem ////////////////////////////////////////////////////////////////////////////
+rem // 
+rem // $URL: $
+rem //
+rem ////////////////////////////////////////////////////////////////////////////
+rem // Revision  : $LastChangedRevision: $
+rem // Project   : MRROC++
+rem ////////////////////////////////////////////////////////////////////////////
+rem // Created   : 2007-05-12
+rem // Developer : janusz.nowacki 
+rem // Modified  : $LastChangedDate: $
+rem // By        : $LastChangedBy: $
+rem ////////////////////////////////////////////////////////////////////////////
+rem // File description:
+rem // 
+rem //  Clean file for dox/PL dir 
+rem //  - deleting recursively (in Windows system) MRROC++ Doxygen documentation.
+rem // 
+rem ////////////////////////////////////////////////////////////////////////////
 
-rem ------------------------------------------------------------
-REM cleanDox.bat
-rem ------------------------------------------------------------
+echo Program: %0
+echo Date: %date% 
+echo Time: %time% 
 
 set CLEAN=cleanDox.bat
 
-rem ################################################################
+rem ############################################################################
 echo dox\PL\cleanDox.bat
-rem ################################################################
+rem ############################################################################
 	FOR %%i IN (*.*) DO ( 
 												IF NOT %%i==%CLEAN% (del %%i) 
 											)
 	FOR /D %%i IN (*.*) DO (rmdir /S /Q  %%i) 
+
--- dox/pic/cleanDox.bat
+++ dox/pic/cleanDox.bat
@@ -1,16 +1,39 @@
 @echo off
+rem ////////////////////////////////////////////////////////////////////////////
+rem //
+rem //                              TECHNICAL SPECIFICATION
+rem //
+rem ////////////////////////////////////////////////////////////////////////////
+rem // 
+rem // $URL: $
+rem //
+rem ////////////////////////////////////////////////////////////////////////////
+rem // Revision  : $LastChangedRevision: $
+rem // Project   : MRROC++
+rem ////////////////////////////////////////////////////////////////////////////
+rem // Created   : 2007-05-12
+rem // Developer : janusz.nowacki 
+rem // Modified  : $LastChangedDate: $
+rem // By        : $LastChangedBy: $
+rem ////////////////////////////////////////////////////////////////////////////
+rem // File description:
+rem // 
+rem //  Clean file for dox/pic dir 
+rem //  - deleting recursively (in Windows system) MRROC++ Doxygen documentation.
+rem // 
+rem ////////////////////////////////////////////////////////////////////////////
 
-rem ------------------------------------------------------------
-REM cleanDox.bat
-rem ------------------------------------------------------------
-
+echo Program: %0
+echo Date: %date% 
+echo Time: %time%
+ 
 set TMP=*.tmp
 set SWAP=*~
 set LOG=*.log
 
-rem ################################################################
+rem ############################################################################
 echo dox\pic\cleanDox.bat
-rem ################################################################
+rem ############################################################################
 	if not exist %TMP%  (echo No %TMP% found) 	else (for %%i in (%TMP%) do del %%i) 
 	if not exist %SWAP% (echo No %SWAP%  found) else (for %%i in (%SWAP%) do del %%i)
 	if not exist %LOG%  (echo No %LOG%  found) 	else (for %%i in (%LOG%) do del %%i)
