d84e13b9bdda530cce0fc5377e1880d0a664d7c0
Tomek Winiarski <tomrobotics@gmail.com>
2008-08-08 12:03:58 +0000

    fix'es
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1433 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/conveyor/ecp_g_visconv.cc
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "common/typedefs.h"
+#include "common/impconst.h"
+#include "common/com_buf.h"
+
+#include "lib/srlib.h"
+#include "ecp/conveyor/ecp_local.h"
+#include "ecp/conveyor/ecp_g_legobrick.h"
+
+conveyor_incremental_move::conveyor_incremental_move(ecp_task& _ecp_task, double inc_move):
+		ecp_generator (_ecp_task), move_length(inc_move) {}
+
+bool conveyor_incremental_move::first_step ( )
+{
+	td.interpolation_node_no = 1;
+	td.internode_step_no = 500;//((int)move_length)*20;
+	td.value_in_step_no = td.internode_step_no - 2;
+
+
+			the_robot->EDP_data.instruction_type = GET;
+			the_robot->EDP_data.get_type = ARM_DV; // arm - ORYGINAL
+			the_robot->EDP_data.set_type = ARM_DV;
+
+			the_robot->EDP_data.set_arm_type = JOINT;
+			the_robot->EDP_data.get_arm_type = JOINT;
+			the_robot->EDP_data.motion_type = ABSOLUTE;
+			the_robot->EDP_data.next_interpolation_type = MIM;
+			the_robot->EDP_data.motion_steps = td.internode_step_no;
+			the_robot->EDP_data.value_in_step_no = td.value_in_step_no;
+
+	
+
+	return true;
+}
+
+bool conveyor_incremental_move::next_step ( )
+{
+	//struct timespec start[9];
+	if (check_and_null_trigger()) { // Koniec odcinka
+		return false;
+	} 
+	// Przygotowanie kroku ruchu - do kolejnego wezla interpolacji
+
+	// nie aktualizujemy pozycjio na podstawie odczytu z EDP
+	the_robot->EDP_data.instruction_type = SET;
+	the_robot->EDP_data.get_type = NOTHING_DV;
+	the_robot->EDP_data.get_arm_type = INVALID_END_EFFECTOR;
+
+	
+	// Obliczenie zadanej pozycji posredniej w tym kroku ruchu
+	// (okreslenie kolejnego wezla interpolacji)
+	/*if (((node_counter)%100)==0) {
+		if( clock_gettime( CLOCK_REALTIME , &start[0]) == -1 ) {
+			printf("blad pomiaru czasu");
+		}
+		printf("ECP conv pomiarow: %d,  czas: %ld, pozycja %f\n", node_counter,
+		       start[0].tv_sec%100, the_robot->EDP_data.current_motor_arm_coordinates[0]);
+	}*/
+
+	// Wspolrzedne kartezjanskie XYZ i katy Eulera ZYZ
+	td.coordinate_delta[0] = 0.01;//move_length/td.internode_step_no;//((double)(((node_counter%2)*2)-1))/20;   // przyrost wspolrzednej X
+	//td.coordinate_delta[1] = 0.0;   // przyrost wspolrzednej Y
+	//td.coordinate_delta[2] = 0.0;   // przyrost wspolrzednej Z
+
+	//td.coordinate_delta[3] = 0.0;   // przyrost wspolrzednej FI
+	//td.coordinate_delta[4] = 0.0;   // przyrost wspolrzednej TETA
+	//td.coordinate_delta[5] = 0.0;   // przyrost wspolrzednej PSI
+
+	//for (int i=0; i<6; i++) {
+		// nastepna pozycja
+		the_robot->EDP_data.next_joint_arm_coordinates[0] = //td.coordinate_delta[0];
+		    the_robot->EDP_data.current_joint_arm_coordinates[0]	+ td.coordinate_delta[0];
+	//}
+	
+	return true;
+}
---
+++ src/ecp/conveyor/ecp_t_visconv.cc
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#include "lib/srlib.h"
+#include "ecp/conveyor/ecp_local.h"
+#include "ecp/conveyor/ecp_t_legobrick_conv.h"
+//#include "ecp/conveyor/ecp_g_legobrick.h"
+
+// KONSTRUKTORY
+ecp_task_conveyor_lego_brick::ecp_task_conveyor_lego_brick(configurator &_config) : ecp_task(_config)
+{
+	absolute_position = 0.0;
+}
+
+ecp_task_conveyor_lego_brick::~ecp_task_conveyor_lego_brick()
+{}
+
+// methods for ECP template to redefine in concrete classes
+void ecp_task_conveyor_lego_brick::task_initialization(void)
+{
+	ecp_m_robot = new ecp_conveyor_robot (*this);
+
+	sr_ecp_msg->message("ECP loaded");
+
+}
+
+
+void ecp_task_conveyor_lego_brick::main_task_algorithm(void)
+{
+	sr_ecp_msg->message("ECP lego brick - wcisnij start");
+	ecp_wait_for_start();
+	//conveyor_incremental_move ysg(*this, 100);
+	ecp_smooth_generator gen(*this, true, true);
+	gen.flush_pose_list();
+
+	POSE_SPECIFICATION ps;
+	ps = JOINT;
+
+	double coordinates[MAX_SERVOS_NR];
+	double vp[MAX_SERVOS_NR];
+	double vk[MAX_SERVOS_NR];
+	double v[MAX_SERVOS_NR];
+	double a[MAX_SERVOS_NR];
+
+	absolute_position-= 10;
+	coordinates[0] = absolute_position;
+	vp[0] = 0.0;
+	vk[0] = 0.0;
+	v[0] = 0.04;
+	a[0] = 0.002;
+	for(int i = 1; i < MAX_SERVOS_NR; ++i){
+		vp[i] = 0.0;
+		vk[i] = 0.0;
+		v[i] = 0.0;
+		a[i] = 0.0;
+		coordinates[i] = 0.0;
+	}
+
+	gen.create_pose_list_head(ps, vp, vk, v, a, coordinates);
+	//ysg.sensor_m = sensor_m;
+	
+
+	for(;;) { // Wewnetrzna petla nieskonczona
+
+		for(;;) {
+			sr_ecp_msg->message("Ruch");
+
+			gen.Move();
+		}
+
+		// Oczekiwanie na STOP
+		//printf("przed wait for stop\n");
+		ecp_wait_for_stop ();
+		break;
+	} // koniec: for(;;) wewnetrznej
+}
+
+ecp_task* return_created_ecp_task (configurator &_config)
+{
+	return new ecp_task_conveyor_lego_brick(_config);
+}
