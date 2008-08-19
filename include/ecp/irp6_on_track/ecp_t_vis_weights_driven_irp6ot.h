54f1b78de422fd4231b5b951b8d2bc3c2051c438
Maciej Staniak <seven@op.pl>
2008-08-19 10:10:42 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1448 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ include/ecp/irp6_on_track/ecp_t_vis_weights_driven_irp6ot.h
@@ -0,0 +1,22 @@
+#if !defined(_ECP_T_WEIGHTS_DRIVEN_IRP6OT_H)
+#define _ECP_T_WEIGHTS_DRIVEN_IRP6OT_H
+
+#include "ecp/common/ecp_task.h"
+#include "ecp/irp6_on_track/ecp_vis_pb_eol_sac_irp6ot.h"
+
+class ecp_task_vislx_irp6ot: public ecp_task  {
+
+public:
+	//static std::map <SENSOR_ENUM, generator*> generator_m;
+	ecp_vis_pb_eol_sac_irp6ot* pbeolsac;
+	// KONSTRUKTORY
+	ecp_task_vislx_irp6ot(configurator &_config);
+	~ecp_task_vislx_irp6ot();
+	
+	// methods for ECP template to redefine in concrete classes
+	void task_initialization(void);
+	void main_task_algorithm(void);
+	
+};
+
+#endif
