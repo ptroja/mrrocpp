c0430b94537d134cf7696d65fb881e186de7479d
Piort Sakowicz <ghard@wp.pl>
2008-08-21 17:50:54 +0000

    added task
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1470 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ include/ecp/irp6_on_track/ecp_t_spots_recognition.h
@@ -0,0 +1,54 @@
+/*!
+ * \file ecp_t_spotsrecognition.h
+ * \brief Declaration of a class responsible
+ * for robot moving, used by Piotr Sakowicz.
+ * - class declaration
+ * \author vented.baffle
+ * \date 21.08.2008
+ */
+
+#ifndef _ECP_T_SpotsRecognition_H_
+#define _ECP_T_SpotsRecognition_H_
+
+#include "ecp/common/ecp_task.h"
+#include "common/typedefs.h"
+#include "common/impconst.h"
+#include "common/com_buf.h"
+
+#include "lib/srlib.h"
+#include "ecp_mp/ecp_mp_t_rcsc.h"
+#include "ecp_mp/ecp_mp_s_schunk.h"
+
+//fradia
+#include "ecp_mp/ecp_mp_s_cvfradia.h"
+#include "ecp/common/ecp_t_cvfradia.h"
+
+#include "ecp/irp6_on_track/ecp_local.h"
+#include "ecp/common/ecp_g_smooth.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+class ecp_t_spots_recognition: public ecp_task
+{
+	protected:
+
+		ecp_smooth_generator* generator;
+
+		/*!
+        * Generator used for communication with cvFraDIA.
+        */
+	    ecp_cvfradia_generator* cvg;
+
+	public:
+		ecp_t_spots_recognition(configurator &_config);
+		~ecp_t_spots_recognition();
+
+		void task_initialization(void);
+		void main_task_algorithm(void);
+};
+
+#endif
+
--- src/ecp/irp6_on_track/ecp_t_spots_recognition.cc
+++ src/ecp/irp6_on_track/ecp_t_spots_recognition.cc
@@ -1,35 +1,63 @@
-#include "common/typedefs.h"
-#include "common/impconst.h"
-#include "common/com_buf.h"
+/*!
+ * \file ecp_t_spots_recognition.cc
+ * \brief Piotr Sakowicz's methods responsible
+ * for robot moving while external calibration.
+ * - methods definitions
+ * \author vented.baffle
+ * \date 21.08.2008
+ */
 
-#include "lib/srlib.h"
-#include "ecp_mp/ecp_mp_t_rcsc.h"
-#include "ecp_mp/ecp_mp_s_schunk.h"
-
-#include "ecp/irp6_on_track/ecp_local.h"
-#include "ecp/common/ecp_g_smooth.h"
-#include "ecp/irp6_on_track/ecp_t_tb_irp6ot.h"
+#include "ecp/irp6_on_track/ecp_t_spots_recognition.h"
 //own libraries
 //Constructors
-ecp_t_tb_irp6ot::ecp_t_tb_irp6ot(configurator &_config): ecp_task(_config){
-};
+ecp_t_spots_recognition::ecp_t_spots_recognition(configurator &_config): ecp_task(_config)
+{
 
-ecp_t_tb_irp6ot::~ecp_t_tb_irp6ot(){
-};
+}
+
+ecp_t_spots_recognition::~ecp_t_spots_recognition()
+{
+
+}
 
 // methods for ECP template to redefine in concrete classes
-void ecp_t_tb_irp6ot::task_initialization(void){
-	ecp_m_robot=new ecp_irp6_on_track_robot(*this);
-	sr_ecp_msg->message("ECP loaded ps");
-};
+void ecp_t_spots_recognition::task_initialization(void)
+{
 
-void ecp_t_tb_irp6ot::main_task_algorithm(void){
-	sr_ecp_msg->message("ECP ps");
+	// Create cvFraDIA sensor - for testing purposes.
+	sensor_m[SENSOR_CVFRADIA] = new ecp_mp_cvfradia_sensor(SENSOR_CVFRADIA, "[vsp_cvfradia]", *this);
+	// Configure sensor.
+	sensor_m[SENSOR_CVFRADIA]->configure_sensor();
+
+// Create an adequate robot. - depending on the ini section name.
+// Note - only on track working
+if (strcmp(config.section_name, "[ecp_irp6_on_track]") == 0)
+{
+    ecp_m_robot = new ecp_irp6_on_track_robot (*this);
+    sr_ecp_msg->message("IRp6 on Track loaded");
+}
+/*else if (strcmp(config.section_name, "[ecp_irp6_postument]") == 0)
+{
+    ecp_m_robot = new ecp_irp6_postument_robot (*this);
+    sr_ecp_msg->message("IRp6 Postument loaded");
+}*/
+	// Create generator and pass sensor to it.
+	cvg = new ecp_cvfradia_generator(*this);
+ 	cvg->sensor_m = sensor_m;
+}
+
+void ecp_t_spots_recognition::main_task_algorithm(void)
+{
+	sr_ecp_msg->message("Press START");
 	ecp_wait_for_start();
-	sr_ecp_msg->message("works");
-	ecp_wait_for_stop();
-};
 
-ecp_task* return_created_ecp_task(configurator &_config){
-	return new ecp_t_tb_irp6ot(_config);
+	cvg->Move();
+
+	sr_ecp_msg->message("Press STOP");
+	ecp_wait_for_stop();
+}
+
+ecp_task* return_created_ecp_task(configurator &_config)
+{
+	return new ecp_t_spots_recognition(_config);
 }
