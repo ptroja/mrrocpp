64181c52a23ef9d6c722c12a1f405cc5cfd0c944
Tomasz Bem <mebmot@wp.pl>
2008-08-30 18:18:20 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1496 4c39dba3-d726-0410-8099-c98d187771e6

--- include/ecp/irp6_on_track/ecp_t_tb_irp6ot.h
+++ include/ecp/irp6_on_track/ecp_t_tb_irp6ot.h
@@ -3,12 +3,17 @@
 
 #include "ecp/common/ecp_task.h"
 #include "ecp/common/ecp_g_smooth.h"
-
+#include "ecp/common/ecp_g_force.h"
 
 class ecp_t_tb_irp6ot: public ecp_task
 {
 	protected:
-	ecp_smooth_generator* sgen;
+		//smoth movement generator
+		ecp_smooth_generator* sgen;
+		//calibration of force
+		bias_edp_force_generator* befgen;
+		//gripper approach with force control
+		ecp_tff_gripper_approach_generator* gagen;
 
 	public:
 		ecp_t_tb_irp6ot(configurator &_config);
--- src/ecp/irp6_on_track/ecp_t_tb_irp6ot.cc
+++ src/ecp/irp6_on_track/ecp_t_tb_irp6ot.cc
@@ -6,13 +6,13 @@
 #include "ecp/irp6_on_track/ecp_local.h"
 #include "ecp/irp6_on_track/ecp_t_tb_irp6ot.h"
 #include <stdio.h>
-//#include "ecp/common/ecp_generator_t.h"
 
-//own libraries
 
 //Constructors
 ecp_t_tb_irp6ot::ecp_t_tb_irp6ot(configurator &_config): ecp_task(_config){
 	sgen=NULL;
+	befgen=NULL;
+	gagen=NULL;
 };
 
 ecp_t_tb_irp6ot::~ecp_t_tb_irp6ot(){
@@ -20,20 +20,32 @@ ecp_t_tb_irp6ot::~ecp_t_tb_irp6ot(){
 
 // methods for ECP template to redefine in concrete classes
 void ecp_t_tb_irp6ot::task_initialization(void){
+	//initialization of robot
 	ecp_m_robot=new ecp_irp6_on_track_robot(*this);
 	sgen=new ecp_smooth_generator(*this, true);
+	befgen=new bias_edp_force_generator(*this);
+	//gripper approach constructor (task&, no_of_steps)
+	gagen=new ecp_tff_gripper_approach_generator (*this, 8);
 	sr_ecp_msg->message("ECP loaded tb");
 };
 
 void ecp_t_tb_irp6ot::main_task_algorithm(void){
 	//char *path="/net/home-host/home/mrrocpp/trj/draughts/pawn_moving.trj";
+	//file with trajectory to smooth generator
 	char *path="/net/lenin/mnt/trj/draughts/pawn_moving.trj";
 	
 	sr_ecp_msg->message("ECP tb.... ready");
 	ecp_wait_for_start();
-	//sr_ecp_msg->message("works");
+	
 	sgen->load_file_with_path(path);
 	sgen->Move();
+	
+	befgen->Move();
+	
+	//configuration of gripper approach configure(speed, time_period)
+	gagen->configure(0.1,2);
+	gagen->Move();
+	
 	ecp_termination_notice();
 	ecp_wait_for_stop();
 };
