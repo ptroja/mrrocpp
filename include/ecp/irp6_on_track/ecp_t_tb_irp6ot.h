ddbe6fe197963437d49ee7d58703dee5cb31b362
Tomasz Bem <mebmot@wp.pl>
2008-09-01 12:54:54 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1500 4c39dba3-d726-0410-8099-c98d187771e6

--- include/ecp/irp6_on_track/ecp_t_tb_irp6ot.h
+++ include/ecp/irp6_on_track/ecp_t_tb_irp6ot.h
@@ -4,9 +4,9 @@
 #include "ecp/common/ecp_task.h"
 #include "ecp/common/ecp_g_smooth.h"
 #include "ecp/common/ecp_g_force.h"
+#include "common/com_buf.h"
 
-class ecp_t_tb_irp6ot: public ecp_task
-{
+class ecp_t_tb_irp6ot: public ecp_task{
 	protected:
 		//smoth movement generator
 		ecp_smooth_generator* sgen;
@@ -14,11 +14,16 @@ class ecp_t_tb_irp6ot: public ecp_task
 		bias_edp_force_generator* befgen;
 		//gripper approach with force control
 		ecp_tff_gripper_approach_generator* gagen;
+		//linear generator
+		ecp_linear_generator *lgen;
+		//trajectory description from com_buf.h
+		trajectory_description tdes;
 
 	public:
 		ecp_t_tb_irp6ot(configurator &_config);
 		~ecp_t_tb_irp6ot();
-
+		void set_tdes(double, double, double, double, double, double, double);
+		void init_tdes(POSE_SPECIFICATION, int,int,int);
 		void task_initialization(void);
 		void main_task_algorithm(void);
 };
--- src/ecp/irp6_on_track/ecp_t_tb_irp6ot.cc
+++ src/ecp/irp6_on_track/ecp_t_tb_irp6ot.cc
@@ -13,6 +13,7 @@ ecp_t_tb_irp6ot::ecp_t_tb_irp6ot(configurator &_config): ecp_task(_config){
 	sgen=NULL;
 	befgen=NULL;
 	gagen=NULL;
+	lgen=NULL;
 };
 
 ecp_t_tb_irp6ot::~ecp_t_tb_irp6ot(){
@@ -30,21 +31,51 @@ void ecp_t_tb_irp6ot::task_initialization(void){
 };
 
 void ecp_t_tb_irp6ot::main_task_algorithm(void){
-	//char *path="/net/home-host/home/mrrocpp/trj/draughts/pawn_moving.trj";
+	char *path="/net/home-host/home/mrrocpp/trj/draughts/1_pawn_moving.trj";
+	sr_ecp_msg->message(path);
 	//file with trajectory to smooth generator
-	char *path="/net/lenin/mnt/trj/draughts/pawn_moving.trj";
+	//char *path="/net/lenin/mnt/trj/draughts/1_pawn_moving.trj";
 	
 	sr_ecp_msg->message("ECP tb.... ready");
 	ecp_wait_for_start();
 	
 	sgen->load_file_with_path(path);
 	sgen->Move();
-	
 	befgen->Move();
 	
 	//configuration of gripper approach configure(speed, time_period)
 	gagen->configure(0.1,2);
 	gagen->Move();
+	
+	//configuration and initalization of linear generator
+	init_tdes(XYZ_EULER_ZYZ,1,2,0);
+	set_tdes(0,0,0.000,0,0,0,0);	//podniesienie o 2mm
+	
+	// Generator trajektorii prostoliniowej
+	lgen=new ecp_linear_generator(*this,tdes,1);
+	sr_ecp_msg->message("test4");
+	//lgen->Move();
+	sr_ecp_msg->message("test4");
+	delete lgen;
+	
+	sr_ecp_msg->message("test5");
+	set_tdes(0,0,0,0,0,0,0.01);	//gripper closing
+	lgen=new ecp_linear_generator(*this,tdes,1);
+	sr_ecp_msg->message("test5");
+	//lgen->Move();
+	sr_ecp_msg->message("test5");
+	delete lgen;	
+	
+	set_tdes(0,0,0.02,0,0,0,0);	//podniesienie o 2cm
+	lgen=new ecp_linear_generator(*this,tdes,1);
+	//lgen->Move();
+	delete lgen;
+	
+	//char *path1="/net/lenin/mnt/trj/draughts/2_pawn_moving.trj";
+	char *path1="/net/home-host/home/mrrocpp/trj/draughts/2_pawn_moving.trj";
+	sr_ecp_msg->message(path1);
+	sgen->load_file_with_path(path1);
+	sgen->Move();
 	
 	ecp_termination_notice();
 	ecp_wait_for_stop();
@@ -53,3 +84,22 @@ void ecp_t_tb_irp6ot::main_task_algorithm(void){
 ecp_task* return_created_ecp_task(configurator &_config){
 	return new ecp_t_tb_irp6ot(_config);
 }
+
+void ecp_t_tb_irp6ot::set_tdes(double cor0, double cor1, double cor2, double cor3, double cor4, double cor5, double cor6){
+	// Wspolrzedne kartezjanskie XYZ i katy Eulera ZYZ
+	tdes.coordinate_delta[0] = cor0; // przyrost wspolrzednej X
+	tdes.coordinate_delta[1] = cor1;// przyrost wspolrzednej Y
+	tdes.coordinate_delta[2] = cor2; // przyrost wspolrzednej Z
+	tdes.coordinate_delta[3] = cor3; // przyrost wspolrzednej FI
+	tdes.coordinate_delta[4] = cor4; // przyrost wspolrzednej TETA
+	tdes.coordinate_delta[5] = cor5; // przyrost wspolrzednej PSI
+	tdes.coordinate_delta[6] = cor6; // przyrost wspolrzednej PSI	
+}
+
+//inicjacja struktury tdes - trajectory description
+void ecp_t_tb_irp6ot::init_tdes(POSE_SPECIFICATION pspec, int interpolation_no, int internode_no, int value_no){
+	tdes.arm_type=pspec;
+	tdes.interpolation_node_no=interpolation_no;
+	tdes.internode_step_no=internode_no;	//motion time
+	tdes.value_in_step_no=value_no;			//motion time-2 ??
+} 
