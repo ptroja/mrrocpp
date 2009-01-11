f54ddc2fa789a7c74033432c3078e3a9fc839f42
Jedrzej Kurylo <jedrzej.kurylo@gmail.com>
2009-01-11 11:43:30 +0000

    Wiimote virtual sensor
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1746 4c39dba3-d726-0410-8099-c98d187771e6

--- include/ecp_mp/ecp_mp_s_wiimote.h
+++ include/ecp_mp/ecp_mp_s_wiimote.h
@@ -7,22 +7,78 @@
 
 #define BUFFER_SIZE 8*256
 
+/**
+ * Virtual sensor that communicates with the Wii-mote
+ * 
+ * @author jedrzej
+ */
 class ecp_mp_wiimote_sensor : public sensor
 {
 private:
+	//socket descriptor
 	int sockfd;
+	
+	//address of the wii-mote server
 	sockaddr_in serv_addr;
+	
+	//pointer to the server
 	hostent* server;
+	
+	//buffer used for communication
 	char buffer[BUFFER_SIZE];
+	
+	//link to SRP communication object
 	sr_ecp& sr_ecp_msg;
+	
+	//sensor name
 	SENSOR_ENUM sensor_name;
 
 public:
+	/**
+	 * Creates the sensor object. Connects to the Wii-mote server
+	 * 
+	 * @param _sensor_name 		the name f the sensor
+	 * @param _section_name		the name of the section in the config file
+	 * @param _ecp_mp_object		
+	 * @param _union_size
+	 *
+	 * @author jedrzej
+	 */
 	ecp_mp_wiimote_sensor (SENSOR_ENUM _sensor_name, const char* _section_name, ecp_mp_task& _ecp_mp_object, int _union_size);
+	
+	/**
+	 * Sends sensor configuration to the Wii-mote server
+	 *
+	 * @author jedrzej
+	 */
 	void configure_sensor (void);
+	
+	/**
+	 * Sends "initiate reading" command to the Wii-mote server
+	 *
+	 * @author jedrzej
+	 */
 	void initiate_reading (void);
+	
+	/**
+	 * Sends given command to the Wii-mote server
+	 *
+	 * @author jedrzej
+	 */
 	void send_reading (ECP_VSP_MSG);
+	
+	/**
+	 * Retrieves aggregated data from the Wii-mote server
+	 *
+	 * @author jedrzej
+	 */
 	void get_reading (void);
+	
+	/**
+	 * Terminates the connection to the Wii-mote server
+	 * 
+	 * @author jedrzej
+	 */
 	void terminate();
 };
 #endif
