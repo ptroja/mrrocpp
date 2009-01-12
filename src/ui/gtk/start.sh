2e679a7235a86c7322f7062594cddd4da9c80f14
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-01-12 19:03:09 +0000

    poprawka skryptu startowego
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1752 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ui/gtk/start.sh
+++ src/ui/gtk/start.sh
@@ -8,7 +8,19 @@ export UI_HOST=`hostname`
 sleep 1
 
 PWD=`pwd`
+DEFAULT_CONFIG="../configs/default_file.cfg"
+if [ ! -f ${DEFAULT_CONFIG} ]; then
+	echo "default config file ${DEFAULT_CONFIG} missing"
+	kill ${MESSIP_PID}
+	exit 1
+fi
 CONFIG=`cat ../configs/default_file.cfg`
+CONFIG_FILE="../configs/${CONFIG}"
+if [ ! -f ${CONFIG_FILE} ]; then
+	echo "config file ${CONFIG_FILE} missing"
+	kill ${MESSIP_PID}
+	exit 1
+fi
 ./configsrv `hostname` `dirname ${PWD}`/ ${CONFIG} &
 CONFIGSRV_PID=$!
 
