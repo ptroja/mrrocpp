c9e8dd268cd2a99e6e9138072a984d86914e49b9
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-01-12 14:40:47 +0000

    dodany plik naglowkowy
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1751 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ui/gtk/sr_console.h
@@ -0,0 +1,13 @@
+/*
+ * sr_console.h
+ *
+ *  Created on: Dec 24, 2008
+ *      Author: ptroja
+ */
+
+#ifndef SR_CONSOLE_H_
+#define SR_CONSOLE_H_
+
+void *sr_thread(void* arg);
+
+#endif /* SR_CONSOLE_H_ */
