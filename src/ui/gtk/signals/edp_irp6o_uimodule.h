a4a7d31dd9d68aa36e2add16beb9b42d9a46a85d
Michal Struginski <michal.struginski@gmail.com>
2009-01-24 17:57:44 +0000

    test dla on_track przyciskow Set oraz Read
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1875 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ui/gtk/signals/edp_irp6o_uimodule.h
@@ -0,0 +1,24 @@
+
+#ifndef __EDP_irp6o
+#define __EDP_irp6o
+
+#include <iostream>
+#include <vector>
+
+#include <gtk/gtkbuilder.h>
+#include <gtk/gtk.h>
+
+#include "ui/ui_ecp_r_irp6_common.h"
+
+class edp_irp6o
+{
+	public:
+
+		edp_irp6o(ui_config_entry &entry);
+		~edp_irp6o();
+
+
+};
+ui_common_robot * robot;
+controller_state_t state;
+#endif /* __EDP_irp6o */
---
+++ src/ui/gtk/signals/irp6o_servo_algorithm_widget.h
@@ -0,0 +1,22 @@
+
+#ifndef __EDP_irp6o_SERVO_ALGORITHM
+#define __EDP_irp6o_SERVO_ALGORITHM
+
+#include <iostream>
+#include <vector>
+
+#include <gtk/gtkbuilder.h>
+#include <gtk/gtk.h>
+#include "edp_irp6o_uimodule.h"
+
+class edp_irp6o_servo_algorithm
+{
+	public:
+
+		edp_irp6o_servo_algorithm(ui_widget_entry &entry);
+		~edp_irp6o_servo_algorithm();
+
+
+};
+
+#endif /* __EDP_irp6o_SERVO_ALGORITHM */
