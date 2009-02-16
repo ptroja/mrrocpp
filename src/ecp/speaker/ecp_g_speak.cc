aa75eb0a37f6d2fd0b9549a087b64d71cf0e4195
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-02-16 12:36:57 +0000

    czyszczenie kodu
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@1942 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/speaker/ecp_g_speak.cc
+++ src/ecp/speaker/ecp_g_speak.cc
@@ -1,7 +1,7 @@
 // -------------------------------------------------------------------------
 //                            ecp.cc
 //            Effector Control Process (ECP) - speaker generators
-// 
+//
 // Ostatnia modyfikacja: 2006
 // -------------------------------------------------------------------------
 
@@ -17,43 +17,34 @@
 #include "ecp/speaker/ecp_g_speak.h"
 
 speaking_generator::speaking_generator(ecp_task& _ecp_task, int step):
-	 ecp_generator(_ecp_task){	step_no = step;  };  
+	 ecp_generator(_ecp_task){	step_no = step;  };
 
 bool speaking_generator::configure(const char* text)
 {
-
-	if (text!=NULL)
-	{ 
-		strcpy( the_robot->EDP_data.text, text);
-		strcpy( the_robot->EDP_data.prosody, "neutral");
+	if (text != NULL) {
+		strcpy(the_robot->EDP_data.text, text);
+		strcpy(the_robot->EDP_data.prosody, "neutral");
 		return true;
-	} else
-	{
+	} else {
 		return false;
 	}
 }
 
-
 bool speaking_generator::first_step ( ) {
-
 
 	for (int i=0; i<6; i++)
 		delta[i]=0.0;
 
 	//(sensor_m.begin())->second->base_period=1;
- 	//(sensor_m.begin())->second->current_period=0;
+	//(sensor_m.begin())->second->current_period=0;
 	communicate_with_edp = true;
-	
 
 	last_sg_state = new_sg_state = SG_FIRST_GET;
 
-
-		the_robot->EDP_data.instruction_type = GET;
-
-	
+	the_robot->EDP_data.instruction_type = GET;
 
 	return true;
-}; // end: bool y_nose_run_force_generator::first_step ( )
+}
 // --------------------------------------------------------------------------
 
 
@@ -69,62 +60,52 @@ bool speaking_generator::next_step ( ) {
 		ecp_t.set_ecp_reply (ECP_ACKNOWLEDGE);
 		ecp_t.mp_buffer_receive_and_send ();
 	}
-	*/ //odrem jako niezalezny od rcsc generator 
-	
+	*/ //odrem jako niezalezny od rcsc generator
+
 	last_sg_state = new_sg_state;
-	
-	
-	
+
+
+
 	// Przygotowanie kroku ruchu - do kolejnego wezla interpolacji
 
 	switch (last_sg_state)
 	{
 		case SG_FIRST_GET:
-			if (the_robot->EDP_data.speaking==0)
-			{
+			if (the_robot->EDP_data.speaking == 0) {
 				the_robot->EDP_data.instruction_type = SET;
-				new_sg_state=SG_AFTER_SET;
+				new_sg_state = SG_AFTER_SET;
+			} else {
+				new_sg_state = SG_FIRST_GET;
+				usleep(1000 * 20);
 			}
-			else
-			{
-				new_sg_state=SG_FIRST_GET;
-				usleep(1000*20);
-			}
-		break;
+			break;
 		case SG_AFTER_SET:
 			the_robot->EDP_data.instruction_type = GET;
 			new_sg_state=SG_LAST_GET;
-		break;
+			break;
 		case SG_LAST_GET:
-			if (the_robot->EDP_data.speaking==0)
-			{
-				new_sg_state=SG_FINISH;	
+			if (the_robot->EDP_data.speaking == 0) {
+				new_sg_state = SG_FINISH;
 				communicate_with_edp = false;
+			} else {
+				new_sg_state = SG_LAST_GET;
+				usleep(1000 * 20);
 			}
-			else
-			{
-				new_sg_state=SG_LAST_GET;
-				usleep(1000*20);
-			}
-		break;
+			break;
 		case SG_FINISH:
 			return false;
-		break;
+			break;
 		default:
-		break;
+			break;
 	}
-	
-	
+
 //	printf("last %d\n",lastSET);
-	
-	
+
 	// for mic
 	// printf("%d \n",(sensor_m.begin())->second->image.sensor_union.mic.word_id);
-	
-	// strcpy( the_robot->EDP_data.text, "przesuñ kostkê w prawo" );
-	 
 
+	// strcpy( the_robot->EDP_data.text, "przesuï¿½ kostkï¿½ w prawo" );
 
 	return true; //false;
-}; //  end:  y_nose_run_force_generator::next_step ( )
+}
 
--- src/ecp/speaker/ecp_local.cc
+++ src/ecp/speaker/ecp_local.cc
@@ -3,7 +3,7 @@
 //            Effector Control Process (ECP) - methods
 // Funkcje do tworzenia procesow ECP
 // robot - speaker
-// 
+//
 // Ostatnia modyfikacja: 16.04.98
 // -------------------------------------------------------------------------
 
@@ -18,52 +18,58 @@
 // ####################################################################################################
 // ####################################################################################################
 
-ecp_speaker_robot::ecp_speaker_robot (configurator &_config, sr_ecp *_sr_ecp)
-	:	ecp_robot (ROBOT_SPEAKER, _config, _sr_ecp)
-{};
-ecp_speaker_robot::ecp_speaker_robot (ecp_task& _ecp_object)
-	:	ecp_robot (ROBOT_SPEAKER, _ecp_object)
-{ };
+ecp_speaker_robot::ecp_speaker_robot(configurator &_config, sr_ecp *_sr_ecp) :
+	ecp_robot(ROBOT_SPEAKER, _config, _sr_ecp)
+{
+}
+
+ecp_speaker_robot::ecp_speaker_robot(ecp_task& _ecp_object) :
+	ecp_robot(ROBOT_SPEAKER, _ecp_object)
+{
+}
 
 // --------------------------------------------------------------------------
-void ecp_speaker_robot::create_command (void) {
-  // wypelnia bufor wysylkowy do EDP na podstawie danych
-  // zawartych w skladowych generatora lub warunku
+void ecp_speaker_robot::create_command(void)
+{
+	// wypelnia bufor wysylkowy do EDP na podstawie danych
+	// zawartych w skladowych generatora lub warunku
 
-  ecp_command.instruction.instruction_type = EDP_data.instruction_type;
+	ecp_command.instruction.instruction_type = EDP_data.instruction_type;
 
-  switch (EDP_data.instruction_type) {
-    case SET:
-    case SET_GET:
-    	strcpy(ecp_command.instruction.arm.text_def.text, EDP_data.text);
-	strcpy(ecp_command.instruction.arm.text_def.prosody, EDP_data.prosody);
-     break;
-    case GET:
-    case QUERY:
-         break;
-     case SYNCHRO:
-    default: // blad: nieprawidlowe polecenie
-      throw ECP_error (NON_FATAL_ERROR, INVALID_ECP_COMMAND);
-  }; // end: switch (instruction_type)
-
-}; // end: ecp_speaker_robot::create_command
+	switch (EDP_data.instruction_type)
+	{
+		case SET:
+		case SET_GET:
+			strcpy(ecp_command.instruction.arm.text_def.text, EDP_data.text);
+			strcpy(ecp_command.instruction.arm.text_def.prosody, EDP_data.prosody);
+			break;
+		case GET:
+		case QUERY:
+			break;
+		case SYNCHRO:
+		default: // blad: nieprawidlowe polecenie
+			throw ECP_error(NON_FATAL_ERROR, INVALID_ECP_COMMAND);
+	} // end: switch (instruction_type)
+}
 // ---------------------------------------------------------------
 
 /*---------------------------------------------------------------------*/
-void ecp_speaker_robot::get_reply (void) {
-  // pobiera z pakietu przeslanego z EDP informacje i wstawia je do
-  // odpowiednich skladowych generatora lub warunku
-EDP_data.reply_type = reply_package.reply_type;
+void ecp_speaker_robot::get_reply(void)
+{
+	// pobiera z pakietu przeslanego z EDP informacje i wstawia je do
+	// odpowiednich skladowych generatora lub warunku
+	EDP_data.reply_type = reply_package.reply_type;
 
- switch (EDP_data.reply_type) {
-   case ERROR:
-     EDP_data.error_no.error0 = reply_package.error_no.error0;
-     EDP_data.error_no.error1 = reply_package.error_no.error1;
-     break;
-   case  ACKNOWLEDGE:
-       EDP_data.speaking = reply_package.arm.text_def.speaking;
-   break;
-   default:  // bledna przesylka
-     throw ECP_error (NON_FATAL_ERROR, INVALID_EDP_REPLY);
- }; // end: switch (EDP_data.reply_type)
-}; // end: ecp_speaker_robot::get_reply ()
+	switch (EDP_data.reply_type)
+	{
+		case ERROR:
+			EDP_data.error_no.error0 = reply_package.error_no.error0;
+			EDP_data.error_no.error1 = reply_package.error_no.error1;
+			break;
+		case ACKNOWLEDGE:
+			EDP_data.speaking = reply_package.arm.text_def.speaking;
+			break;
+		default: // bledna przesylka
+			throw ECP_error(NON_FATAL_ERROR, INVALID_EDP_REPLY);
+	} // end: switch (EDP_data.reply_type)
+} // end: ecp_speaker_robot::get_reply ()
