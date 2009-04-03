181ea1f20a6edec22f49f693ea907d1da9445099
Jedrzej Kurylo <jedrzej.kurylo@gmail.com>
2009-04-03 10:49:34 +0000

    Poprawki
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2023 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/irp6_on_track/ecp_g_wii_velocity.cc
+++ src/ecp/irp6_on_track/ecp_g_wii_velocity.cc
@@ -66,12 +66,12 @@ bool ecp_wii_velocity_generator::next_step()
 
 	//wyznaczenie nowych wartosci predkosci
 	configure_velocity(
-		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_0 : 0,	
+		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up && !sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.left && !sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.right ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_0 : 0,	
 		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.right ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_1 :	 0,
 		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.down ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_2 : 0,
 		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.left ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_3 : 0,
-		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up && sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.right ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_4 : 0,
-		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up && sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.left ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_5 : 0
+		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up && sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.right && !sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.left ? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_4 : 0,
+		sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.up && sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.left && !sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.right? sensor_m[SENSOR_WIIMOTE]->image.sensor_union.wiimote.orientation_y * C_5 : 0
 	);	
 
 	for (int i=0; i<6; i++)
