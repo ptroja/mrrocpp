fd29ce697b5b8b7db258e420c87e3774fe94acab
Tomasz Kornuta <tkornuta@gmail.com>
2009-05-12 11:38:16 +0000

    Missing calibration headers.
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2181 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ include/ecp/irp6_on_track/ecp_g_robot_calibration.h
@@ -0,0 +1,45 @@
+/*
+ * ECP_G_ROBOT_CALIBRATION.h
+ *
+ *  Created on: May 08, 2009
+ *      Author: mani.baktha
+ */
+
+#ifndef ECP_G_ROBOT_CALIBRATION_H_
+#define ECP_G_ROBOT_CALIBRATION_H_
+
+//PCBIRD Virtual Sensors defined here
+#include "ecp_mp/ecp_mp_s_pcbird.h"
+#include "ecp/common/ecp_t_pcbird.h"
+
+// we will write a generator robocalibgen derived from generator namespace 
+#include "ecp/common/ecp_generator.h"	
+
+
+namespace mrrocpp {
+namespace ecp {
+namespace irp6ot {
+namespace generator {
+
+// robocalibgen similar to spots.class representing generator
+class robotcalibgen : public common::generator::generator
+{
+	short iter;
+	ecp_mp::sensor::pcbird * sensor;
+	// sensor image to receive the readings from pcbird
+	lib::SENSOR_IMAGE pcbird_sensor_image;
+
+
+  public:
+	robotcalibgen(common::task::task& _ecp_task);
+	bool first_step();
+	bool next_step();
+
+};
+
+}
+} // namespace irp6ot
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif /* ECP_G_ROBOT_CALIBRATION_H */
