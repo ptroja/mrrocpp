1bbcf81ca22e1807a03dfe2b4ec0adcc0a7d0c6a
Michal Struginski <michal.struginski@gmail.com>
2009-05-18 18:08:39 +0000

    temporary solution - UI libraries doesn't seem to compile properly.
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2200 4c39dba3-d726-0410-8099-c98d187771e6

--- src/cross.sh
+++ src/cross.sh
@@ -2,14 +2,20 @@
 # cross-compilation for Linux UI
 
 #QNX binaries
-#make TARGET=qnx
-#make TARGET=qnx install
+make TARGET=qnx
+make TARGET=qnx install
 
 make clean
 
 #Linux UI libraries + messip binaries
 make TARGET=linux
 make TARGET=linux install
+
+#temporary solution - above doesn't seem to compile properly the UI libraries (though compile properly messip binaries)
+cd ui/src/gcc_ntox86
+make BUILD_TARGET=linux -k 2>/dev/null
+cd ../../..
+
 
 #Linux UI binaries
 make TARGET=linux_gtk
