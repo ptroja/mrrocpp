33eeffb6bee31c49a655926bef1cda479957f988
Rafal Tulwin <rtulwin@gmail.com>
2009-08-19 13:06:13 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2369 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/irp6_on_track/ecp_vis_ib_eih_follower_irp6ot.cc
@@ -0,0 +1,191 @@
+/*
+ * ecp_vis_ib_eih_follower_irp6ot.cc
+ *
+ *  Created on: Dec 9, 2008
+ *      Author: rtulwin
+ */
+
+#include "ecp/irp6_on_track/ecp_vis_ib_eih_follower_irp6ot.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace irp6ot {
+
+ecp_vis_ib_eih_follower_irp6ot::ecp_vis_ib_eih_follower_irp6ot(common::task::task& _ecp_task) :
+	common::ecp_visual_servo(_ecp_task) {
+	retrieve_parameters();
+}
+
+ecp_vis_ib_eih_follower_irp6ot::~ecp_vis_ib_eih_follower_irp6ot() {
+
+}
+
+/*void ecp_vis_ib_eih_planar_irp6ot::retrieve_parameters() {
+	//Maksymalna wartosc  predkosci.
+	v_max = ecp_t.config.return_double_value("v_max");
+
+	//Wartosc przyspieszenia z jakim osiagana jest maksymalna predkosc.
+	a = ecp_t.config.return_double_value("a");
+
+	//Minimalna  wartosc predkosci do jakiej schodzimy przy hamowaniu.
+	v_min = ecp_t.config.return_double_value("v_min");
+
+	//Dystans wyrazony w pikselach, przy ktorym powinnismy hamowac.
+	breaking_dist = ecp_t.config.return_double_value("breaking_dist");
+
+}*/
+
+bool ecp_vis_ib_eih_follower_irp6ot::first_step() {
+	vsp_fradia = sensor_m[lib::SENSOR_CVFRADIA];
+
+	the_robot->EDP_data.instruction_type = lib::GET;
+	the_robot->EDP_data.get_type = ARM_DV;
+	the_robot->EDP_data.get_arm_type = lib::XYZ_ANGLE_AXIS;
+	the_robot->EDP_data.motion_type = lib::ABSOLUTE;
+	vsp_fradia->to_vsp.haar_detect_mode = lib::WITHOUT_ROTATION;
+	//t_m = MOTION_STEPS * STEP;
+	//x = 0;y = 0;v = 0;s = 0;
+	//breaking = false;
+	//first_move =  true;
+
+	//ecp_t.sr_ecp_msg->message("PIERWSZY");
+
+	//above_object = false;
+
+	return true;
+}
+
+bool ecp_vis_ib_eih_follower_irp6ot::next_step() {
+	//Odczytanie pozycji poczatkowej koncowki w reprezentacji os-kat.
+	if (node_counter == 1) {
+
+		memcpy(next_position,
+	 			the_robot->EDP_data.current_XYZ_AA_arm_coordinates, 6
+						* sizeof(double));
+		next_position[6] = the_robot->EDP_data.current_gripper_coordinate;
+
+		the_robot->EDP_data.instruction_type = lib::SET_GET;
+		the_robot->EDP_data.get_type = ARM_DV;
+		the_robot->EDP_data.set_type = ARM_DV;
+		the_robot->EDP_data.set_arm_type = lib::XYZ_ANGLE_AXIS;
+		the_robot->EDP_data.get_arm_type = lib::JOINT;
+		the_robot->EDP_data.motion_type = lib::ABSOLUTE;
+		the_robot->EDP_data.next_interpolation_type = lib::MIM;
+		the_robot->EDP_data.motion_steps = MOTION_STEPS;
+		the_robot->EDP_data.value_in_step_no = MOTION_STEPS - 1;
+
+		memcpy(the_robot->EDP_data.next_XYZ_AA_arm_coordinates, next_position,
+				6 * sizeof(double));
+		the_robot->EDP_data.next_gripper_coordinate = next_position[6];
+	}
+
+	//Zadawanie ruchu wzgledem aktualnego polozenia koncowki.
+	else {
+		alpha = the_robot->EDP_data.current_joint_arm_coordinates[1]- the_robot->EDP_data.current_joint_arm_coordinates[6];
+		//Uchyb wyrazony w pikselach.
+		double ux = vsp_fradia->from_vsp.comm_image.sensor_union.deviation.x;
+		double uy = vsp_fradia->from_vsp.comm_image.sensor_union.deviation.y;
+
+		//Sprawdz czy jest odczyt z fradii.
+		lib::VSP_REPORT vsp_report = vsp_fradia->from_vsp.vsp_report;
+		if (vsp_report == lib::VSP_REPLY_OK) {
+			//Sprawdzam czy osiagnieto odleglosc przy ktorej hamujemy.
+			if (fabs(ux) < breaking_dist && fabs(uy) < breaking_dist)
+				breaking = true;
+			if (breaking) { //Gdy raz wkroczymy w rejon hamowania hamujemy do konca.
+				if (v <= v_min)//Osiagnieto zadana predkosc przy hamowaniu.
+				{
+					breaking = false;
+					above_object = true;
+					return false;
+				}
+				else { //Wyhamuj
+					s = v * t_m - 0.5 * a * t_m * t_m;
+					v = v - a * t_m;//Predkosc w nastepnym makrokroku.
+					//Sprawdzam czy nie przekroczono predkosci mnimalnej.
+					if (v < v_min)
+						v = v_min;
+				}
+			} else if (v < v_max) { //Przyspieszanie
+				s = v * t_m + 0.5 * a * t_m * t_m;
+				v = v + a * t_m;//Predkosc w nastepnym makrokroku.
+				//Sprawdzam czy nie przekroczono predkosci maxymalnej.
+				if (v > v_max)
+					v = v_max;
+			} else if (v = v_max) { //Ruch jednostajny.
+				//return false;
+				s = v * t_m;
+			}
+		} else{
+			s = 0;
+			first_move=true;
+		}
+
+		//Sprawdzamy czy nie bylo zbyt dluzej przerwy.
+		if(!check_if_followed())
+			return false;
+
+		//Kierunek ruchu wzgledem ukladu chwytaka.
+		double direction = atan2(-ux, -uy) + alpha;
+		x = cos(direction) * s;
+		y = sin(direction) * s;
+
+
+		//std::cout << frame_no << " " << s << " " << x << " " << y << "<< direction << std::endl;
+
+		next_position[0] += x;
+		next_position[1] += y;
+//		next_position[2] += -0.002;
+
+		the_robot->EDP_data.instruction_type = lib::SET_GET;
+		the_robot->EDP_data.get_type = ARM_DV;
+		the_robot->EDP_data.set_type = ARM_DV;
+		the_robot->EDP_data.set_arm_type = lib::XYZ_ANGLE_AXIS;
+		the_robot->EDP_data.get_arm_type = lib::JOINT;
+		the_robot->EDP_data.motion_type = lib::ABSOLUTE;
+		the_robot->EDP_data.next_interpolation_type = lib::MIM;
+		the_robot->EDP_data.motion_steps = MOTION_STEPS;
+		the_robot->EDP_data.value_in_step_no = MOTION_STEPS - 1;
+		memcpy(the_robot->EDP_data.next_XYZ_AA_arm_coordinates, next_position,
+				6 * sizeof(double));
+		the_robot->EDP_data.next_gripper_coordinate = next_position[6];
+
+		return true;
+	}
+}
+
+/*bool ecp_vis_ib_eih_planar_irp6ot::check_if_followed(){
+	double frame_no =
+			vsp_fradia->from_vsp.comm_image.sensor_union.deviation.frame_number;
+
+	//Ograniczenia na ruch
+	if (first_move){
+		first_move = false;
+		old_frame_no = frame_no;
+		holes=0;
+	}else {
+		if (old_frame_no == frame_no)//ten sam obiekt
+			holes++;
+		else
+			holes = 0;
+	}
+	old_frame_no = frame_no;
+	//sprawdzam czy nie bylo zbyt dlugiej przerwy w wykryciach obiektu
+	if (holes > 10) {
+		ecp_t.sr_ecp_msg->message("Zgubiono obiekt\n");
+		first_move = true;
+		holes = 0;
+		return false;
+	}else
+		return true;
+
+}*/
+
+/*void ecp_vis_ib_eih_planar_irp6ot::entertain_constraints() {
+
+}*/
+} // namespace irp6ot
+} // namespace ecp
+} // namespace mrrocpp
+
+
---
+++ src/ecp/irp6_on_track/ecp_vis_ib_eih_follower_irp6ot.h
@@ -0,0 +1,82 @@
+/*
+ * ecp_vis_ib_eih_follower_irp6ot.h
+ *
+ *  Created on: Dec 9, 2008
+ *      Author: rtulwin
+ */
+
+#ifndef ECP_VIS_IB_EIH_FOLLOWER_IRP6OT_H_
+#define ECP_VIS_IB_EIH_FOLLOWER_IRP6OT_H_
+
+#include "ecp/common/ecp_visual_servo.h"
+
+#include <string.h>
+#include <math.h>
+
+#include "lib/impconst.h"
+#include "lib/com_buf.h"
+
+#include "ecp_mp/ecp_mp_s_cvfradia.h"
+#include "ecp/common/ecp_t_cvfradia.h"
+
+#include <iostream>
+
+namespace mrrocpp {
+namespace ecp {
+namespace irp6ot {
+
+#define MOTION_STEPS 25
+
+
+class ecp_vis_ib_eih_follower_irp6ot: public common::ecp_visual_servo {
+	//bool check_if_followed();
+public:
+	//Wirtualny sensor
+	lib::sensor *vsp_fradia;
+	//Pozycja w nastepnym kroku.
+    double next_position[8];
+    //Obliczone sterowanie dla osi x;
+    double x;
+    //Obliczone sterowanie dla osi y;
+    double y;
+    //Przyrost polozenia w makrokroku
+    double s;
+
+    //Orientacja koncokwi wzgledem ukladu bazowego.
+    double alpha;
+
+	//Maksymalna wartosc  predkosci.
+	double v_max;
+	//Wartosc przyspieszenia z jakim osiagana jest maksymalna predkosc.
+	double  a;
+	//Minimalna  wartosc predkosci do jakiej schodzimy przy hamowaniu.
+	double v_min;
+	//Predkosc chwilowa.
+	double v;
+	//Dystans wyrazony w pikselach, przy ktorym nastepuje hamowanie.
+	double breaking_dist;
+	//Czas makrokroku.
+	double t_m;
+	//Droga do przejechania w nastepnym makrokroku.
+
+	double old_frame_no;
+	bool first_move;
+	bool breaking;
+	int holes;
+	bool above_object;
+
+	ecp_vis_ib_eih_follower_irp6ot(common::task::task& _ecp_task);
+	virtual ~ecp_vis_ib_eih_follower_irp6ot();
+	virtual bool first_step(void);
+	virtual bool next_step();
+	//virtual void entertain_constraints();
+	//virtual void retrieve_parameters();
+};
+
+} // namespace irp6ot
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif /* ECP_VIS_IB_EIH_FOLLOWER_IRP6OT_H_ */
+
+
