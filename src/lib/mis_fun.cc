96ee8edba4a57f3e14aaa47ead6cd4af4fce368c
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-08-31 17:55:37 +0000

    thread priority setting warning messages
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2424 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/mis_fun.cc
+++ src/lib/mis_fun.cc
@@ -13,9 +13,29 @@ void set_thread_priority(pthread_t thread, int sched_priority_l)
 	if (pthread_getschedparam(thread, &policy, &param)) {
 		perror("pthread_getschedparam()");
 	}
-	param.sched_priority = sched_priority_l;
-	if (pthread_setschedparam(thread, policy, &param)) {
-		perror("pthread_setschedparam()");
+
+	// check priority range
+	int policy_priority_min = sched_get_priority_min(policy);
+	if(policy_priority_min == -1) {
+		perror("sched_get_priority_min()");
+	}
+
+	int policy_priority_max = sched_get_priority_max(policy);
+	if(policy_priority_max == -1) {
+		perror("sched_get_priority_max()");
+	}
+
+	if ((sched_priority_l < policy_priority_min) ||
+		(sched_priority_l > policy_priority_max)) {
+		fprintf(stderr,
+				"requested thread priority (%d) not in <%d:%d> priority range\n",
+				sched_priority_l,
+				policy_priority_min, policy_priority_max);
+	} else {
+		param.sched_priority = sched_priority_l;
+		if (pthread_setschedparam(thread, policy, &param)) {
+			perror("pthread_setschedparam()");
+		}
 	}
 }
 
