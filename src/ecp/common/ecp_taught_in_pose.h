bd24540ac8b5534bb7215999f9bb4e9c6f858ec5
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-08-31 08:57:45 +0000

    const modifier to constructor parameter added
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2416 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/common/ecp_taught_in_pose.h
+++ src/ecp/common/ecp_taught_in_pose.h
@@ -13,14 +13,19 @@ public:
   lib::POSE_SPECIFICATION arm_type;
   double motion_time;
   double coordinates[MAX_SERVOS_NR];
-  
+
   int extra_info; // by Y uzupelnienie struktury o dodatkowe pole, do dowolnego wykorzystania
-  
+
   ecp_taught_in_pose (void);
 
-  ecp_taught_in_pose (lib::POSE_SPECIFICATION at, double mt, double* c);
+  ecp_taught_in_pose (lib::POSE_SPECIFICATION at,
+		  double mt,
+		  const double* c);
 
-  ecp_taught_in_pose (lib::POSE_SPECIFICATION at, double mt, int e_info, double* c); 
+  ecp_taught_in_pose (lib::POSE_SPECIFICATION at,
+		  double mt,
+		  int e_info,
+		  const double* c);
 }; // end:class ecp_taught_in_pose
 
 } // namespace common
