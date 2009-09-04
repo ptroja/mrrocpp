beb238e2c5e465cf819ee8b30707b236baac69bc
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-09-04 11:49:01 +0000

    Unhandled force motion specification - FIXED
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2476 4c39dba3-d726-0410-8099-c98d187771e6

--- src/edp/common/edp_irp6s_postument_track.cc
+++ src/edp/common/edp_irp6s_postument_track.cc
@@ -515,7 +515,6 @@ void irp6s_postument_track_effector::pose_force_torque_at_frame_move(lib::c_buff
 			* base_pos_xyz_rot_xyz_vector;
 			break;
 		default:
-			fprintf(stderr, "Unhandled force motion specification @ %s:%d\n", __FILE__, __LINE__);
 			break;
 		}
 
