2d349877053fde6dc75488814c11e1f5e049c5c4
Jakub Kosiorek <jkosiore@gmail.com>
2009-09-07 10:42:13 +0000

    eihcalibration task update
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2483 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/common/ecp_t_eihcalibration.cc
@@ -0,0 +1,371 @@
+/// \file ecp_t_eihcalibration.cc
+/// \brief definicja zadania kalibracji ukladu oko - reka
+/// \author 2009 Jakub Kosiorek
+///////////////////////////////////////////////////////////////////////////////
+
+#include "ecp/common/ecp_t_eihcalibration.h"
+#include "ecp/irp6_on_track/ecp_r_irp6ot.h"
+#include "ecp/irp6_postument/ecp_r_irp6p.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace common {
+namespace task {
+
+#define A 0.025
+#define C 0.02	//0.15
+#define D 0.02	//0.14
+#define E 0.02	//0.2
+
+//Constructors
+eihcalibration::eihcalibration(lib::configurator &_config): task(_config){
+	smoothgen = NULL;
+	nose = NULL;
+	linear_gen = NULL;
+	generator = NULL;
+}
+
+//Desctructor
+eihcalibration::~eihcalibration(){
+
+}
+
+//methods for ECP template to redefine in concrete classes
+void eihcalibration::task_initialization(void) {
+
+    // Create an adequate robot. - depending on the ini section name.
+    if (strcmp(config.section_name, "[ecp_irp6_on_track]") == 0)
+    {
+        ecp_m_robot = new irp6ot::robot (*this);
+        sr_ecp_msg->message("IRp6ot loaded");
+    }
+    else if (strcmp(config.section_name, "[ecp_irp6_postument]") == 0)
+    {
+        ecp_m_robot = new irp6p::robot(*this);
+        sr_ecp_msg->message("IRp6p loaded");
+    }
+
+	smoothgen = new generator::smooth(*this, true);
+
+	nose = new generator::eih_nose_run(*this, 8);
+	nose->tff_nose_run::configure_pulse_check (true);
+
+	sensor_m[lib::SENSOR_CVFRADIA] = new ecp_mp::sensor::cvfradia(lib::SENSOR_CVFRADIA, "[vsp_cvfradia]", *this, sizeof(lib::sensor_image_t::sensor_union_t::chessboard_t));
+	sensor_m[lib::SENSOR_CVFRADIA]->configure_sensor();
+
+	sr_ecp_msg->message("ECP loaded eihcalibration");
+
+	generator = new generator::eihgenerator(*this);
+	generator->sensor_m = sensor_m;
+}
+
+void eihcalibration::main_task_algorithm(void ){
+
+	sr_ecp_msg->message("ECP eihcalibration ready");
+
+	//Czekam, az czujnik bedzie skonfigurowany.
+	sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+	while(sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.vsp_report == lib::VSP_SENSOR_NOT_CONFIGURED){
+		sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+	}
+
+	smoothgen->set_absolute();
+
+	// wczytanie pozycji poczatkowej i przejscie do niej za pomoca smooth
+	if (smoothgen->load_file_with_path("../trj/eihcalibration/eih_calibration_start1_track.trj")) {
+	  smoothgen->Move();
+	}
+
+	// doprowadzenie chwytaka do szachownicy "wodzeniem za nos"
+	while(sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.found == false){
+		sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+		nose->Move();
+		generator->Move();
+	}
+	nose->Move();
+
+	sr_ecp_msg->message("linear_gen\n");
+
+	//Inicjalizacja td.
+	init_td(lib::XYZ_ANGLE_AXIS, 500);
+
+	int i = 0, j = 0, k, l, m = 0;
+	double a, b, c, d, e;
+	struct timespec delay;
+	delay.tv_nsec = 400000000;//400ms
+	delay.tv_sec = 0;
+
+	set_td_coordinates(0.0, 0.0, -1 * A, 0.0, 0.0, 0.0, 0.0);
+	linear_gen=new common::generator::linear(*this, td, 1);
+	//opusc chwytak az przestanie "widziec" szachownice
+	while(sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.found == true){
+		//opuszczenie chwytaka o 2.5 cm
+		linear_gen->Move();
+		nanosleep(&delay, NULL);
+		sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+		generator->Move();
+		++i;
+	}
+	delete linear_gen;
+
+	// podnies chwytak do ostatniej pozycji w ktorej wykryto szachownice
+	set_td_coordinates(0.0, 0.0, A, 0.0, 0.0, 0.0, 0.0);
+	linear_gen=new common::generator::linear(*this, td, 1);
+	linear_gen->Move();
+	delete linear_gen;
+	nanosleep(&delay, NULL);
+	--i;
+	sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+
+	// zabezpieczenie przed przekroczeniem obszaru roboczego robota
+	bool flaga = true;
+
+	// pomachaj chwytakiem zeby zrobic fajne zdjecia
+	while(i >= 0 && sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.calibrated == false)
+	{
+		if (i % 2 == 1)
+			l = 1;
+		else
+			l = 0;
+
+		for(; l < 6; l += 2)
+		{
+			c = 0.0;
+			d = 0.0;
+			e = 0.0;
+			if(l == 0)
+			{	// obrot
+				c = C;
+			}
+			else if(l == 1)
+			{	// obrot
+				c = -1 * C;
+			}
+			else if(l == 2)
+			{	// obrot
+				d = D;
+			}
+			else if(l == 3)
+			{	// obrot
+				d = D;
+			}
+			else if(l == 4)
+			{	// obrot
+				e = E;
+			}
+			else if(l == 5)
+			{	// obrot
+				e = -1 * E;
+			}
+
+			set_td_coordinates(0.0, 0.0, 0.0, e, c, d, 0.0);
+			linear_gen=new common::generator::linear(*this, td, 1);
+
+			while(((sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.found) == true)
+				&& ((sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.calibrated) == false) && m < 1 )
+			{
+				linear_gen->Move();
+				nanosleep(&delay, NULL);
+				generator->Move();
+				++m;
+				sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+			}
+
+			delete linear_gen;
+
+			if(m != 0)
+			{
+				//powrot do poprzedniej pozycji
+				set_td_coordinates(0.0, 0.0, 0.0, -1.0 * m * e, -1.0 * m * c, -1.0 * m * d, 0.0);
+				linear_gen=new common::generator::linear(*this, td, 1);
+				linear_gen->Move();
+				delete linear_gen;
+				m = 0;
+				nanosleep(&delay, NULL);
+				sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+			}
+		}
+
+		for(k = 0; k < 8; ++k)
+		{
+			a = 0.0;
+			b = 0.0;
+			if(k == 0)
+			{	//przesuniecie w prawo (patrzac na manipulator z przodu)
+				b = A;
+			}
+			else if(k == 1)
+			{	//przesuniecie w lewo (patrzac na manipulator z przodu)
+				b = -1 * A;
+			}
+			else if(k == 2)
+			{//przesuniecie do operatora (patrzac na manipulator z przodu)
+				a = A;
+			}
+			else if(k == 3)
+			{//przesuniecie od operatora (patrzac na manipulator z przodu)
+				a = -1 * A;
+			}
+			else if(k == 4)
+			{	//przesuniecie na skos
+				a = A;
+				b = A;
+			}
+			else if(k == 5)
+			{	//przesuniecie na skos
+				a = -1 * A;
+				b = A;
+			}
+			else if(k == 6)
+			{	//przesuniecie na skos
+				a = A;
+				b = -1 * A;
+			}
+			else if(k == 7)
+			{	//przesuniecie na skos
+				a = -1 * A;
+				b = -1 * A;
+			}
+
+			while(sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.found == true
+				&& sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.calibrated == false && flaga)
+			{
+				set_td_coordinates(a, b, 0.0, 0.0, 0.0, 0.0, 0.0);
+				linear_gen=new common::generator::linear(*this, td, 1);
+				linear_gen->Move();
+				nanosleep(&delay, NULL);
+				generator->Move();
+				++j;
+				delete linear_gen;
+				sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+
+				if (a < 0.0)
+					l = 1;
+				else
+					l = 0;
+
+				for(; l < 6; l += 2)
+				{
+					c = 0.0;
+					d = 0.0;
+					e = 0.0;
+					if(l == 0)
+					{	// obrot
+						c = C;
+					}
+					else if(l == 1)
+					{	// obrot
+						c = -1 * C;
+					}
+					else if(l == 2)
+					{	// obrot
+						d = D;
+					}
+					else if(l == 3)
+					{	// obrot
+						d = D;
+					}
+					else if(l == 4)
+					{	// obrot
+						e = E;
+					}
+					else if(l == 5)
+					{	// obrot
+						e = -1 * E;
+					}
+
+					set_td_coordinates(0.0, 0.0, 0.0, e, c, d, 0.0);
+					linear_gen=new common::generator::linear(*this, td, 1);
+
+					// zabezpieczenie przed przekroczeniem obszaru roboczego robota
+/*start2 b>0 d<0*/					if (a > 0.0 && m == 0 && c > 0 && ((i == 0 && j == 1) || ( i == 1 && j == 1) || (i == 2 && j == 2) || (i == 3 && j == 3)))
+/*start1 a>0 c>0*/						flaga = false;
+
+					while(((sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.found) == true)
+						&& ((sensor_m[lib::SENSOR_CVFRADIA]->from_vsp.comm_image.sensor_union.chessboard.calibrated) == false) && m < 1 && flaga)
+					{
+						linear_gen->Move();
+						nanosleep(&delay, NULL);
+						generator->Move();
+						++m;
+						sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+					}
+
+					delete linear_gen;
+
+					flaga = true;
+
+					if(m != 0)
+					{
+						//powrot do poprzedniej pozycji
+						set_td_coordinates(0.0, 0.0, 0.0, -1.0 * m * e, -1.0 * m * c, -1.0 * m * d, 0.0);
+						linear_gen=new common::generator::linear(*this, td, 1);
+						linear_gen->Move();
+						delete linear_gen;
+						m = 0;
+						nanosleep(&delay, NULL);
+						sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+					}
+				}
+				// zabezpieczenie przed przekroczeniem obszaru roboczego robota
+/*start2 b>0*/				if(a > 0.0 && ((i == 1 && j == 1) || (i == 2 && j == 2) || (i == 3 && j == 3) || (i == 0 && j == 1)))
+/*start1 a>0*/					flaga = false;
+			}
+
+			flaga = true;
+
+			if(j != 0)
+			{
+				//powrot do poprzedniej pozycji
+				set_td_coordinates(-1.0 * j * a, -1.0 * j * b, 0.0, 0.0, 0.0, 0.0, 0.0);
+				linear_gen=new common::generator::linear(*this, td, 1);
+				linear_gen->Move();
+				delete linear_gen;
+				j = 0;
+				nanosleep(&delay, NULL);
+				sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+			}
+		}
+
+		// podnies chwytak o 2.5 cm
+		set_td_coordinates(0.0, 0.0, 0.025, 0.0, 0.0, 0.0, 0.0);
+		linear_gen=new common::generator::linear(*this, td, 1);
+		linear_gen->Move();
+		delete linear_gen;
+		nanosleep(&delay, NULL);
+		sensor_m[lib::SENSOR_CVFRADIA]->get_reading();
+		--i;
+	}
+
+	ecp_termination_notice();
+	//ecp_wait_for_stop();
+}
+
+void eihcalibration::set_td_coordinates(double cor0, double cor1, double cor2, double cor3, double cor4, double cor5, double cor6){
+	// Wspolrzedne kartezjanskie XYZ i katy Eulera ZYZ
+	td.coordinate_delta[0] = cor0; // przyrost wspolrzednej X
+	td.coordinate_delta[1] = cor1; // przyrost wspolrzednej Y
+	td.coordinate_delta[2] = cor2; // przyrost wspolrzednej Z
+	td.coordinate_delta[3] = cor3; // przyrost wspolrzednej FI
+	td.coordinate_delta[4] = cor4; // przyrost wspolrzednej TETA
+	td.coordinate_delta[5] = cor5; // przyrost wspolrzednej PSI
+	td.coordinate_delta[6] = cor6; // przyrost wspolrzednej PSI
+}
+
+//inicjacja struktury td - trajectory description
+void eihcalibration::init_td(lib::POSE_SPECIFICATION pspec, int internode_no){
+	td.arm_type=pspec;
+	td.interpolation_node_no=1;
+	td.internode_step_no=internode_no;	//motion time
+	td.value_in_step_no=internode_no-2;			//motion time-2 ??
+}
+
+task* return_created_ecp_task(lib::configurator &_config){
+	return new eihcalibration(_config);
+}
+
+} // namespace task
+} // namespace common
+} // namespace ecp
+} // namespace mrrocpp
+
---
+++ src/ecp/common/ecp_t_eihcalibration.h
@@ -0,0 +1,59 @@
+/// \file ecp_t_eihcalibration.h
+/// \brief definicja zadania kalibracji ukladu oko - reka
+/// \author 2009 Jakub Kosiorek
+///////////////////////////////////////////////////////////////////////////////
+
+#if !defined(_ECP_T_EIHCALIBRATION_H)
+#define _ECP_T_EIHCALIBRATION_H
+
+#include "lib/typedefs.h"
+#include "lib/impconst.h"
+#include "lib/com_buf.h"
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+
+#include "ecp/common/ecp_task.h"
+#include "ecp/common/ecp_g_smooth.h"
+#include "ecp/common/ecp_generator_t.h"
+#include "ecp_mp/ecp_mp_s_cvfradia.h"
+#include "ecp/common/ecp_g_force.h"
+#include "ecp/common/ecp_g_eihcalibration.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace common {
+namespace task {
+
+class eihcalibration: public common::task::task {
+
+	protected:
+		// generator do wodzenia za nos
+		generator::eih_nose_run* nose;
+		// generator smooth
+		generator::smooth* smoothgen;
+		// generator o zadany przyrost polozenia/orientacji
+		generator::linear* linear_gen;
+		// generator do wysylania danych do fradii
+		generator::eihgenerator* generator;
+		//Trajectory description.
+		lib::trajectory_description td;
+
+	public:
+		eihcalibration(lib::configurator &_config);
+		~eihcalibration();
+		void task_initialization(void);
+		void main_task_algorithm(void);
+
+		//Metods modifing td.
+		void set_td_coordinates(double cor0, double cor1, double cor2, double cor3, double cor4, double cor5, double cor6);
+		void init_td(lib::POSE_SPECIFICATION ps, int internode_no);
+};
+
+}
+} // namespace common
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif
