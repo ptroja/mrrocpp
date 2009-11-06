f6ec8e1bf31bfec0790e956025861d62e31c7b6c
Krzysztof Czajkowski <k.czajkowski.1@gmail.com>
2009-11-06 00:18:43 +0000

    Kwadrat
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2825 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/irp6_on_track/ecp_t_kcz_test.h
@@ -0,0 +1,32 @@
+//Zadanie kt�re realizuje dan� trajektori� u�ywaj�c smooth2 generatora
+
+#if !defined(_ECP_T_KCZ_TEST_H)
+#define _ECP_T_KCZ_TEST_H
+
+#include "ecp/common/ecp_task.h"
+#include "ecp/common/ecp_g_smooth2.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace irp6ot {
+namespace task {
+
+class kcz_test: public common::task::task {
+
+  protected:
+	  common::generator::smooth2* smoothgen2;
+	  double kw_bok;
+
+  public:
+	  kcz_test(lib::configurator &_config);
+
+	  void main_task_algorithm(void);
+};
+
+}
+} // namespace irp6ot
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif
+
