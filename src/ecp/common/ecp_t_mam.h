0a8ce061bdc95b89238dae4a5396a7548bef9db0
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-11-11 21:06:43 +0000

    manual moves task ported to messip
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2883 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/common/ecp_t_mam.h
+++ src/ecp/common/ecp_t_mam.h
@@ -1,28 +1,44 @@
-// -------------------------------------------------------------------------
-//                            ecp_t_mam.h dla QNX6
-// Definicje struktur danych i metod dla procesow ECP
-//
-// Modyfikacje:
-// 1. metody wirtualne w klasie bazowej sensor - ok. 160
-// 2. bonusy do testowania
-//
-// Ostatnia modyfikacja: 25.06.2003
-// autor modyfikacji: tkornuta
-// -------------------------------------------------------------------------
-
 #if !defined(_ECP_T_MAM_H)
 #define _ECP_T_MAM_H
 
 #include "ecp/common/ecp_task.h"
+
+// Generator ruchu.
+#include "ecp/common/ecp_g_mam.h"
 
 namespace mrrocpp {
 namespace ecp {
 namespace common {
 namespace task {
 
-class mam: public common::task::task  {
+class mam : public common::task::task
+{
+private:
+	void show_mam_window
+#if !defined(USE_MESSIP_SRR)
+		(int UI_fd);
+#else
+		(messip_channel_t *UI_fd);
+#endif
+
+	void * UI_communication_thread(void* arg);
 
 public:
+	// Kanal komunikacyjny z procesem UI.
+#if !defined(USE_MESSIP_SRR)
+	name_attach_t * UI_ECP_attach;
+#else
+	messip_channel_t * UI_ECP_attach;
+#endif
+
+	// Obiekt generatora trajektorii.
+	generator::manual_moves_automatic_measures *mam_gen;
+
+	// Flaga uzywana do informowania o koncu pracy.
+	bool TERMINATE;
+	// Flaga uzywana do zatrzymywania/uruchamiania zbierania pomiarow.
+	bool START_MEASURES;
+
 	// KONSTRUKTORY
 	mam(lib::configurator &_config);
 
