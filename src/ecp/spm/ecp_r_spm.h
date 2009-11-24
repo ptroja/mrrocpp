1cb765240c2c2589c7a2d6c1b17da4baf8824f6b
Tomek Winiarski <tomrobotics@gmail.com>
2009-11-24 07:07:43 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2915 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/spm/ecp_r_spm.h
@@ -0,0 +1,43 @@
+// -------------------------------------------------------------------------
+//                            ecp_local.h dla QNX6
+// Definicje struktur danych i metod dla procesow ECP
+// robot - irp6_mechatronika
+//
+// -------------------------------------------------------------------------
+
+#if !defined(_ECP_R_SPM_H)
+#define _ECP_R_SPM_H
+
+#include "ecp/common/ecp_robot.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace spm {
+
+// ---------------------------------------------------------------
+class robot: public common::ecp_robot
+{
+	// Klasa dla robota irp6_postument (sztywnego)
+
+	public:
+	robot (lib::configurator &_config, lib::sr_ecp &_sr_ecp);
+	robot (common::task::task& _ecp_object);
+
+	virtual void create_command (void);
+	// wypelnia bufor wysylkowy do EDP na podstawie danych zawartych w obrazie
+	// robota wykorzystywanych przez generator
+	// Ten bufor znajduje sie w robocie
+
+	void get_reply (void);
+	virtual void get_input_reply (void);
+	virtual void get_arm_reply (void);
+	virtual void get_rmodel_reply (void);
+
+}; // end: class ecp_irp6_mechatronika_robot
+// ---------------------------------------------------------------
+
+} // namespace spm
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif
