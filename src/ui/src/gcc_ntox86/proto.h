e4f782c9fd053bcb8ca474d3fbf6ddc897f39920
Tomek Winiarski <tomrobotics@gmail.com>
2009-12-03 11:27:14 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@2981 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ui/src/gcc_ntox86/proto.h
+++ src/ui/src/gcc_ntox86/proto.h
@@ -103,10 +103,13 @@ int wind_conveyor_moves_init ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbac
 int conveyor_move_to_preset_position ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int wind_conveyor_moves_move ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int EDP_conveyor_synchronise ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
+int EDP_conveyor_synchronise_int ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int init_wnd_conveyor_servo_algorithm ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int conv_servo_algorithm_set ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int EDP_conveyor_create ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
+int EDP_conveyor_create_int ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int EDP_conveyor_slay ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
+int EDP_conveyor_slay_int ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 int pulse_reader_conv_start ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
 bool pulse_reader_conv_start_exec_pulse ( void );
 int pulse_reader_conv_stop ( PtWidget_t *widget , ApInfo_t *apinfo , PtCallbackInfo_t *cbinfo );
