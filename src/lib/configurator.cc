9a9bf6e910081438b8c3299763354af224068fd8
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-12-09 15:18:43 +0000

    setsid() removed from process_spawn
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3049 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/configurator.cc
+++ src/lib/configurator.cc
@@ -408,9 +408,11 @@ pid_t configurator::process_spawn(const std::string & _section_name) {
 		);
 
 		// create new session for separation of signal delivery
+		/*
 		if(setsid() == (pid_t) -1) {
 			perror("setsid()");
 		}
+		*/
 
 		if (exists("username", _section_name)) {
 			std::string username = return_string_value("username", _section_name);
