b62fca161ed5853565fcc6676bffe0a81938c291
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-12-10 23:49:53 +0000

    headers cleanup
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3079 4c39dba3-d726-0410-8099-c98d187771e6

--- src/vsp/common/vsp_sensor.cc
+++ src/vsp/common/vsp_sensor.cc
@@ -12,20 +12,6 @@
 // Data:		30.11.2006
 // ------------------------------------------------------------------------
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/neutrino.h>
-#include <sys/sched.h>
-#include <sys/iofunc.h>
-#include <sys/dispatch.h>
-#include <errno.h>
-
-
 #include "lib/srlib.h"
 
 #include "vsp/vsp_sensor.h"				// zawiera deklaracje klasy vsp_sensor + struktury komunikacyjne
