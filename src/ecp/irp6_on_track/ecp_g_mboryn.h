5d1c538603137d2801791e3f2628a103191fdf04
Mateusz Borys <M.Boryn@stud.elka.pw.edu.pl>
2009-12-11 13:06:46 +0000

    task i generator testowy
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3083 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/irp6_on_track/ecp_g_mboryn.h
@@ -0,0 +1,37 @@
+/*
+ * ecp_g_mboryn.h
+ *
+ *  Created on: Dec 11, 2009
+ *      Author: mboryn
+ */
+
+#ifndef ECP_G_MBORYN_H_
+#define ECP_G_MBORYN_H_
+
+#include "ecp/common/ecp_generator.h"
+
+namespace mrrocpp {
+
+namespace ecp {
+
+namespace irp6ot {
+
+namespace generator {
+
+class ecp_g_mboryn: public mrrocpp::ecp::common::generator::generator {
+public:
+	ecp_g_mboryn(common::task::task & _ecp_task);
+	virtual ~ecp_g_mboryn();
+	virtual bool first_step();
+	virtual bool next_step();
+};
+
+} // namespace generator
+
+} // namespace irp6ot
+
+} // namespace ecp
+
+} // namespace mrrocpp
+
+#endif /* ECP_G_MBORYN_H_ */
