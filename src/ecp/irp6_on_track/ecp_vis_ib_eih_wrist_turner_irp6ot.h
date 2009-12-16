a1bbd44a8408a329b9a12ed23775eee7ef9f91ed
Piotr Trojanek <piotr.trojanek@gmail.com>
2009-12-16 11:50:48 +0000

    svn:executable property removed from source files
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3189 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/ecp/irp6_on_track/ecp_vis_ib_eih_wrist_turner_irp6ot.h
@@ -0,0 +1,50 @@
+/*
+ * ecp_vis_ib_eih_wrist_turner_irp6ot.h
+ *
+ *  Created on: DEC 10, 2009
+ *      Author: rtulwin
+ */
+
+#ifndef ECP_VIS_IB_EIH_WRIST_TURNER_IRP6OT_H_
+#define ECP_VIS_IB_EIH_WRIST_TURNER_IRP6OT_H_
+
+#include "ecp/common/ecp_visual_servo.h"
+
+#include <string.h>
+#include <math.h>
+
+#include "lib/impconst.h"
+#include "lib/com_buf.h"
+
+#include "ecp_mp/sensor/ecp_mp_s_cvfradia.h"
+#include "ecp/common/task/ecp_t_cvfradia.h"
+
+#include <iostream>
+
+namespace mrrocpp {
+namespace ecp {
+namespace irp6ot {
+
+#define MOTION_STEPS 25
+
+class ecp_vis_ib_eih_wrist_turner_irp6ot: public common::ecp_visual_servo {
+
+public:
+
+	lib::sensor *vsp_fradia; //wirtualny sensor
+    double next_position[MAX_SERVOS_NR]; 	//pozycja w nastepnym kroku.
+    bool tracking; //jesli true, obiekt jest sledzony, jesli false, zagubiony (przychodzi z fradii)
+    bool reached; //jesli true, pozycja nadgarstka jest odpowiednia do chwycenia (przychodzi z fradii)
+
+	ecp_vis_ib_eih_wrist_turner_irp6ot(common::task::task& _ecp_task);
+	virtual bool first_step(void);
+	virtual bool next_step_without_constraints();
+	virtual void entertain_constraints();
+
+};
+
+} // namespace irp6ot
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif /* ECP_VIS_IB_EIH_WRIST_TURNER_IRP6OT_H_ */
