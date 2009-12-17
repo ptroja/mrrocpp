08c40d3e6dfc1be7ae97aa7905d706661a505023
Tomek Winiarski <tomrobotics@gmail.com>
2009-12-17 23:41:25 +0000

    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3239 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/lib/mathtr/mrmath.h
@@ -0,0 +1,19 @@
+// ------------------------------------------------------------------------
+// Proces:		-
+// Plik:			mathtr.h
+// System:	QNX/MRROC++  v. 6.3
+// Opis:		Klasy K_vector, Homog_matrix, Ft_v_vector,  Jacobian_matrix
+//				- definicja klas
+//
+// Autor:		tkornuta
+// Data:		14.02.2007
+// ------------------------------------------------------------------------
+
+
+#ifndef __MRMATH_H
+#define __MRMATH_H
+
+#include "lib/mathtr/mathtr.h"
+#include <lib/mathtr/ForceTrans.h>
+
+#endif
