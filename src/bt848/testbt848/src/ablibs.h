185b9a1225d04aa7459c09d892db11aa97938c11
Tomek Winiarski <tomrobotics@gmail.com>
2009-12-18 17:34:39 +0000

    some tasks removed
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3261 4c39dba3-d726-0410-8099-c98d187771e6

--- src/bt848/testbt848/src/ablibs.h
+++
@@ -1,4 +0,0 @@
-/* Libs header for application - AppBuilder 2.03  */
-
-#include <Pt.h>
-#include <Ap.h>
--- src/bt848/testbt848/src/abwidgets.h
+++
@@ -1,4 +0,0 @@
-/* Widget header for application - AppBuilder 2.03  */
-
-ApWidget_t AbWidgets[ 3 ];
-
