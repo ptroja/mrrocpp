d28077e46974fef1033592b23bb1b28834c50338
Tomek Winiarski <tomrobotics@gmail.com>
2009-12-19 14:34:53 +0000

    edp conveyor irp6m servo_buffer fix
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3284 4c39dba3-d726-0410-8099-c98d187771e6

--- src/edp/conveyor/sg_conv.cc
+++ src/edp/conveyor/sg_conv.cc
@@ -48,7 +48,7 @@ uint8_t servo_buffer::Move_a_step (void)
 /*-----------------------------------------------------------------------*/
 servo_buffer::servo_buffer (effector &_master) : common::servo_buffer(_master), master(_master)
 {
-
+	thread_id = new boost::thread(boost::bind(&servo_buffer::operator(), this));
 }
 
 /*-----------------------------------------------------------------------*/
@@ -136,6 +136,8 @@ servo_buffer::~servo_buffer(void)
 	}
 
 	delete hi;
+
+	delete thread_id;
 }
 /*-----------------------------------------------------------------------*/
 
