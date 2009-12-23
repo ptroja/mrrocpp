c96d38808a90dfaacaa6723a257cfe3e653296e5
Krzysztof Czajkowski <k.czajkowski.1@gmail.com>
2009-12-23 17:49:33 +0000

    PCbird + IRp6 data acquisition dev
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3336 4c39dba3-d726-0410-8099-c98d187771e6

--- src/application/calibration/ecp_t_kcz_test.cc
+++ src/application/calibration/ecp_t_kcz_test.cc
@@ -39,14 +39,91 @@ void kcz_test::main_task_algorithm(void ) {
 
 	smoothgen2->set_absolute();
 
-	smoothgen2->load_coordinates(lib::ECP_XYZ_ANGLE_AXIS, -0.289, 1.08, -0.2, 0.762*3.11, 0.648*3.11, -0.019*3.11, 0.074, 0.000, false);
+	smoothgen2->load_coordinates(lib::ECP_XYZ_ANGLE_AXIS, -0.150, 1.12, -0.1, 0.729*3.14, 0.685*3.14, -0.001*3.14, 0.09, 0.000, true);
 	smoothgen2->Move();
+	smoothgen2->reset();
+	smoothgen2->load_coordinates(lib::ECP_XYZ_ANGLE_AXIS,
+						sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x - 0.421,
+						-1.0 * sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y + 1.119,
+						-1.0 * sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z - 0.22 + 0.20,
+						0.729*3.14,
+						0.685*3.14,
+						-0.001*3.14,
+						0.09, 0.000, true);
+	smoothgen2->Move();
+	smoothgen2->reset();
+
+	smoothgen2->set_relative();
 
 	double vv[MAX_SERVOS_NR]={0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3};
-	double aa[MAX_SERVOS_NR]={1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+	double aa[MAX_SERVOS_NR]={0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
 
+	float lastx = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x;
+	float lasty = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y;
+	float lastz = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z;
+
+	float Zang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.a * M_PI / 180;
+	float Yang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.b * M_PI / 180;
+	float Xang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.g * M_PI / 180;
+	double lastrot[3][3];
+	lastrot[0][0] = cos(Yang)*cos(Zang);
+	lastrot[0][1] = cos(Yang)*sin(Zang);
+	lastrot[0][2] = -sin(Yang);
+	lastrot[1][0] = -cos(Xang)*sin(Zang)+sin(Xang)*sin(Yang)*cos(Zang);
+	lastrot[1][1] = cos(Xang)*cos(Zang)+sin(Xang)*sin(Yang)*sin(Zang);
+	lastrot[1][2] = sin(Xang)*cos(Yang);
+	lastrot[2][0] = sin(Xang)*sin(Zang)+cos(Xang)*sin(Yang)*cos(Zang);
+	lastrot[2][1] = -sin(Xang)*cos(Zang)+cos(Xang)*sin(Yang)*sin(Zang);
+	lastrot[2][2] = cos(Xang)*cos(Yang);
+	lib::Homog_matrix matrix;
+	double temp1[6];
+	matrix.set_rotation_matrix(lastrot);
+	matrix.get_xyz_angle_axis(temp1);
+	float lasta = temp1[3];
+	float lastb = temp1[4];
+	float lastc = temp1[5];
+
+	float movex, movey, movez, movea, moveb, movec;
+	float delta = 0.001;
 	do {
-	smoothgen2->load_coordinates(lib::ECP_XYZ_ANGLE_AXIS, vv, aa, sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x - 0.315, -1.0 * sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y + 1.216, -1.0 * sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z - 0.081 + 0.1, 0.762*3.11, 0.648*3.11, -0.019*3.11, 0.074, 0.000, false);
+    movex = abs(sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y - lasty) > delta ? sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y - lasty : 0;
+    movey = abs(sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x - lastx) > delta ? sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x - lastx : 0;
+    movez = abs(sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z - lastz) > delta ? sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z - lastz : 0;
+
+	Zang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.a * M_PI / 180;
+	Yang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.b * M_PI / 180;
+	Xang = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.g * M_PI / 180;
+	lastrot[0][0] = cos(Yang)*cos(Zang);
+	lastrot[0][1] = cos(Yang)*sin(Zang);
+	lastrot[0][2] = -sin(Yang);
+	lastrot[1][0] = -cos(Xang)*sin(Zang)+sin(Xang)*sin(Yang)*cos(Zang);
+	lastrot[1][1] = cos(Xang)*cos(Zang)+sin(Xang)*sin(Yang)*sin(Zang);
+	lastrot[1][2] = sin(Xang)*cos(Yang);
+	lastrot[2][0] = sin(Xang)*sin(Zang)+cos(Xang)*sin(Yang)*cos(Zang);
+	lastrot[2][1] = -sin(Xang)*cos(Zang)+cos(Xang)*sin(Yang)*sin(Zang);
+	lastrot[2][2] = cos(Xang)*cos(Yang);
+	lib::Homog_matrix matrix;
+	matrix.set_rotation_matrix(lastrot);
+	matrix.get_xyz_angle_axis(temp1);
+
+    movea = abs(temp1[3] - lasta) > delta ? temp1[3] - lasta : 0;
+    moveb = abs(temp1[4] - lastb) > delta ? temp1[4] - lastb : 0;
+    movec = abs(temp1[5] - lastc) > delta ? temp1[5] - lastc : 0;
+
+    smoothgen2->load_coordinates(lib::ECP_XYZ_ANGLE_AXIS, vv, aa,
+															-1.0 * movex,
+															1.0 * movey,
+															1.0 * movez,
+															1.0 * moveb,
+															-1.0 * movea,
+															-1.0 * movec,
+															0.0, 0.0, true);
+	lastx = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.x;
+	lasty = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.y;
+	lastz = sensor_m[lib::SENSOR_PCBIRD]->image.sensor_union.pcbird.z;
+	lasta = temp1[3];
+	lastb = temp1[4];
+	lastc = temp1[5];
 	smoothgen2->Move();
 	} while(true);
 
