3a2f0000f919549d52219ab50aeb3e3f593d6e44
Tomek Winiarski <tomrobotics@gmail.com>
2009-12-23 22:02:47 +0000

    temporary commit not compiling
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3338 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/mrmath/ft_v_vector.cc
+++ src/lib/mrmath/ft_v_vector.cc
@@ -48,79 +48,6 @@ Ft_v_vector::Ft_v_vector(double fx, double fy, double fz, double tx, double ty, 
 
 }// end Ft_v_vector::Ft_v_vector(double t[6])
 
-/*
-Ft_v_vector::Ft_v_vector(const K_vector force, const K_vector torque)
-{
-	for(int i=0; i<3; i++)
-	{
-		w[i] = force[i];
-		w[i+3] = torque[i];
-	}
-}
-*/
-
-
-/* ------------------------------------------------------------------------
-Wyznaczenie uchybu pozycji tzn. r�znicy pozycji aktualnej i zadanej
-
-  Wejscie:
-  * local_current_end_effector_frame - macierz przeksztacenia jednorodnego (MPJ)
-		opisujca aktualne poloenie i orientacje koncowki (narzedzia) w ukladzie bazowym.
-  * local_desired_end_effector_frame - macierz przeksztacenia jednorodnego (MPJ)
-		opisujca zadane poloenie i orientacje koncowki (narzedzia) w ukladzie bazowym.
-
-  Wyjscie:
-  * desired_distance - 6-elementowy wektor opisujacy uchyb. 3 pierwsze elementy odpowiadaj�
-		przesuni�ciu liniowym, a kolejne 3 uchybowi rotacji
-
-Sibi
- ------------------------------------------------------------------------ */
-
-void Ft_v_vector::position_distance(frame_tab* local_current_end_effector_frame, frame_tab* local_desired_end_effector_frame)
-{
-
-double n_t[3], n_d[3], o_t[3], o_d[3], a_t[3], a_d[3];
-
-//Wyliczenie wektora przesuniecia : w - predkosc katowa
-// n, o, a - wektory normalny, orientacji i zbli�enia
-
-n_t[0]=(*local_current_end_effector_frame)[0][0];
-n_t[1]=(*local_current_end_effector_frame)[1][0];
-n_t[2]=(*local_current_end_effector_frame)[2][0];
-
-n_d[0]=(*local_desired_end_effector_frame)[0][0];
-n_d[1]=(*local_desired_end_effector_frame)[1][0];
-n_d[2]=(*local_desired_end_effector_frame)[2][0];
-
-o_t[0]=(*local_current_end_effector_frame)[0][1];
-o_t[1]=(*local_current_end_effector_frame)[1][1];
-o_t[2]=(*local_current_end_effector_frame)[2][1];
-
-o_d[0]=(*local_desired_end_effector_frame)[0][1];
-o_d[1]=(*local_desired_end_effector_frame)[1][1];
-o_d[2]=(*local_desired_end_effector_frame)[2][1];
-
-a_t[0]=(*local_current_end_effector_frame)[0][2];
-a_t[1]=(*local_current_end_effector_frame)[1][2];
-a_t[2]=(*local_current_end_effector_frame)[2][2];
-
-a_d[0]=(*local_desired_end_effector_frame)[0][2];
-a_d[1]=(*local_desired_end_effector_frame)[1][2];
-a_d[2]=(*local_desired_end_effector_frame)[2][2];
-
-//Wyliczenie wektora przesuniecia : v - predkosc obrotowa
-
-w[0]=(0.5)*((n_t[1]*n_d[2]-n_t[2]*n_d[1])+(o_t[1]*o_d[2]-o_t[2]*o_d[1])+(a_t[1]*a_d[2]-a_t[2]*a_d[1]));
-w[1]=(0.5)*((n_t[2]*n_d[0]-n_t[0]*n_d[2])+(o_t[2]*o_d[0]-o_t[0]*o_d[2])+(a_t[2]*a_d[0]-a_t[0]*a_d[2]));
-w[2]=(0.5)*((n_t[0]*n_d[1]-n_t[1]*n_d[0])+(o_t[0]*o_d[1]-o_t[1]*o_d[0])+(a_t[0]*a_d[1]-a_t[1]*a_d[0]));
-
-//Wyliczenie wektora przesuniecia : v - predkosc liniowa
-
-w[3]=(*local_desired_end_effector_frame)[0][3]-(*local_current_end_effector_frame)[0][3];
-w[4]=(*local_desired_end_effector_frame)[1][3]-(*local_current_end_effector_frame)[1][3];
-w[5]=(*local_desired_end_effector_frame)[2][3]-(*local_current_end_effector_frame)[2][3];
-}// end Ft_v_vector::position_distance(frame_tab*, frame_tab* )
-
 
 Ft_v_vector::Ft_v_vector(const Ft_v_vector & wzor)
 {
@@ -187,75 +114,6 @@ void Ft_v_vector::wypisz_wartosc_na_konsole() const
 
 
 
-Ft_v_vector & Ft_v_vector::operator=(const Ft_v_vector & p)
-{
-	// operator przypisania
-	// parametry wektora przyjmuja wartosci takie jak parametry wektora podanego jako argument
-
-	if(this == &p) return *this;
-
-	set_values(p.w);
-
-return *this;
-}// end Ft_v_vector::operator=(const Ft_v_vector & p)
-
-Ft_v_vector Ft_v_vector::operator+(const Ft_v_vector & dod) const
-{
-	// operator dodawania
-	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
-
-	Ft_v_vector zwracany;
-
-	for(int i=0; i<6; i++)
-		zwracany.w[i] = w[i] + dod.w[i];
-
-return zwracany;
-
-}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
-
-
-Ft_v_vector Ft_v_vector::operator-(const Ft_v_vector & dod) const
-{
-	// operator dodawania
-	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
-
-	Ft_v_vector zwracany;
-
-	for(int i=0; i<6; i++)
-		zwracany.w[i] = w[i] - dod.w[i];
-
-return zwracany;
-
-}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
-
-
-
-Ft_v_vector Ft_v_vector::operator*(const double skalar) const
-{
-	Ft_v_vector zwracany;
-	for(int i=0;i<6;i++) zwracany.w[i]=w[i]*skalar;
-	return zwracany;
-}
-
-
-Ft_v_vector Ft_v_vector::operator!() const
-{
-	// odwrocenie wektora
-
-	Ft_v_vector zwracany;
-	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
-	return zwracany;
-}
-
-
-Ft_v_vector Ft_v_vector::operator-() const
-{
-	// odwrocenie wektora
-
-	Ft_v_vector zwracany;
-	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
-	return zwracany;
-}
 
 
 void Ft_v_vector::operator+=(const Ft_v_vector & dod)
@@ -301,6 +159,260 @@ double Ft_v_vector::max_element()
 
   return MAX;
 }// end Ft_v_vector::max_element()
+
+
+/////////////////////////////////////////
+//
+//  Ft_vector
+//
+//////////////////////////////////////////
+
+
+
+Ft_vector::Ft_vector() : Ft_v_vector()
+{}
+
+Ft_vector::Ft_vector(const double t[6]) : Ft_v_vector(t)
+{}
+
+Ft_vector::Ft_vector(double fx, double fy, double fz, double tx, double ty, double tz)
+: Ft_v_vector(fx, fy, fz, tx, ty, tz)
+{}
+
+Ft_vector::Ft_vector(const Ft_vector & wzor) : Ft_v_vector(wzor)
+{}
+
+Ft_vector & Ft_vector::operator=(const Ft_vector & p)
+{
+	// operator przypisania
+	// parametry wektora przyjmuja wartosci takie jak parametry wektora podanego jako argument
+
+	if(this == &p) return *this;
+
+	set_values(p.w);
+
+return *this;
+}// end Ft_v_vector::operator=(const Ft_v_vector & p)
+
+Ft_vector Ft_vector::operator+(const Ft_vector & dod) const
+{
+	// operator dodawania
+	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
+
+	Ft_vector zwracany;
+
+	for(int i=0; i<6; i++)
+		zwracany.w[i] = w[i] + dod.w[i];
+
+return zwracany;
+
+}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
+
+
+Ft_vector Ft_vector::operator-(const Ft_vector & dod) const
+{
+	// operator dodawania
+	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
+
+	Ft_vector zwracany;
+
+	for(int i=0; i<6; i++)
+		zwracany.w[i] = w[i] - dod.w[i];
+
+return zwracany;
+
+}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
+
+
+
+Ft_vector Ft_vector::operator*(const double skalar) const
+{
+	Ft_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=w[i]*skalar;
+	return zwracany;
+}
+
+
+Ft_vector Ft_vector::operator!() const
+{
+	// odwrocenie wektora
+
+	Ft_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
+	return zwracany;
+}
+
+
+Ft_vector Ft_vector::operator-() const
+{
+	// odwrocenie wektora
+
+	Ft_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
+	return zwracany;
+}
+
+
+
+
+/////////////////////////////////////////
+//
+//  V_vector
+//
+//////////////////////////////////////////
+
+
+/* ------------------------------------------------------------------------
+Wyznaczenie uchybu pozycji tzn. r�znicy pozycji aktualnej i zadanej
+
+  Wejscie:
+  * local_current_end_effector_frame - macierz przeksztacenia jednorodnego (MPJ)
+		opisujca aktualne poloenie i orientacje koncowki (narzedzia) w ukladzie bazowym.
+  * local_desired_end_effector_frame - macierz przeksztacenia jednorodnego (MPJ)
+		opisujca zadane poloenie i orientacje koncowki (narzedzia) w ukladzie bazowym.
+
+  Wyjscie:
+  * desired_distance - 6-elementowy wektor opisujacy uchyb. 3 pierwsze elementy odpowiadaj�
+		przesuni�ciu liniowym, a kolejne 3 uchybowi rotacji
+
+Sibi
+ ------------------------------------------------------------------------ */
+
+
+V_vector::V_vector() : Ft_v_vector()
+{}
+
+V_vector::V_vector(const double t[6]) : Ft_v_vector(t)
+{}
+
+V_vector::V_vector(double fx, double fy, double fz, double tx, double ty, double tz)
+: Ft_v_vector(fx, fy, fz, tx, ty, tz)
+{}
+
+V_vector::V_vector(const V_vector & wzor) : Ft_v_vector(wzor)
+{}
+
+V_vector & V_vector::operator=(const V_vector & p)
+{
+	// operator przypisania
+	// parametry wektora przyjmuja wartosci takie jak parametry wektora podanego jako argument
+
+	if(this == &p) return *this;
+
+	set_values(p.w);
+
+return *this;
+}// end Ft_v_vector::operator=(const Ft_v_vector & p)
+
+V_vector V_vector::operator+(const V_vector & dod) const
+{
+	// operator dodawania
+	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
+
+	V_vector zwracany;
+
+	for(int i=0; i<6; i++)
+		zwracany.w[i] = w[i] + dod.w[i];
+
+return zwracany;
+
+}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
+
+
+V_vector V_vector::operator-(const V_vector & dod) const
+{
+	// operator dodawania
+	// umozliwia zmodyfikowanie parametrow wersora zgodnie z zawartoscia argumentu
+
+	V_vector zwracany;
+
+	for(int i=0; i<6; i++)
+		zwracany.w[i] = w[i] - dod.w[i];
+
+return zwracany;
+
+}// end Ft_v_vector::operator+(const Ft_v_vector & dod) const
+
+
+
+V_vector V_vector::operator*(const double skalar) const
+{
+	V_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=w[i]*skalar;
+	return zwracany;
+}
+
+
+V_vector V_vector::operator!() const
+{
+	// odwrocenie wektora
+
+	V_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
+	return zwracany;
+}
+
+
+V_vector V_vector::operator-() const
+{
+	// odwrocenie wektora
+
+	V_vector zwracany;
+	for(int i=0;i<6;i++) zwracany.w[i]=-w[i];
+	return zwracany;
+}
+
+
+
+
+
+
+void V_vector::position_distance(frame_tab* local_current_end_effector_frame, frame_tab* local_desired_end_effector_frame)
+{
+
+double n_t[3], n_d[3], o_t[3], o_d[3], a_t[3], a_d[3];
+
+//Wyliczenie wektora przesuniecia : w - predkosc katowa
+// n, o, a - wektory normalny, orientacji i zbli�enia
+
+n_t[0]=(*local_current_end_effector_frame)[0][0];
+n_t[1]=(*local_current_end_effector_frame)[1][0];
+n_t[2]=(*local_current_end_effector_frame)[2][0];
+
+n_d[0]=(*local_desired_end_effector_frame)[0][0];
+n_d[1]=(*local_desired_end_effector_frame)[1][0];
+n_d[2]=(*local_desired_end_effector_frame)[2][0];
+
+o_t[0]=(*local_current_end_effector_frame)[0][1];
+o_t[1]=(*local_current_end_effector_frame)[1][1];
+o_t[2]=(*local_current_end_effector_frame)[2][1];
+
+o_d[0]=(*local_desired_end_effector_frame)[0][1];
+o_d[1]=(*local_desired_end_effector_frame)[1][1];
+o_d[2]=(*local_desired_end_effector_frame)[2][1];
+
+a_t[0]=(*local_current_end_effector_frame)[0][2];
+a_t[1]=(*local_current_end_effector_frame)[1][2];
+a_t[2]=(*local_current_end_effector_frame)[2][2];
+
+a_d[0]=(*local_desired_end_effector_frame)[0][2];
+a_d[1]=(*local_desired_end_effector_frame)[1][2];
+a_d[2]=(*local_desired_end_effector_frame)[2][2];
+
+//Wyliczenie wektora przesuniecia : v - predkosc obrotowa
+
+w[0]=(0.5)*((n_t[1]*n_d[2]-n_t[2]*n_d[1])+(o_t[1]*o_d[2]-o_t[2]*o_d[1])+(a_t[1]*a_d[2]-a_t[2]*a_d[1]));
+w[1]=(0.5)*((n_t[2]*n_d[0]-n_t[0]*n_d[2])+(o_t[2]*o_d[0]-o_t[0]*o_d[2])+(a_t[2]*a_d[0]-a_t[0]*a_d[2]));
+w[2]=(0.5)*((n_t[0]*n_d[1]-n_t[1]*n_d[0])+(o_t[0]*o_d[1]-o_t[1]*o_d[0])+(a_t[0]*a_d[1]-a_t[1]*a_d[0]));
+
+//Wyliczenie wektora przesuniecia : v - predkosc liniowa
+
+w[3]=(*local_desired_end_effector_frame)[0][3]-(*local_current_end_effector_frame)[0][3];
+w[4]=(*local_desired_end_effector_frame)[1][3]-(*local_current_end_effector_frame)[1][3];
+w[5]=(*local_desired_end_effector_frame)[2][3]-(*local_current_end_effector_frame)[2][3];
+}// end V_vector::position_distance(frame_tab*, frame_tab* )
+
+
 
 } // namespace lib
 } // namespace mrrocpp
--- src/lib/mrmath/ft_v_vector.h
+++ src/lib/mrmath/ft_v_vector.h
@@ -27,26 +27,21 @@ namespace lib {
 // klasa reprezentujaca wektor sila-moment i wektora predkosci
 class Ft_v_vector
 {
-private:
+protected:
 	double w[6];
 public:
 	friend class Ft_v_tr;						// klasa Ft_v_tr musi miec dostep do prywatnych
-	friend class Ft_tr;
-	friend class V_tr;
-												// skladnikow klasy Ft_v_vector
-     friend class Jacobian_matrix;			//Klasa Jacobian_matrix ma miec dostep do skladowych - Sibi
+
 
 	Ft_v_vector();													// konstruktor domniemany [0, 0, 0, 0, 0, 0]
 	Ft_v_vector(const double t[6]);										// utworzenie wektora na podstawie podanej tablicy
 	Ft_v_vector(double fx, double fy, double fz, double tx, double ty, double tz);
 
-	//Ft_v_vector(const K_vector force, const K_vector torque);
 
 	Ft_v_vector(const Ft_v_vector &);								// konstruktor kopiujacy
 
 	void set_values(const double t[6]);										// wypelnienie wektora na podstawie podanej tablicy
 	void set_values(double fx, double fy, double fz, double tx, double ty, double tz);
-
 
 	// Ustawienie elementu wektora.
 	void set_value(int i, const double value);
@@ -54,10 +49,6 @@ public:
 	void get_value(int i, double &value) const;
 	// Zwrocenie elementu wektora.
 	double get_value(int i) const;
-
-	//Sibi
-	 //Wektor predkosci jako odleglosc dwuch pozycji zadanych w postaci ramek
-	void position_distance(frame_tab* local_current_end_effector_frame, frame_tab* local_desired_end_effector_frame);
 
 	// Wyspisanie na ekran wektora
 	void wypisz_wartosc_na_konsole() const;
@@ -67,15 +58,7 @@ public:
       // in theory, the LHS operator
     double& operator[](const int i);
 
-	// Odwracanie macierzy.
-	Ft_v_vector operator!() const;
-	Ft_v_vector operator-() const;
-	Ft_v_vector & operator=(const Ft_v_vector &);			// operator przypisania
-	Ft_v_vector operator+(const Ft_v_vector &) const;
-	Ft_v_vector operator-(const Ft_v_vector &) const;
-	Ft_v_vector operator*(double) const;					// skalowanie wektora
 	void operator+=(const Ft_v_vector &);
-
 
 	void to_table(double tablica[6]) const;					// przepisanie wektora do tablicy podanej jako argument
 
@@ -85,72 +68,79 @@ public:
 	//Wyciagniecie max elementu z wektora
 	double max_element ();	//wyciagniecie maksymalnego elementu wektora
 
+
+	// Odwracanie macierzy.
+	Ft_v_vector operator!() const;
+	Ft_v_vector operator-() const;
+	Ft_v_vector & operator=(const Ft_v_vector &);			// operator przypisania
+	Ft_v_vector operator+(const Ft_v_vector &) const;
+	Ft_v_vector operator-(const Ft_v_vector &) const;
+	Ft_v_vector operator*(double) const;					// skalowanie wektora
+
 };// end class Ft_v_vector
 
 
 
-/*
 // klasa reprezentujaca wektor sila-moment i wektora predkosci
-class Ft_v_vector
+class Ft_vector : public Ft_v_vector
 {
-private:
-	double w[6];
+
 public:
-	friend class Ft_v_tr;						// klasa Ft_v_tr musi miec dostep do prywatnych
 	friend class Ft_tr;
+
+												// skladnikow klasy Ft_v_vector
+
+	Ft_vector();													// konstruktor domniemany [0, 0, 0, 0, 0, 0]
+	Ft_vector(const double t[6]);										// utworzenie wektora na podstawie podanej tablicy
+	Ft_vector(double fx, double fy, double fz, double tx, double ty, double tz);
+
+	Ft_vector(const Ft_vector &);								// konstruktor kopiujacy
+
+	// Odwracanie macierzy.
+	Ft_vector operator!() const;
+	Ft_vector operator-() const;
+	Ft_vector & operator=(const Ft_vector &);			// operator przypisania
+	Ft_vector operator+(const Ft_vector &) const;
+	Ft_vector operator-(const Ft_vector &) const;
+	Ft_vector operator*(double) const;					// skalowanie wektora
+};// end class Ft_vector
+
+
+// klasa reprezentujaca wektor sila-moment i wektora predkosci
+class V_vector : public Ft_v_vector
+{
+
+public:
+
 	friend class V_tr;
 												// skladnikow klasy Ft_v_vector
      friend class Jacobian_matrix;			//Klasa Jacobian_matrix ma miec dostep do skladowych - Sibi
 
-	Ft_v_vector();													// konstruktor domniemany [0, 0, 0, 0, 0, 0]
-	Ft_v_vector(const double t[6]);										// utworzenie wektora na podstawie podanej tablicy
-	Ft_v_vector(double fx, double fy, double fz, double tx, double ty, double tz);
+											// skladnikow klasy Ft_v_vector
 
-	Ft_v_vector(const Ft_v_vector &);								// konstruktor kopiujacy
+	V_vector();													// konstruktor domniemany [0, 0, 0, 0, 0, 0]
+	V_vector(const double t[6]);										// utworzenie wektora na podstawie podanej tablicy
+	V_vector(double fx, double fy, double fz, double tx, double ty, double tz);
 
-	void set_values(const double t[6]);										// wypelnienie wektora na podstawie podanej tablicy
-	void set_values(double fx, double fy, double fz, double tx, double ty, double tz);
+	V_vector(const V_vector &);								// konstruktor kopiujacy
 
-	// Ustawienie elementu wektora.
-	void set_value(int i, const double value);
-	// Zwrocenie elementu wektora.
-	void get_value(int i, double &value) const;
-	// Zwrocenie elementu wektora.
-	double get_value(int i) const;
+	// Odwracanie macierzy.
+	V_vector operator!() const;
+	V_vector operator-() const;
+	V_vector & operator=(const V_vector &);			// operator przypisania
+	V_vector operator+(const V_vector &) const;
+	V_vector operator-(const V_vector &) const;
+	V_vector operator*(double) const;					// skalowanie wektora
 
 	//Sibi
 	 //Wektor predkosci jako odleglosc dwuch pozycji zadanych w postaci ramek
 	void position_distance(frame_tab* local_current_end_effector_frame, frame_tab* local_desired_end_effector_frame);
 
-	// Wyspisanie na ekran wektora
-	void wypisz_wartosc_na_konsole() const;
-
-	// in theory, the RHS operator
-    double operator[](const int i) const;
-      // in theory, the LHS operator
-    double& operator[](const int i);
-
-	// Odwracanie macierzy.
-	Ft_v_vector operator!() const;
-	Ft_v_vector operator-() const;
-	Ft_v_vector & operator=(const Ft_v_vector &);			// operator przypisania
-	Ft_v_vector operator+(const Ft_v_vector &) const;
-	Ft_v_vector operator-(const Ft_v_vector &) const;
-	Ft_v_vector operator*(double) const;					// skalowanie wektora
-	void operator+=(const Ft_v_vector &);
+};// end class Ft_vector
 
 
-	void to_table(double tablica[6]) const;					// przepisanie wektora do tablicy podanej jako argument
 
-	friend std::ostream& operator<<(std::ostream & s, Ft_v_vector & w);		// operator wypisania
 
-	//Sibi
-	//Wyciagniecie max elementu z wektora
-	double max_element ();	//wyciagniecie maksymalnego elementu wektora
-
-};// end class Ft_v_vector
-
-*/
 
 
 
