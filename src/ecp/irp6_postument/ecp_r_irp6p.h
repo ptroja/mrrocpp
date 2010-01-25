4b58b9e7eb792fd7f85ac0641a129b7302e347d6
Tomek Winiarski <tomrobotics@gmail.com>
2010-01-25 23:12:16 +0000

    temporary not compiling commit
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3641 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/irp6_postument/ecp_r_irp6p.h
+++ src/ecp/irp6_postument/ecp_r_irp6p.h
@@ -11,20 +11,29 @@
 #include "ecp/common/ecp_robot.h"
 #include "lib/irp6p_const.h"
 
+#include "kinematics/common/kinematics_manager.h"
+#include "kinematics/irp6_postument/kinematic_model_irp6p_with_wrist.h"
+#include "kinematics/irp6_postument/kinematic_model_irp6p_5dof.h"
+#include "kinematics/irp6_postument/kinematic_model_calibrated_irp6p_with_wrist.h"
+#include "kinematics/irp6_postument/kinematic_model_irp6p_jacobian_with_wrist.h"
+#include "kinematics/irp6_postument/kinematic_model_irp6p_jacobian_transpose_with_wrist.h"
+#include "kinematics/common/kinematic_model_with_tool.h"
+
 namespace mrrocpp {
 namespace ecp {
 namespace irp6p {
 
 // ---------------------------------------------------------------
-class robot: public common::ecp_robot
+class robot: public common::ecp_robot, public kinematics::common::kinematics_manager
 {
 	// Klasa dla robota irp6_postument (sztywnego)
+protected:
+	// Metoda tworzy modele kinematyczne dla robota IRp-6 na torze.
+	virtual void create_kinematic_models_for_given_robot(void);
 
-	public:
-	robot (lib::configurator &_config, lib::sr_ecp &_sr_ecp);
-	robot (common::task::task& _ecp_object);
-
-
+public:
+	robot(lib::configurator &_config, lib::sr_ecp &_sr_ecp);
+	robot(common::task::task& _ecp_object);
 
 }; // end: class ecp_irp6_postument_robot
 // ---------------------------------------------------------------
