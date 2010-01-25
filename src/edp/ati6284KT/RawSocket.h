46e36e5826a7e82bbc570dafc02ba591c9611780
Kamil Tarkowski <K.Tarkowski@stud.elka.pw.edu.pl>
2010-01-25 14:00:50 +0000

    Dodanie plików.
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3615 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/edp/ati6284KT/RawSocket.h
@@ -0,0 +1,34 @@
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+
+#include <signal.h>
+#include <net/if_dl.h>
+#include <net/if_ether.h>
+#include <net/bpf.h>
+
+
+namespace sensor {
+
+  class RawSocket {
+  private:
+    static const std::size_t SEND_BUF_LEN = 1024;
+    static const std::size_t RECV_BUF_LEN = 1024;
+    static const unsigned int ETHERNET_HEADER_LEN = 14;
+  public:
+     RawSocket(const char *device, const uint8_t mac[6]);
+     RawSocket(const char *device);
+     void send(const unsigned char *buf, std::size_t len);
+     std::size_t recv(unsigned char *buf, std::size_t bufLen);
+  private:
+    int bpf_;
+    int bpfBufLen_;
+    bpf_hdr *bpf_buf;
+    unsigned char sendBuf_[SEND_BUF_LEN];
+    unsigned char recvBuf_[RECV_BUF_LEN];
+    void setupBpf(const char *device, const uint8_t mac[6]);
+//    ~RawSocket(){*this = NULL;};
+  };
+
+}
--- src/edp/ati6284KT/edp_s.h
+++ src/edp/ati6284KT/edp_s.h
@@ -116,6 +116,34 @@ namespace sensor {
 #define INTR_NS_DELAY 10000 // opoznienie pomiedzy dwoma stanami wyjscia karty
 // w funkcji obslugi przerwania w nanosekundach
 
+static const uint8_t TARGET_ETHERNET_ADDRESS[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF };        //board mac address
+
+//static const uint8_t TARGET_ETHERNET_ADDRESS[] = {0x00, 0xE0, 0x7D, 0xDD, 0x7C, 0x94};
+//static const uint8_t TARGET_ETHERNET_ADDRESS[] = {0x00, 0x1F, 0x1F, 0x4D, 0x5B, 0xB0}; //koleszko
+//static const uint8_t LOCAL_ETHERNET_ADDRESS[] = {0x00, 0x02, 0x44, 0x9F, 0xBD, 0x24}; //lenin eth0
+static const uint8_t LOCAL_ETHERNET_ADDRESS[] = { 0x00, 0x1F, 0x1F, 0x0D, 0x99, 0x4C }; //lenin eth1, p2p
+
+
+/* macierz konwersji napiecia z czujnika na sile i momenty sil */
+double conversion_scale[6] = {
+   4.5511972116989,
+   4.5511972116989,
+   1.41244051397552,
+  84.8843245576086,
+  84.8843245576086,
+  80.9472037525247
+};
+
+double conversion_matrix[6][6] = {
+  {-0.40709,  -0.27318,   0.34868, -33.58156,  -0.32609,  33.54162},
+  { 0.35472,  38.22730,  -0.41173, -19.49156,   0.49550, -19.15271},
+  {18.72635,  -0.59676,  19.27843,  -0.56931,  18.69352,  -0.67633},
+  {-0.40836,  -0.95908, -33.37957,   1.38537,  32.52522,  -0.51156},
+  {37.13715,  -1.02875, -20.00474,  -0.27959, -19.34135,   1.42577},
+  {-0.15775, -18.16831,  -0.00133, -18.78961,   0.31895, -18.38586}
+};
+
+
 
 /********** klasa czujnikow po stronie VSP **************/
 class ATI3084_force : public force{
