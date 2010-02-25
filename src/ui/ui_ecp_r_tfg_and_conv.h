ef1460d233366f3fee810d9ebd54dd617b63c178
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-02-25 13:22:16 +0000

    ui ecp robot refactor in progress
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@3948 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ui/ui_ecp_r_tfg_and_conv.h
+++ src/ui/ui_ecp_r_tfg_and_conv.h
@@ -14,61 +14,19 @@
 // Konfigurator.
 #include "lib/configurator.h"
 #include "lib/mrmath/mrmath.h"
+#include "ui/ui_ecp_r_common.h"
 
 // ---------------------------------------------------------------
-class ui_tfg_and_conv_robot
+class ui_tfg_and_conv_robot : public ui_common_robot
 {
-	// Klasa do obslugi robotow irp6 z poziomu UI
-
-	// Dopuszczalne przyrosty polozenia w pojedynczym kroku [2ms] przy ruchach
-	// recznych dla roznych wspolrzednych
-	double MOTOR_STEP; // Przyrost kata obrotu walu silnika [rad]
-	double MOTOR_GRIPPER_STEP;
-	double JOINT_ANGULAR_STEP; // Przyrost kata obrotu w przegubie obrotowym [rad]
-	double JOINT_LINEAR_STEP; // Przyrost liniowy w przegubach posuwistych [mm]
-	double JOINT_GRIPPER_STEP;
-	double END_EFFECTOR_LINEAR_STEP; // Przyrost wspolrzednej polozenia koncowki [mm]
-	double END_EFFECTOR_ANGULAR_STEP; // Przyrost wspolrzednej orientacji koncowki [rad]
-	double END_EFFECTOR_GRIPPER_STEP; // Przyrost wspolrzednej orientacji koncowki [rad]
-
-	double desired_position[MAX_SERVOS_NR]; // polozenie zadane
-	double current_position[MAX_SERVOS_NR]; // polozenie aktualne
 
 public:
-	ecp::common::ecp_robot *ecp;
 
 	// ecp_buffer ui_edp_package; // by Y
 	ui_tfg_and_conv_robot(lib::configurator &_config, lib::sr_ecp &_sr_ecp_msg, lib::robot_name_t _robot_name); // Konstruktor
 
-	virtual ~ui_tfg_and_conv_robot();
-
-	virtual void execute_motion(void);
-	// virtual void set_edp_master_pid ( pid_t edppid ) {EDP_Pid = edppid;};
-	// Przekazanie identyfikatora procesu EDP
-	// virtual void synchronise ( void ); // Zlecenie synchronizacji robota
-	// virtual bool is_synchronised( void ) { return synchronised;};
-
-	virtual void set_desired_position(double des_position[MAX_SERVOS_NR]);
-	// Przepisanie polozen zadanych
-	// do tablicy desired_position[]
-	virtual void get_current_position(double c_position[MAX_SERVOS_NR]); // Pobranie aktualnych polozen
-
-	// by Y - do odczytu stanu poczatkowego robota
-	void get_controller_state(lib::controller_state_t & robot_controller_initial_state_l);
-
-	// Zlecenie ruchu
 	void move_motors(const double final_position[MAX_SERVOS_NR]);
 	void move_joints(const double final_position[MAX_SERVOS_NR]);
-
-	void set_kinematic(uint8_t kinematic_model_no);
-	void set_servo_algorithm(uint8_t algorithm_no[MAX_SERVOS_NR], uint8_t parameters_no[MAX_SERVOS_NR]);
-
-	// Odczyt polozenia
-	void read_motors(double current_position[MAX_SERVOS_NR]);
-	void read_joints(double current_position[MAX_SERVOS_NR]);
-
-	void get_kinematic(uint8_t* kinematic_model_no);
-	void get_servo_algorithm(uint8_t algorithm_no[MAX_SERVOS_NR], uint8_t parameters_no[MAX_SERVOS_NR]);
 
 };
 #endif
