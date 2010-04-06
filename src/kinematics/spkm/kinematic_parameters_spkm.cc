8a9f6fe60c3867fbbe97eeb8da3908b208287db1
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-06 11:47:22 +0000

    Removed invalid W_S_T matrix settings - a temporary solution.
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4062 4c39dba3-d726-0410-8099-c98d187771e6

--- src/kinematics/spkm/kinematic_parameters_spkm.cc
+++ src/kinematics/spkm/kinematic_parameters_spkm.cc
@@ -16,9 +16,9 @@ kinematic_parameters_spkm::kinematic_parameters_spkm()
 {
 	// TODO Set parameters.
 	// TODO Set W_S_P;
-
+	// std::cout<< W_S_P<<std::endl;
 	// Construct W_S_T on the base of W_S_P.
-	W_S_T.matrix() << Matrix3d::Identity(), W_S_P.transpose(), 0,0,0, 1;
+	//W_S_T.matrix() << Matrix3d::Identity(), W_S_P.transpose(), 0,0,0, 1;
 }
 
 
