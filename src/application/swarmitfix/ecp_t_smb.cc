f2b641bd2f13dd345a01e1b41082927f841bb701
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-06 09:35:03 +0000

    swarm in progress
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4054 4c39dba3-d726-0410-8099-c98d187771e6

---
+++ src/application/swarmitfix/ecp_t_smb.cc
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#include "lib/typedefs.h"
+#include "lib/impconst.h"
+#include "lib/com_buf.h"
+
+#include "lib/srlib.h"
+#include "ecp_mp/task/ecp_mp_t_rcsc.h"
+
+#include "ecp/smb/ecp_r_smb.h"
+#include "ecp/common/generator/ecp_g_smooth.h"
+#include "ecp_t_smb.h"
+
+namespace mrrocpp {
+namespace ecp {
+namespace smb {
+namespace task {
+
+// KONSTRUKTORY
+swarmitfix::swarmitfix(lib::configurator &_config) :
+	task(_config)
+{
+	// the robot is choose dependendat on the section of configuration file sent as argv[4]
+	ecp_m_robot = new robot(*this);
+
+	gt = new common::generator::transparent(*this);
+	sg = new common::generator::smooth(*this, true);
+
+	sr_ecp_msg->message("ECP loaded");
+}
+
+void swarmitfix::main_task_algorithm(void)
+{
+	for (;;) {
+		sr_ecp_msg->message("Waiting for MP order");
+
+		get_next_state();
+
+		sr_ecp_msg->message("Order received");
+		//printf("postument: %d\n", mp_command.ecp_next_state.mp_2_ecp_next_state);
+		flushall();
+
+		switch ((ecp_mp::task::RCSC_ECP_STATES) mp_command.ecp_next_state.mp_2_ecp_next_state)
+		{
+			case ecp_mp::task::ECP_GEN_TRANSPARENT:
+				gt->throw_kinematics_exceptions = (bool) mp_command.ecp_next_state.mp_2_ecp_next_state_variant;
+				gt->Move();
+				break;
+			case ecp_mp::task::ECP_GEN_SMOOTH: {
+				std::string path(mrrocpp_network_path);
+				path += mp_command.ecp_next_state.mp_2_ecp_next_state_string;
+
+				switch ((ecp_mp::task::SMOOTH_MOTION_TYPE) mp_command.ecp_next_state.mp_2_ecp_next_state_variant)
+				{
+					case ecp_mp::task::RELATIVE:
+						sg->set_relative();
+						break;
+					case ecp_mp::task::ABSOLUTE:
+						sg->set_absolute();
+						break;
+					default:
+						break;
+				}
+
+				sg->load_file_with_path(path.c_str());
+				sg->Move();
+				break;
+			}
+			default:
+				break;
+		} // end switch
+
+		ecp_termination_notice();
+	} //end for
+}
+
+}
+} // namespace smb
+
+namespace common {
+namespace task {
+
+task* return_created_ecp_task(lib::configurator &_config)
+{
+	return new smb::task::swarmitfix(_config);
+}
+
+}
+} // namespace common
+} // namespace ecp
+} // namespace mrrocpp
