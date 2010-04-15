90f374af861aa2028780c0d6a066c7f9cb4c00d0
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-15 20:22:16 +0000

    single thread port in progress
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4105 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/single_thread_port.h
+++ src/lib/single_thread_port.h
@@ -73,8 +73,8 @@ public:
 	single_thread_port_manager() {
 	}
 
-	void add_port(std::string name, single_thread_port_interface* single_thread_port_inter) {
-		single_thread_port_map[name] = single_thread_port_inter;
+	void add_port(single_thread_port_interface* single_thread_port_inter) {
+		single_thread_port_map[single_thread_port_inter->get_name()] = single_thread_port_inter;
 	}
 
 	template<class T>
