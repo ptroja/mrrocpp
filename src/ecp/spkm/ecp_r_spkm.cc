eeb8fa7fc68c589212fbce6ad8ed8c3fa7ebcdae
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-19 09:10:26 +0000

    swarm epos ustilization in progress
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4115 4c39dba3-d726-0410-8099-c98d187771e6

--- src/ecp/spkm/ecp_r_spkm.cc
+++ src/ecp/spkm/ecp_r_spkm.cc
@@ -17,27 +17,62 @@ namespace ecp {
 namespace spkm {
 
 robot::robot(lib::configurator &_config, lib::sr_ecp &_sr_ecp) :
-	robot_epos_command_data_port(EPOS_COMMAND_DATA_PORT),
-			robot_epos_reply_data_port(EPOS_REPLY_DATA_PORT), ecp_robot(
-					lib::ROBOT_SPKM, SPKM_NUM_OF_SERVOS, EDP_SPKM_SECTION,
-					_config, _sr_ecp) {
+	epos_command_data_port(EPOS_COMMAND_DATA_PORT), epos_reply_data_port(
+			EPOS_REPLY_DATA_PORT), ecp_robot(lib::ROBOT_SPKM,
+			SPKM_NUM_OF_SERVOS, EDP_SPKM_SECTION, _config, _sr_ecp) {
 	add_data_ports();
 }
 
 robot::robot(common::task::task& _ecp_object) :
-	robot_epos_command_data_port(EPOS_COMMAND_DATA_PORT),
-			robot_epos_reply_data_port(EPOS_REPLY_DATA_PORT), ecp_robot(
-					lib::ROBOT_SPKM, SPKM_NUM_OF_SERVOS, EDP_SPKM_SECTION,
-					_ecp_object) {
+	epos_command_data_port(EPOS_COMMAND_DATA_PORT), epos_reply_data_port(
+			EPOS_REPLY_DATA_PORT), ecp_robot(lib::ROBOT_SPKM,
+			SPKM_NUM_OF_SERVOS, EDP_SPKM_SECTION, _ecp_object) {
 	add_data_ports();
 }
 
-
 void robot::add_data_ports() {
-	port_manager.add_port(&robot_epos_command_data_port);
-	port_manager.add_port(&robot_epos_reply_data_port);
+	port_manager.add_port(&epos_command_data_port);
+	port_manager.add_port(&epos_reply_data_port);
 }
 
+void robot::create_command() {
+
+	if (epos_command_data_port.is_new_data()) {
+		epos_data_port_command_structure = epos_command_data_port.get();
+		// generator command interpretation
+		// narazie proste przepisanie
+		for (int i = 0; i < 6; i++) {
+			ecp_edp_cbuffer.em[i] = epos_data_port_command_structure.em[i];
+			ecp_edp_cbuffer.emdm[i] = epos_data_port_command_structure.emdm[i];
+			ecp_edp_cbuffer.aa[i] = epos_data_port_command_structure.aa[i];
+			ecp_edp_cbuffer.da[i] = epos_data_port_command_structure.da[i];
+			ecp_edp_cbuffer.av[i] = epos_data_port_command_structure.av[i];
+		}
+		ecp_edp_cbuffer.tt = epos_data_port_command_structure.tt;
+		ecp_edp_cbuffer.profile_type
+				= epos_data_port_command_structure.profile_type;
+	} else {
+		ecp_edp_cbuffer.profile_type = lib::EPOS_GEN_PROFILE_NO_ACTION;
+	}
+	// message serialization
+	memcpy(ecp_command.instruction.arm.serialized_command, &ecp_edp_cbuffer,
+			sizeof(ecp_edp_cbuffer));
+}
+
+void robot::get_reply() {
+	// message deserialization
+	memcpy(&edp_ecp_rbuffer, reply_package.arm.serialized_reply,
+			sizeof(edp_ecp_rbuffer));
+
+	// generator reply generation
+	for (int i = 0; i < 6; i++) {
+		edp_ecp_rbuffer.position[i]
+				= epos_data_port_reply_structure.position[i];
+		edp_ecp_rbuffer.motion_in_progress[i]
+				= epos_data_port_reply_structure.motion_in_progress[i];
+	}
+
+}
 
 } // namespace spkm
 } // namespace ecp
--- src/ecp/spkm/ecp_r_spkm.h
+++ src/ecp/spkm/ecp_r_spkm.h
@@ -20,10 +20,12 @@ namespace spkm {
 class robot: public common::ecp_robot {
 	// Klasa dla robota irp6_postument (sztywnego)
 protected:
-	lib::single_thread_port<epos_command> robot_epos_command_data_port;
-	lib::single_thread_port<epos_reply> robot_epos_reply_data_port;
-	epos_command robot_epos_command;
-	epos_reply robot_epos_reply;
+	lib::single_thread_port<epos_command> epos_command_data_port;
+	lib::single_thread_port<epos_reply> epos_reply_data_port;
+	epos_command epos_data_port_command_structure;
+	epos_reply epos_data_port_reply_structure;
+	lib::spkm_cbuffer ecp_edp_cbuffer;
+	lib::spkm_rbuffer edp_ecp_rbuffer;
 
 	void add_data_ports();
 
@@ -31,6 +33,8 @@ public:
 	robot(lib::configurator &_config, lib::sr_ecp &_sr_ecp);
 	robot(common::task::task& _ecp_object);
 
+	void create_command();
+	void get_reply();
 
 }; // end: class ecp_irp6_mechatronika_robot
 // ---------------------------------------------------------------
