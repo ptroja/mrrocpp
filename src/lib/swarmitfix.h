8c3819316076cace7cc2546665c16345a0d0875e
ptrojane <ptrojane@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-28 05:35:13 +0000

    code cleanup
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4208 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/swarmitfix.h
+++ src/lib/swarmitfix.h
@@ -1,56 +1,30 @@
-////////////////////////////////////////////////////////////////////////////////
-/*! \file     src/lib/com_buf.h
- *
- *  Data structures for IPC.
- *
- *  \author   tkornuta
- *  \date     2006-11-29
- *  \URL: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk/include/lib/com_buf.h $
- *  $LastChangedRevision: 4088 $
- *  $LastChangedDate: 2010-04-13 12:53:09 +0200 (Tue, 13 Apr 2010) $
- *  $LastChangedBy: yoyek $
- *
- *  \todo <ul>
- *          <li>Translate to English where necessary.</li>
- *          <li>Write detailed comments.</li>
- *          <li>Suplement comments for those consts, variables and structures
- *              that are not commented at all.</li>
- *          <li>Clean up the commented fragments of code.</li>
- *        </ul>
- */
-////////////////////////////////////////////////////////////////////////////////
-
 #ifndef __SWARMITFIX_H
 #define __SWARMITFIX_H
-
 
 namespace mrrocpp {
 namespace lib {
 
-
-
 enum EPOS_GEN_PROFILE {
-	TRAPEZOIDAL_VELOCITY=0, CUBIC_POSITION, EPOS_GEN_PROFILE_NO_ACTION
+	TRAPEZOIDAL_VELOCITY, CUBIC_POSITION, EPOS_GEN_PROFILE_NO_ACTION
 };
 
 enum SMB_PIN_INSERTION {
-	INSERT = 0, WITHDRAWN, SMB_PIN_INSERTION_NO_ACTION
+	INSERT, WITHDRAWN, SMB_PIN_INSERTION_NO_ACTION
 }; // namespace mrrocpp
 
 enum SMB_PIN_LOCKING {
-	CLAMB = 0, UNCLAMB, SMB_PIN_LOCKING_NO_ACTION
+	CLAMB, UNCLAMB, SMB_PIN_LOCKING_NO_ACTION
 }; // namespace mrrocpp
 
 enum SHEAD_HEAD_SOLIDIFICATION {
-	SOLIDIFY = 0, DESOLIDIFY, SHEAD_HEAD_SOLIDIFICATION_NO_ACTION
+	SOLIDIFY, DESOLIDIFY, SHEAD_HEAD_SOLIDIFICATION_NO_ACTION
 }; // namespace mrrocpp
 
 enum SHEAD_VACUUM_ACTIVATION {
-	VACUUM_ON= 0, VACUUM_OFF, SHEAD_VACUUM_ACTIVATION_NO_ACTION
+	VACUUM_ON, VACUUM_OFF, SHEAD_VACUUM_ACTIVATION_NO_ACTION
 }; // namespace mrrocpp
 
 }
 }
-
 
 #endif
