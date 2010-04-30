340725375087ea33d9ad30fed1b0759ea929c9da
yoyek <yoyek@4c39dba3-d726-0410-8099-c98d187771e6>
2010-04-30 16:21:26 +0000

    shead in progress
    git-svn-id: https://segomo.elka.pw.edu.pl/svn/mrrocpp/base/trunk@4239 4c39dba3-d726-0410-8099-c98d187771e6

--- src/lib/data_port_headers/shead.h
+++ src/lib/data_port_headers/shead.h
@@ -8,7 +8,6 @@
 namespace mrrocpp {
 namespace lib {
 
-
 enum SHEAD_HEAD_SOLIDIFICATION {
 	SOLIDIFY, DESOLIDIFY, SHEAD_HEAD_SOLIDIFICATION_NO_ACTION
 }; // namespace mrrocpp
@@ -16,6 +15,18 @@ enum SHEAD_HEAD_SOLIDIFICATION {
 enum SHEAD_VACUUM_ACTIVATION {
 	VACUUM_ON, VACUUM_OFF, SHEAD_VACUUM_ACTIVATION_NO_ACTION
 }; // namespace mrrocpp
+
+
+#define SHEAD_HEAD_SOLIDIFICATION_DATA_PORT "shead_head_soldification_data_port"
+#define SHEAD_VACUUM_ACTIVATION_DATA_PORT "shead_vacuum_activation_data_port"
+#define SHEAD_REPLY_DATA_REQUEST_PORT "shead_reply_data_request_port"
+
+struct shead_reply {
+	bool head_solfified;
+	bool vacuum_on;
+	bool soldification_in_progress;
+	bool vacumization_in_progress;
+};
 
 }
 }
