f743ed475db0fedcc9d6bd36252b8d8e48388532
Piotr Trojanek <piotr.trojanek@gmail.com>
2010-05-02 22:21:46 +0900

    datatypes code cleanup

--- src/edp/ati6284KT/ForceSensor.h
+++ src/edp/ati6284KT/ForceSensor.h
@@ -11,16 +11,16 @@ namespace mrrocpp {
 namespace edp {
 namespace sensor {
 
-struct AdcReadings {
-    bool timeout;
-    int16_t readings[6];
-};
-
 class ForceSensor6284 {
 public:
+	typedef struct _AdcReadings {
+	    bool timeout;
+	    int16_t readings[6];
+	} AdcReadings_t;
+
     ForceSensor6284(RawSocket* socket);
     ~ForceSensor6284();
-    AdcReadings getAdcReadings(boost::posix_time::time_duration timeout);
+    AdcReadings_t getAdcReadings(boost::posix_time::time_duration timeout);
 private:
     void receiveThread();
     RawSocket* socket_;
@@ -30,7 +30,6 @@ private:
     std::size_t responseSize_;
     boost::mutex responseReceivedMt_;
     boost::condition_variable responseReceivedCv_;
-    
 };
 
 }
