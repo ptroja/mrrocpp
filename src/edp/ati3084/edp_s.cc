09a979feca7b8480fadafe94b0a6769d84f7ecd6
Piotr Trojanek <piotr.trojanek@gmail.com>
2010-05-02 23:46:40 +0900

    ati3084 code cleaned

--- src/edp/ati3084/edp_s.cc
+++ src/edp/ati3084/edp_s.cc
@@ -1,37 +1,30 @@
 // -------------------------------------------------------------------------
 //                            edp_s.cc 		dla QNX6.3.0
 //
-//            Virtual Sensor Process (lib::VSP) - methods for Schunk force/torgue sensor
+//            Virtual Sensor Process (lib::VSP) - methods for Schunk force/torque sensor
 // Metody klasy VSP
 //
 // Ostatnia modyfikacja: grudzie 2004
 // Autor: Yoyek (Tomek Winiarski)
 // na podstawie szablonu vsp Tomka Kornuty i programu obslugi czujnika Artura Zarzyckiego
 // -------------------------------------------------------------------------
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdint>
+#include <iostream>
+
 #include <unistd.h>
-#include <string.h>
 #include <signal.h>
 #include <process.h>
-#include <math.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/sched.h>
-#include <string.h>
-#include <fstream>
-#include <iomanip>
-#include <ctype.h>
-#include <errno.h>
+
 #include <sys/iofunc.h>
-#include <sys/dispatch.h>
-#include <iostream>
 #include <sys/neutrino.h>
 #include <hw/inout.h>
 #include <sys/dispatch.h>
 #include <hw/pci.h>
 #include <hw/pci_devices.h>
-#include <stddef.h>
 #include <sys/mman.h>
 
 #include "lib/typedefs.h"
@@ -48,11 +41,10 @@ namespace mrrocpp {
 namespace edp {
 namespace sensor {
 
-static int sint_id;
 static struct sigevent sevent;
 
 static struct pci_dev_info info;// do karty advantech pci1751
-static uintptr_t base_io_adress; // do obslugi karty advantech pci1751
+static uintptr_t base_io_address; // do obslugi karty advantech pci1751
 
 const struct sigevent * schunk_int_handler(void *arg, int sint_id)
 {
@@ -115,9 +107,8 @@ ATI3084_force::ATI3084_force(common::manip_effector &_master) :
 
 void ATI3084_force::connect_to_hardware(void)
 {
-	if (!(master.test_mode)) {
+	if (!master.test_mode) {
 		ThreadCtl(_NTO_TCTL_IO, NULL); // nadanie odpowiednich uprawnien watkowi
-		// 	printf("KONTRUKTOR EDP_S POCATEK\n");
 
 		// ZMIENNE POMOCNICZE
 		int_timeout = SCHUNK_INTR_TIMEOUT_HIGH;// by Y
@@ -129,10 +120,10 @@ void ATI3084_force::connect_to_hardware(void)
 		phdl = pci_attach(0);
 		if (phdl == -1) {
 			fprintf(stderr, "Unable to initialize PCI\n");
+			// TODO: throw
 			return;
 		}
-		// 	printf("po pci_attach_device\n");
-		delay(100);
+
 		/* Initialize the pci_dev_info structure */
 		memset(&info, 0, sizeof(info));
 		pidx = 0x0;
@@ -142,13 +133,14 @@ void ATI3084_force::connect_to_hardware(void)
 		hdl = pci_attach_device(NULL, PCI_INIT_ALL, pidx, &info);
 		if (hdl == NULL) {
 			fprintf(stderr, "Unable to locate Advantech 1751\n");
+			// TODO: throw
 			return;
 		}
 		// 	printf("connected to Advantech 1751\n");
-		delay(100);
+
 		// printf("Przerwanie numer: %d\n",info.Irq);
-		base_io_adress = mmap_device_io(info.BaseAddressSize[2], PCI_IO_ADDR(info.CpuBaseAddress[2]));
-		// 	printf("base: %d\n",base_io_adress);
+		base_io_address = mmap_device_io(info.BaseAddressSize[2], PCI_IO_ADDR(info.CpuBaseAddress[2]));
+		// 	printf("base: %d\n",base_io_address);
 
 		initiate_registers();// konfiguracja karty
 
@@ -163,8 +155,10 @@ void ATI3084_force::connect_to_hardware(void)
 		nanospin_calibrate(1);
 
 		interrupt_id = InterruptAttach(info.Irq, schunk_int_handler, (void *) &mds, sizeof(mds), 0);
-		if (interrupt_id == -1)
-			printf("Unable to attach interrupt handler: \n");
+		if (interrupt_id == -1) {
+			// TODO: throw
+			perror("InterruptAttach()");
+		}
 
 		// USTAWIENIE ZLACZA SZEREGOWEGO
 
@@ -234,7 +228,7 @@ void ATI3084_force::connect_to_hardware(void)
 
 ATI3084_force::~ATI3084_force(void)
 {
-	if (!(master.test_mode)) {
+	if (!master.test_mode) {
 		if(interrupt_id != -1)
 			InterruptDetach(interrupt_id);
 		if(hdl != NULL)
@@ -253,7 +247,7 @@ void ATI3084_force::configure_sensor(void)
 	is_sensor_configured = true;
 	//  printf("EDP Sensor configured\n");
 	sr_msg->message("EDP Sensor configured");
-	if (!(master.test_mode)) {
+	if (!master.test_mode) {
 		InterruptLock(&mds.spinlock);
 		mds.intr_mode = 0;
 		InterruptUnlock(&mds.spinlock);
@@ -261,13 +255,13 @@ void ATI3084_force::configure_sensor(void)
 #ifdef SERIAL
 		if (do_send_command(SB) == -1)
 			printf("Blad wyslania polecenia SB\n");
-		do_Wait("SB");
+		do_Wait();
 #endif
 
 #ifdef PARALLEL
 		parallel_do_send_command(SB);
-		do_Wait("SB");
-		do_Wait("SB");
+		do_Wait();
+		do_Wait();
 #endif
 		InterruptLock(&mds.spinlock);
 		mds.intr_mode = 1; // przywrocenie do 7 bajtowego trybu odbiotu danych
@@ -376,10 +370,9 @@ void ATI3084_force::wait_for_event()
 				do_init(); // komunikacja wstepna
 			} else {
 				if (iter_counter > 1) {
-					sr_msg->message("Force / Torque sensor connection reastablished");
+					sr_msg->message("Force / Torque sensor connection reestablished");
 				}
 			}
-
 		} while (iw_ret == -1); // dopoki nie zostanie odebrana paczka pomiarow
 	} else {
 		usleep(1000);
@@ -404,10 +397,10 @@ void ATI3084_force::initiate_reading(void)
 
 		InterruptLock(&mds.spinlock);
 
-		for (int i = 0; i < 6; i++)
-			ft_table[i] = static_cast <double> (mds.data[i + 1]);
+		uint16_t measure_report = mds.data[0];
 
-		short measure_report = mds.data[0];
+		for (int i = 0; i < 6; ++i)
+			ft_table[i] = static_cast <double> (mds.data[i + 1]);
 
 		InterruptUnlock(&mds.spinlock);
 
@@ -415,7 +408,7 @@ void ATI3084_force::initiate_reading(void)
 		if (measure_report == COMMAND_OK) {
 			is_reading_ready = true;
 
-			// jesli ma byc wykorzytstywana biblioteka transformacji sil
+			// jesli ma byc wykorzystywana biblioteka transformacji sil
 			if (master.force_tryb == 2 && gravity_transformation) {
 				for (int i = 0; i < 3; i++)
 					ft_table[i] /= 20;
@@ -462,7 +455,7 @@ void ATI3084_force::parallel_do_send_command(const char* command)
 	rqtp.tv_nsec = 100000;
 
 	while ((a = *command++) != 0) {
-		short value = short(a);
+		uint16_t value = uint16_t(a);
 		set_output(value);
 		while (!check_ack());
 		set_obf(0);
@@ -476,52 +469,47 @@ void ATI3084_force::parallel_do_send_command(const char* command)
 	}
 }
 
-void ATI3084_force::set_char_output(char* znak)
+void ATI3084_force::set_output(uint16_t value)
 {
-	short value = *znak;
-	set_output(value);
-}
+	uint16_t output = 0;
+	uint16_t comp = 0x0001;
 
-void ATI3084_force::set_output(short value)
-{
-	short output = 0;
-	unsigned short comp = 0x0001;
-	uint8_t lower, upper;
 	// wersja z pajaczkiem
 	// 	const unsigned char output_positions[16]={15,7,14,6,13,5,12,4,0,8,1,9,2,10,3,11};
 	// wersja z nowa plytka
 	const unsigned char output_positions[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
 	for (int i = 0; i < 16; i++) {
-		unsigned short mask = 0x0001;
+		uint16_t mask = 0x0001;
 		mask <<= output_positions[i];
 		if (value & comp)
 			output |= mask;
 		comp <<= 1;
 	}
-	lower = (unsigned char) (output % 256);
-	upper = (unsigned char) (output >>= 8);
 
-	out8(base_io_adress + LOWER_OUTPUT, lower);
-	out8(base_io_adress + UPPER_OUTPUT, upper);
+	uint8_t lower = (unsigned char) (output % 256);
+	uint8_t upper = (unsigned char) (output >>= 8);
+
+	out8(base_io_address + LOWER_OUTPUT, lower);
+	out8(base_io_address + UPPER_OUTPUT, upper);
 }
 
-short get_input(void)
+uint16_t get_input(void)
 {
-	short input = 0, temp_input;
-	unsigned short comp = 0x0001;
+	uint16_t input = 0, temp_input;
+	uint16_t comp = 0x0001;
 	// wersja z pajaczkiem
 	// 	const unsigned char input_positions[16]={8,10,12,14,7,5,3,1,9,11,13,15,6,4,2,0};
 	// wersja z nowa plytka
 	const unsigned char input_positions[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-	uint8_t lower = in8(base_io_adress + LOWER_INPUT);
-	uint8_t upper = in8(base_io_adress + UPPER_INPUT);
+	uint8_t lower = in8(base_io_address + LOWER_INPUT);
+	uint8_t upper = in8(base_io_address + UPPER_INPUT);
 
 	temp_input = lower + 256* upper ;
 
 	for (int i = 0; i < 16; i++) {
-		unsigned short mask = 0x0001;
+		uint16_t mask = 0x0001;
 		mask <<= input_positions[i];
 		if (temp_input & comp)
 			input |= mask;
@@ -532,31 +520,31 @@ short get_input(void)
 
 void ATI3084_force::set_obf(unsigned char state)
 {
-	uint8_t temp_register = in8(base_io_adress + CONTROL_OUTPUT);
+	uint8_t temp_register = in8(base_io_address + CONTROL_OUTPUT);
 
 	if (state)
 		temp_register |= 0x10;// dla przejsciowki
 	else
 		temp_register &= 0xef;
 
-	out8(base_io_adress + CONTROL_OUTPUT, temp_register);
+	out8(base_io_address + CONTROL_OUTPUT, temp_register);
 }
 
 void set_ibf(unsigned char state)
 {
-	uint8_t temp_register = in8(base_io_adress + CONTROL_OUTPUT);
+	uint8_t temp_register = in8(base_io_address + CONTROL_OUTPUT);
 
 	if (state)
 		temp_register |= 0x20;// dla przejsciowki
 	else
 		temp_register &= 0xdf;
 
-	out8(base_io_adress + CONTROL_OUTPUT, temp_register);
+	out8(base_io_address + CONTROL_OUTPUT, temp_register);
 }
 
 bool ATI3084_force::check_ack()
 {
-	uint8_t temp_register = in8(base_io_adress + ACK_PORT_INPUT);
+	uint8_t temp_register = in8(base_io_address + ACK_PORT_INPUT);
 
 	if (temp_register & 0x01)
 		return true;
@@ -566,7 +554,7 @@ bool ATI3084_force::check_ack()
 
 bool check_stb()
 {
-	uint8_t temp_register = in8(base_io_adress + STB_PORT_INPUT);
+	uint8_t temp_register = in8(base_io_address + STB_PORT_INPUT);
 
 	if (temp_register & 0x01)
 		return true;
@@ -576,9 +564,9 @@ bool check_stb()
 
 void ATI3084_force::initiate_registers(void)
 {
-	out8(base_io_adress + PORT_0_CONFIG, 0x03);
-	out8(base_io_adress + PORT_1_CONFIG, 0x03);
-	out8(base_io_adress + INTER_CONFIG, 0x10);// przerwanie od !stb, mozna dolaczyc przerwanie od !ack
+	out8(base_io_address + PORT_0_CONFIG, 0x03);
+	out8(base_io_address + PORT_1_CONFIG, 0x03);
+	out8(base_io_address + INTER_CONFIG, 0x10);// przerwanie od !stb, mozna dolaczyc przerwanie od !ack
 	delay(100);
 	set_obf(1);
 	set_ibf(0);
@@ -586,28 +574,23 @@ void ATI3084_force::initiate_registers(void)
 
 bool check_intr(void)
 {
-	uint8_t temp_register = in8(base_io_adress + INTER_CONFIG);
+	uint8_t temp_register = in8(base_io_address + INTER_CONFIG);
 
 	if (temp_register & 0x80)
-		return 1;
+		return true;
 	else
-		return 0;
+		return false;
 }
 
 void ATI3084_force::check_cs(void)
 {
-	printf("Input int: %d, char: %c,   ", get_input(), get_input());
-	if (check_ack())
-		printf("ACK HIGH,  ");
-	else
-		printf("ACK LOW,   ");
-	if (check_stb())
-		printf("STB HIGH\n");
-	else
-		printf("STB LOW\n");
+	printf("ATI3084_force::check_cs() %04hx, %04hx, ACK %s, STB %s\n",
+			get_input(), get_input(),
+			check_ack() ? "HIGH" : "LOW",
+			check_stb() ? "HIGH" : "LOW");
 }
 
-short ATI3084_force::do_Wait(const char* command)
+void ATI3084_force::do_Wait(void)
 {
 	int iw_ret;
 
@@ -624,11 +607,9 @@ short ATI3084_force::do_Wait(const char* command)
 		}
 
 	} while (iw_ret != -1);
-
-	return OK;
 }
 
-short ATI3084_force::do_send_command(const char* command)
+int ATI3084_force::do_send_command(const char* command)
 {
 	// ew. miejce na pzerwanie o pustej kolejce - obecnie while pod spodem
 	// 	while ( ! ( in8 ( LSREG ) & 0x40 ));
@@ -644,7 +625,7 @@ short ATI3084_force::do_send_command(const char* command)
 		}
 		out8(TxBUF, a);
 	}
-	return OK;
+	return 0;
 }
 
 // metoda na wypadek skasowanie pamiecia nvram
@@ -664,7 +645,6 @@ void ATI3084_force::solve_transducer_controller_failure(void)
 
 	int i = do_send_command(YESCOMM); /* command ^W to FT */
 	if (i == -1) {
-		ERROR_CODE = __ERROR_INIT_SEND;
 		printf("Blad wyslania YESCOMM w solve_transducer_controller_failure\n");
 	}
 
@@ -680,88 +660,65 @@ void ATI3084_force::solve_transducer_controller_failure(void)
 	// 	printf("Input int: %s,   \n",aaac);
 }
 
-short ATI3084_force::do_init(void)
+void ATI3084_force::do_init(void)
 {
 #ifdef SERIAL
-
-	short i = do_send_command(RESET); /* command ^W to FT */
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(CL_0);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(CD_B);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(CD_B);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(CD_R);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(CV_6);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(SA);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(SM);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	delay(20);
-	i = do_send_command(SB);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
+	do_send_command(RESET); /* command ^W to FT */
 	delay(20);
 
-	i = do_send_command(CP_P);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CP_P");
-	i = do_send_command(CL_0);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CL_0");
-	i = do_send_command(CD_B);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CD_B");
-	i = do_send_command(CD_B);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CD_B");
-	i = do_send_command(CD_R);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CD_R");
-	i = do_send_command(CV_6);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("CV_6");
-	i = do_send_command(SA);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("SA");
-	i = do_send_command(SM);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("SM");
-	i = do_send_command(SZ);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("SZ");
-	i = do_send_command(SB);
-	if (i == -1)
-		ERROR_CODE = __ERROR_INIT_SEND;
-	i = do_Wait("SB");
+	do_send_command(CL_0);
+	delay(20);
 
+	do_send_command(CD_B);
+	delay(20);
+
+	do_send_command(CD_B);
+	delay(20);
+
+	do_send_command(CD_R);
+	delay(20);
+
+	do_send_command(CV_6);
+	delay(20);
+
+	do_send_command(SA);
+	delay(20);
+
+	do_send_command(SM);
+	delay(20);
+
+	do_send_command(SB);
+	delay(20);
+
+	do_send_command(CP_P);
+	do_Wait();
+
+	do_send_command(CL_0);
+	do_Wait();
+
+	do_send_command(CD_B);
+	do_Wait();
+
+	do_send_command(CD_B);
+	do_Wait();
+
+	do_send_command(CD_R);
+	do_Wait();
+
+	do_send_command(CV_6);
+	do_Wait();
+
+	do_send_command(SA);
+	do_Wait();
+
+	do_send_command(SM);
+	do_Wait();
+
+	do_send_command(SZ);
+	do_Wait();
+
+	do_send_command(SB);
+	do_Wait();
 #endif
 
 #ifdef PARALLEL
@@ -794,48 +751,45 @@ short ATI3084_force::do_init(void)
 	delay(20);
 
 	parallel_do_send_command(CP_P);
-	i=do_Wait("CP_P");
+	do_Wait();
 
 	parallel_do_send_command(CL_0);
-	i=do_Wait("CL_0");
+	do_Wait();
 
 	parallel_do_send_command(CD_B);
-	i=do_Wait("CD_B");
+	do_Wait();
 
 	parallel_do_send_command(CD_B);
-	i=do_Wait("CD_B");
+	do_Wait();
 
 	parallel_do_send_command(CD_R);
-	i=do_Wait("CD_R");
+	do_Wait();
 
 	parallel_do_send_command(CV_6);
-	i=do_Wait("CV_6");
+	do_Wait();
 
 	parallel_do_send_command(SA);
-	i=do_Wait("SA");
+	do_Wait();
 
 	parallel_do_send_command(SM);
-	i=do_Wait("SM");
+	do_Wait();
 
 	parallel_do_send_command(SZ);
-	i=do_Wait("SZ");
+	do_Wait();
 
 	parallel_do_send_command(SB);
-	i=do_Wait("SB");
-	i=do_Wait("SB");// by Y bez tego nie dziala
-
+	do_Wait();
+	do_Wait();// by Y bez tego nie dziala
 #endif
-
-	return OK;
 }
 
 void clear_intr(void)
 {
-	uint8_t temp_register = in8(base_io_adress + INTER_CONFIG);
+	uint8_t temp_register = in8(base_io_address + INTER_CONFIG);
 
 	temp_register |= 0x80;
 
-	out8(base_io_adress + INTER_CONFIG, temp_register);
+	out8(base_io_address + INTER_CONFIG, temp_register);
 }
 
 force* return_created_edp_force_sensor(common::manip_effector &_master)
