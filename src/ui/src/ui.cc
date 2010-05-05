980e64d35027c822be4ec2a62a10cae82728ebc0
yoyek <yoyek@segomo.ia.pw.edu.pl>
2010-05-05 23:23:29 +0800

    feb_thread class intoduced in UI - bug 1375 resolved fixed

--- src/ui/src/ui.cc
+++ src/ui/src/ui.cc
@@ -8,13 +8,11 @@
 #include "ui/ui.h"
 
 ui_sr_buffer::ui_sr_buffer() :
-	cb(UI_SR_BUFFER_LENGHT)
-{
+	cb(UI_SR_BUFFER_LENGHT) {
 
 }
 
-void ui_sr_buffer::put_one_msg(const lib::sr_package_t& new_msg)
-{
+void ui_sr_buffer::put_one_msg(const lib::sr_package_t& new_msg) {
 
 	boost::mutex::scoped_lock lock(mtx);
 	cb.push_back(new_msg);
@@ -22,8 +20,7 @@ void ui_sr_buffer::put_one_msg(const lib::sr_package_t& new_msg)
 	return;
 }
 
-void ui_sr_buffer::get_one_msg(lib::sr_package_t& new_msg)
-{
+void ui_sr_buffer::get_one_msg(lib::sr_package_t& new_msg) {
 	boost::mutex::scoped_lock lock(mtx);
 	new_msg = cb.front();
 	cb.pop_front();
@@ -37,58 +34,47 @@ bool ui_sr_buffer::buffer_empty() // sprawdza czy bufor jest pusty
 	return cb.empty();
 }
 
-
 ui_ecp_buffer::ui_ecp_buffer() :
-	synchroniser()
-{
+	synchroniser() {
 
 	communication_state = UI_ECP_AFTER_REPLY;
 }
 
-
 busy_flagger::busy_flagger(busy_flag & _flag) :
-	flag(_flag)
-{
+	flag(_flag) {
 	flag.increment();
 }
 
-busy_flagger::~busy_flagger()
-{
+busy_flagger::~busy_flagger() {
 	flag.decrement();
 }
 
 busy_flag::busy_flag() :
-	counter(0)
-{
+	counter(0) {
 }
 
-void busy_flag::increment(void)
-{
+void busy_flag::increment(void) {
 	boost::mutex::scoped_lock lock(m_mutex);
 	counter++;
 }
 
-void busy_flag::decrement(void)
-{
+void busy_flag::decrement(void) {
 	boost::mutex::scoped_lock lock(m_mutex);
 	counter--;
 }
 
-bool busy_flag::is_busy() const
-{
+bool busy_flag::is_busy() const {
 	//	boost::mutex::scoped_lock lock(m_mutex);
 	return (counter);
 }
 
 function_execution_buffer::function_execution_buffer() :
-	has_command(false)
-{
+	has_command(false) {
 
 }
 
-void function_execution_buffer::command(command_function_t _com_fun)
-{
-	boost::unique_lock <boost::mutex> lock(mtx);
+void function_execution_buffer::command(command_function_t _com_fun) {
+	boost::unique_lock<boost::mutex> lock(mtx);
 
 	// assign command for execution
 	com_fun = _com_fun;
@@ -99,12 +85,11 @@ void function_execution_buffer::command(command_function_t _com_fun)
 	return;
 }
 
-int function_execution_buffer::wait_and_execute()
-{
+int function_execution_buffer::wait_and_execute() {
 	command_function_t popped_command;
 
 	{
-		boost::unique_lock <boost::mutex> lock(mtx);
+		boost::unique_lock<boost::mutex> lock(mtx);
 
 		while (!has_command) {
 			cond.wait(lock);
@@ -117,5 +102,34 @@ int function_execution_buffer::wait_and_execute()
 	busy_flagger flagger(communication_flag);
 	set_ui_state_notification(UI_N_BUSY);
 	return popped_command();
+}
+
+void feb_thread::operator()() {
+#if defined(__QNXNTO__)
+	sigset_t set;
+
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGALRM);
+
+	if (SignalProcmask(0, 0, SIG_BLOCK, &set, NULL) == -1) {
+		perror("SignalProcmask(edp_irp6p_tid)");
+	}
+#endif
+
+	while (true) {
+		feb.wait_and_execute();
+	}
+}
+
+feb_thread::feb_thread(function_execution_buffer & _feb) :
+	feb(_feb) {
+	thread_id = new boost::thread(boost::bind(&feb_thread::operator(), this));
+}
+
+feb_thread::~feb_thread() {
+	thread_id->interrupt();
+	thread_id->join(); // join it
+	delete thread_id;
 }
 
