8df39e2816e3ef004252e6a9260902c9eed4265a
Konrad Banachowicz <konradb3@gmail.com>
2010-05-16 18:38:17 +0800

    JointArray and MotorArray modified to use Eigen::VectorXd as base class.

--- src/lib/mrmath/MotorArray.h
+++ src/lib/mrmath/MotorArray.h
@@ -8,17 +8,32 @@
 #ifndef MOTORARRAY_H_
 #define MOTORARRAY_H_
 
-#include <vector>
+#include <Eigen/Core>
 
 namespace mrrocpp {
 namespace lib {
 
-class MotorArray : public std::vector<double>
+class MotorArray : public Eigen::VectorXd
 {
 public:
-  MotorArray() {}
-  MotorArray(int size) : std::vector<double> (size) {}
-  MotorArray(const double *ptr, size_t n) : std::vector<double> (ptr, ptr+n) {}
+  MotorArray() : Eigen::VectorXd () {}
+  MotorArray(int size) : Eigen::VectorXd (size) {}
+  MotorArray(const double *ptr, size_t n) : Eigen::VectorXd (n) 
+  {
+	  for(unsigned int i = 0; i<n; i++)
+	  {
+		m_storage.data()[i] = ptr[i];
+	  }
+  }
+
+  typedef Eigen::VectorXd Base;
+  template<typename OtherDerived>
+  MotorArray & operator= (const Eigen::MatrixBase <OtherDerived>& other)
+  {
+	this->Base::operator=(other);
+	return *this;
+  }
+
 };
 
 
