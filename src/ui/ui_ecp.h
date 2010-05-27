c37447f8970a715f710f45378a0337ae388d4872
Tomasz Winiarski <yoyek@segomo.ia.pw.edu.pl>
2010-05-27 15:12:33 +0200

    pip

--- src/ui/ui_ecp.h
+++ src/ui/ui_ecp.h
@@ -8,7 +8,7 @@
 #ifndef _UI_ECP_H
 #define _UI_ECP_H
 
-#include "ui/ui_ecp_r_speaker.h"
+#include "ui/src/speaker/ui_ecp_r_speaker.h"
 #include "ui/ui_ecp_r_tfg_and_conv.h"
 
 #include "lib/robot_consts/all_robots_const.h"
