72ef3d69950b545d20fad9bae904222b823bed18
Tomasz Winiarski <yoyek@segomo.ia.pw.edu.pl>
2010-06-02 10:58:39 +0200

    pip

---
+++ src/ui/src/bird_hand/wnd_bird_hand_command_and_status.h
@@ -0,0 +1,41 @@
+// -------------------------------------------------------------------------
+//                            ui_class.h
+// Definicje klasy Ui
+//
+// Ostatnia modyfikacja: 2010
+// -------------------------------------------------------------------------
+
+#ifndef __WND_BIRD_HAND_COMMAND_AND_STATUS_H
+#define __WND_BIRD_HAND_COMMAND_AND_STATUS_H
+
+#include "ui/ui.h"
+#include "ui/ui_robot.h"
+
+//
+//
+// KLASA UiRobotBirdHand
+//
+//
+
+
+// super klasa agregujaca porozrzucane struktury
+
+class Ui;
+class UiRobotBirdHand;
+
+class WndBirdHandCommandAndStatus {
+private:
+	Ui& ui;
+	UiRobotBirdHand& bird_hand;
+
+public:
+	bool is_open;
+
+	WndBirdHandCommandAndStatus(Ui& _ui, UiRobotBirdHand& _bird_hand);
+
+	int get_index_f_0_command();
+	int set_index_f_0_status();
+};
+
+#endif
+
