c16b6236c3bb41d5be795114c2b0466b8d62d534
Piotr Trojanek <piotr.trojanek@gmail.com>
2010-06-14 17:14:05 +0200

    commendted code removed

--- src/lib/r_buffer.cc
+++ src/lib/r_buffer.cc
@@ -13,23 +13,6 @@ r_buffer::r_buffer (void) :
 
   error_no.error0 = OK;
   error_no.error1 = OK;
-
- // address_byte = 0;
- /*
-  for (int i=0; i<4; i++)
-    for (int j=0; j<3; j++)
-      if (i==j)
-        arm.pf_def.arm_frame[j][i] = 1.0;
-      else
-        arm.pf_def.arm_frame[j][i] = 0.0;
-  for (int i=0; i<4; i++)
-    for (int j=0; j<3; j++)
-      if (i==j)
-        robot_model.tool_frame_def.tool_frame[j][i] = 1.0;
-      else
-        robot_model.tool_frame_def.tool_frame[j][i] = 0.0;
-        */
-//  robot_model.tool_frame_def.address_byte = 0;
 }
 
 } // namespace lib
