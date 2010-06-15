2c53c48cd9e40c8adf5fadb47c4c186dde796f63
Piotr Trojanek <piotr.trojanek@gmail.com>
2010-06-15 11:26:32 +0200

    EDP force -> coordinator: fixups

--- src/edp/common/edp_force_sensor.cc
+++ src/edp/common/edp_force_sensor.cc
@@ -216,12 +216,17 @@ void force::configure_sensor()
 	is_sensor_configured = true;
 
 	try {
-		coordinator = new RemoteAgent(MP_SECTION, 0);
+		if (!coordinator) {
+			coordinator = new RemoteAgent(MP_SECTION, 0);
+		}
 
-		remote_buffer = new RemoteBuffer<lib::Ft_vector>(*coordinator, master.robot_name + ":ForceTorque");
+		if(!remote_buffer) {
+			remote_buffer = new RemoteBuffer<lib::Ft_vector>(*coordinator, master.robot_name + ":ForceTorque");
+		}
 	}
 	catch (mrrocpp::lib::exception::System_error & e) {
 		sr_msg->message("Unable to connect to coordinator agent");
+		// TODO: fixme
 		coordinator = NULL;
 		remote_buffer = NULL;
 	}
