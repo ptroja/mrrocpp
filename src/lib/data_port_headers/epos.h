d0215d500fa6d5c073ae93b5bed1cdff9175ef9c
Tomasz Winiarski <yoyek@segomo.ia.pw.edu.pl>
2010-06-29 13:25:25 +0200

    swarm cleanup's

--- src/lib/data_port_headers/epos.h
+++ src/lib/data_port_headers/epos.h
@@ -5,10 +5,13 @@
 #if !defined(__EPOS_DATA_PORT_H)
 #define __EPOS_DATA_PORT_H
 
+#define EPOS_DATA_PORT_SERVOS_NUMBER 7
+
 namespace mrrocpp {
 namespace lib {
 
-enum EPOS_GEN_PROFILE {
+enum EPOS_GEN_PROFILE
+{
 	TRAPEZOIDAL_VELOCITY, CUBIC_POSITION, EPOS_GEN_PROFILE_NO_ACTION
 };
 
@@ -16,31 +19,35 @@ enum EPOS_GEN_PROFILE {
 #define EPOS_LOW_LEVEL_COMMAND_DATA_PORT "epos_low_level_command_data_port"
 #define EPOS_REPLY_DATA_REQUEST_PORT "epos_reply_data_request_port"
 
-struct single_controller_epos_reply {
+struct single_controller_epos_reply
+{
 	double position;
 	bool motion_in_progress;
 }__attribute__((__packed__));
 
-struct epos_gen_parameters {
+struct epos_gen_parameters
+{
 	EPOS_GEN_PROFILE profile_type;
-	double dm[6];
-	double aa[6];
-	double da[6];
-	double mv[6];
+	double dm[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double aa[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double da[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double mv[EPOS_DATA_PORT_SERVOS_NUMBER];
 };
 
-struct epos_low_level_command {
-	double em[6];
-	double emdm[6];
-	double aa[6];
-	double da[6];
-	double av[6];
+struct epos_low_level_command
+{
+	double em[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double emdm[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double aa[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double da[EPOS_DATA_PORT_SERVOS_NUMBER];
+	double av[EPOS_DATA_PORT_SERVOS_NUMBER];
 	double tt;
 	EPOS_GEN_PROFILE profile_type;
 };
 
-struct epos_reply {
-	single_controller_epos_reply epos_controller[6];
+struct epos_reply
+{
+	single_controller_epos_reply epos_controller[EPOS_DATA_PORT_SERVOS_NUMBER];
 	bool contact;
 };
 
--- src/lib/data_port_headers/smb.h
+++ src/lib/data_port_headers/smb.h
@@ -5,21 +5,26 @@
 #if !defined(__SMB_DATA_PORT_H)
 #define __SMB_DATA_PORT_H
 
+#define SMB_DATA_PORT_SERVOS_NUMBER 3
+
 namespace mrrocpp {
 namespace lib {
 
-struct smb_leg_reply {
+struct smb_leg_reply
+{
 	bool is_inserted;
 	bool is_locked;
 	bool insertion_in_progress;
 	bool locking_in_progress;
 }__attribute__((__packed__));
 
-enum SMB_PIN_INSERTION {
+enum SMB_PIN_INSERTION
+{
 	INSERT, WITHDRAWN, SMB_PIN_INSERTION_NO_ACTION
 }; // namespace mrrocpp
 
-enum SMB_PIN_LOCKING {
+enum SMB_PIN_LOCKING
+{
 	CLAMB, UNCLAMB, SMB_PIN_LOCKING_NO_ACTION
 }; // namespace mrrocpp
 
@@ -27,16 +32,19 @@ enum SMB_PIN_LOCKING {
 #define SMB_MULTI_PIN_LOCKING_DATA_PORT "smb_multi_pin_locking_data_port"
 #define SMB_MULTI_LEG_REPLY_DATA_REQUEST_PORT "smb_multi_leg_reply_data_request_port"
 
-struct smb_multi_pin_insertion {
-	SMB_PIN_INSERTION leg[3];
+struct smb_multi_pin_insertion
+{
+	SMB_PIN_INSERTION leg[SMB_DATA_PORT_SERVOS_NUMBER];
 };
 
-struct smb_multi_pin_locking {
-	SMB_PIN_LOCKING leg[3];
+struct smb_multi_pin_locking
+{
+	SMB_PIN_LOCKING leg[SMB_DATA_PORT_SERVOS_NUMBER];
 };
 
-struct smb_multi_leg_reply {
-	smb_leg_reply leg[3];
+struct smb_multi_leg_reply
+{
+	smb_leg_reply leg[SMB_DATA_PORT_SERVOS_NUMBER];
 };
 
 }
