b14899160a19a8f404822b9ff6272b2fd0b19a11
Rafal Tulwin <rtulwin@gmail.com>
2010-07-01 01:52:01 +0200

    small fix

--- src/generator/ecp/trajectory_interpolator/bang_bang_interpolator.cc
+++ src/generator/ecp/trajectory_interpolator/bang_bang_interpolator.cc
@@ -27,7 +27,7 @@ bool bang_bang_interpolator::interpolate_relative_pose(vector<ecp_mp::common::tr
 	vector<double> coordinates (it->axes_num);
 	for (int i = 0; i < it->interpolation_node_no; i++) {
 		for (int j = 0; j < it->axes_num; j++) {
-			coordinates[j] = it->k[j] * mc * it->v_r[j];
+			//coordinates[j] = it->k[j] * mc * it->v_r[j];
 		}
 		cv.push_back(coordinates);
 	}
@@ -42,7 +42,7 @@ bool bang_bang_interpolator::interpolate_absolute_pose(vector<ecp_mp::common::tr
 	vector<double> coordinates (it->axes_num);
 	for (i = 0; i < it->interpolation_node_no; i++) {
 		for (j = 0; j < it->axes_num; j++) {
-			coordinates[j] = it->start_position[j] + (it->k[j] * (i+1) * mc * it->v_r[j]);
+			//coordinates[j] = it->start_position[j] + (it->k[j] * (i+1) * mc * it->v_r[j]);
 		}
 		cv.push_back(coordinates);
 	}
