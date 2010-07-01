f2d66230d46d05d7652211af1dc489f552f9f966
Tomasz Winiarski <yoyek@segomo.ia.pw.edu.pl>
2010-07-01 11:45:19 +0200

    fix

---
+++ src/lib/single_thread_port.cc
@@ -0,0 +1,30 @@
+/**
+ * \file single_thread_port.h
+ *
+ * \date 2010
+ * \author yoyek
+ *
+ * \brief Template class for single robot communication port
+ */
+
+#include <string>
+#include <map>
+
+#include "single_thread_port.h"
+namespace mrrocpp {
+namespace lib {
+
+single_thread_port_interface::single_thread_port_interface(std::string _name, single_thread_port_manager & _port_manager) :
+	name(_name), new_data(false)
+{
+	_port_manager.add_port(this);
+}
+
+std::string single_thread_port_interface::get_name()
+{
+	return name;
+}
+
+}
+}
+
