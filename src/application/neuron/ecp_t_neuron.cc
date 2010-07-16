f4614ba2e2e2eaf28aa64ad868f80b02614c7840
Youmak <mebmot@wp.pl>
2010-07-16 13:24:30 +0200

    task changed

--- src/application/neuron/ecp_t_neuron.cc
+++ src/application/neuron/ecp_t_neuron.cc
@@ -19,13 +19,13 @@ namespace task {
 /*==============================Constructor==================================*/
 //Constructors
 Neuron::Neuron(lib::configurator &_config): task(_config){
-	sensor_m[ecp_mp::sensor::ECP_MP_NEURON_SENSOR] = new ecp_mp::sensor::neuron_sensor(config);
+	//sensor_m[ecp_mp::sensor::ECP_MP_NEURON_SENSOR] = new ecp_mp::sensor::neuron_sensor(config);
 	ecp_m_robot=new irp6ot_m::robot(*this);				//initialization of robot
 
 	//sgen=new common::generator::smooth(*this, true);
 	//sgen2 = new common::generator::smooth(*this, true);
-	neurong = new common::generator::neuron_generator(*this);
-	neurong->sensor_m=sensor_m;
+	//neurong = new common::generator::neuron_generator(*this);
+	//neurong->sensor_m=sensor_m;
 	sr_ecp_msg->message("ECP loaded Neuron");
 };
 
@@ -39,6 +39,9 @@ Neuron::~Neuron(){
 void Neuron::mp_2_ecp_next_state_string_handler(void){
 	uint8_t choice;
 	if (mp_2_ecp_next_state_string == ecp_mp::task::ECP_T_NEURON) {
+		sensor_m[ecp_mp::sensor::ECP_MP_NEURON_SENSOR] = new ecp_mp::sensor::neuron_sensor(config);
+		neurong = new common::generator::neuron_generator(*this);
+		neurong->sensor_m=sensor_m;
 		choice=choose_option ("Do you want to play: 1 - Black(blue), or 2 - White(red)", 2);
 		sr_ecp_msg->message("Game canceled");
 
