7dcb9274ba61488ee48d164861d493f5d07d9ef9
Mateusz Boryn <mateusz.boryn@gmail.com>
2010-07-28 18:42:06 +0200

    conveyor uniform accel generator

---
+++ src/application/servovision/visualservo_tester/ecp_g_conveyor_uniform_acceleration.h
@@ -0,0 +1,51 @@
+/*
+ * ecp_g_conveyor_uniform_acceleration.h
+ *
+ *  Created on: May 20, 2010
+ *      Author: mboryn
+ */
+
+#ifndef ECP_G_CONVEYOR_UNIFORM_ACCELERATION_H_
+#define ECP_G_CONVEYOR_UNIFORM_ACCELERATION_H_
+
+#include <string>
+
+#include "base/ecp/ecp_generator.h"
+
+namespace mrrocpp {
+
+namespace ecp {
+
+namespace common {
+
+namespace generator {
+
+class ecp_g_conveyor_uniform_acceleration : public mrrocpp::ecp::common::generator::generator
+{
+public:
+	ecp_g_conveyor_uniform_acceleration(mrrocpp::ecp::common::task::task & ecp_task, const std::string& section_name);
+	virtual ~ecp_g_conveyor_uniform_acceleration();
+
+	bool first_step();
+	bool next_step();
+private:
+
+	int motion_steps;
+	double dt;
+	double A;
+	double f;
+	double t;
+
+	bool initial_position_saved;
+	double initial_position;
+};
+
+}//namespace
+
+}
+
+}
+
+}
+
+#endif /* ECP_G_CONVEYOR_UNIFORM_ACCELERATION_H_ */
