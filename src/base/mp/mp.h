f13c1ae577a94f86eb70c422fa65019fb01d71e2
Tomasz Winiarski <tomrobotics@gmail.com>
2010-08-20 13:49:49 +0200

    mp.h fix'es

--- src/base/mp/mp.h
+++ src/base/mp/mp.h
@@ -8,31 +8,11 @@
 #if !defined(__MP_H)
 #define __MP_H
 
-#include <map>
-#include <cstdio>
-
-#if defined(__QNXNTO__)
-#include <sys/iofunc.h>
-#include <sys/dispatch.h>
-#endif
-
-#include "lib/impconst.h"
-#include "lib/com_buf.h"
 #include "lib/configurator.h"
-
-namespace mrrocpp {
-namespace mp {
-namespace robot {
-class robot;
-}
-
-} // namespace mp
-} // namespace mrrocpp
 
 // to fix forward declaration issues
 #include "base/mp/mp_generator.h"
 #include "base/mp/mp_task.h"
 #include "base/mp/mp_robot.h"
-#include "lib/com_buf.h"
 
 #endif
