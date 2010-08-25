4803a0192549da2c4c67a90f2a1ac0eb0a9de2d0
Tomasz Winiarski <tomrobotics@gmail.com>
2010-08-25 14:30:58 +0200

    dox files fix

--- src/robot/smb/ecp_r_smb.cc
+++ src/robot/smb/ecp_r_smb.cc
@@ -7,7 +7,7 @@
 // -------------------------------------------------------------------------
 
 #include "base/lib/impconst.h"
-#include "base/lib/com_buf.h"
+
 #include "base/lib/mis_fun.h"
 
 #include "robot/smb/ecp_r_smb.h"
