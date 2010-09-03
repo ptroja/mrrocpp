b3df1ce00452399c95f696d458684137aadc1a08
Tomasz Winiarski <tomrobotics@gmail.com>
2010-09-03 15:51:27 +0200

    mp robots doc

--- src/robot/irp6p_m/mp_r_irp6p_m.h
+++ src/robot/irp6p_m/mp_r_irp6p_m.h
@@ -16,9 +16,19 @@ namespace mrrocpp {
 namespace mp {
 namespace robot {
 
+/*!
+ * @brief Irp6 postument manipulator mp robot class
+ *
+ * @author twiniars <twiniars@ia.pw.edu.pl>, Warsaw University of Technology
+ * @ingroup irp6p_m
+ */
 class irp6p_m : public robot
 {
 public:
+	/**
+	 * @brief constructor
+	 * @param mp_object_l mp task object reference
+	 */
 	irp6p_m(task::task &mp_object_l);
 };
 
