d013f6af7a855d5cafe4d52af2293fb4c4d296b9
Tomasz Winiarski <tomrobotics@gmail.com>
2010-09-20 16:52:59 +0200

    mp task doc

--- src/base/mp/mp_task.h
+++ src/base/mp/mp_task.h
@@ -52,46 +52,111 @@ namespace task {
 		}\
 		})
 
-// klasa globalna dla calego procesu MP
+/*!
+ * @brief Base class of all mp tasks
+ *
+ * @author twiniars <twiniars@ia.pw.edu.pl>, Warsaw University of Technology
+ * @ingroup mp
+ */
 class task : public ecp_mp::task::task
 {
 private:
+	/**
+	 * @brief Initializes communication channels
+	 */
 	void initialize_communication(void);
 
 public:
 #if !defined(USE_MESSIP_SRR)
+	/**
+	 * @brief communication channels descriptors
+	 */
 	static name_attach_t *mp_pulse_attach;
 #else
 	static messip_channel_t *mp_pulse_attach;
 #endif
-	/// KONSTRUKTORY
+
+	/**
+	 * @brief Constructor
+	 * @param _config configurator object reference.
+	 */
 	task(lib::configurator &_config);
+
+	/**
+	 * @brief Destructor
+	 */
 	virtual ~task(void);
 
-	/// utworzenie robotow
+	/**
+	 * @brief pure virtual method to create robots
+	 * it have to be reimplemented in inherited classes
+	 */
 	virtual void create_robots(void) =0;
 
+	/**
+	 * @brief Waits for stop pulse from UI and terminated all ECP's
+	 */
 	void stop_and_terminate(void);
 
-	// oczekiwanie na puls z ECP
+	/**
+	 * @brief Enum of two possible pulse receive variants (BLOCK/NONBLOCK)
+	 */
 	typedef enum _MP_RECEIVE_PULSE_ENUM
 	{
 		NONBLOCK, BLOCK
 	} RECEIVE_PULSE_MODE;
 
+	/**
+	 * @brief Enum of three possible variants of pulse origin processes
+	 */
 	typedef enum _WAIT_FOR_NEW_PULSE_ENUM
 	{
 		NEW_ECP_PULSE, NEW_UI_PULSE, NEW_UI_OR_ECP_PULSE
 	} WAIT_FOR_NEW_PULSE_MODE;
 
+	/**
+	 * @brief sets the goal pf player controlled robot
+	 * @param robot_l robot label
+	 * @param goal motion goal
+	 */
 	void set_next_playerpos_goal(lib::robot_name_t robot_l, const lib::playerpos_goal_t &goal);
 
+	/**
+	 * @brief sets the next state of ECP
+	 * it calls dedicated generator and then sends new command in generator Move instruction
+	 * @param l_state state label sent to ECP
+	 * @param l_variant variant value sent to ECP
+	 * @param l_string optional string sent to ECP
+	 * @param str_len string length
+	 * @param number_of_robots number of robots to receive command
+	 * @param ... robots labels
+	 */
 	void
 			set_next_ecps_state(std::string l_state, int l_variant, const char* l_string, int str_len, int number_of_robots, ...);
 
+	/**
+	 * @brief sends end motion command to ECP's
+	 * it calls dedicated generator and then sends command in generator Move instruction
+	 * @param number_of_robots number of robots to receive command
+	 * @param ... robots labels
+	 */
 	void send_end_motion_to_ecps(int number_of_robots, ...);
+
+	/**
+	 * @brief sends end motion command to ECP's - mkisiel xml task version
+	 * it calls dedicated generator and then sends command in generator Move instruction
+	 * @param number_of_robots number of robots to receive command
+	 * @param properRobotsSet pointer to robot list
+	 */
 	void send_end_motion_to_ecps(int number_of_robots, lib::robot_name_t *properRobotsSet);
 
+	/**
+	 * @brief runs extended empty generator
+	 * it calls dedicated generator and then sends command in generator Move instruction
+	 * @param activate_trigger determines if mp_trigger finishes generator execution
+	 * @param number_of_robots number of robots to receive command
+	 * @param ... robots labels
+	 */
 	void run_extended_empty_gen(bool activate_trigger, int number_of_robots, ...);
 	void
 			run_extended_empty_generator_for_set_of_robots_and_wait_for_task_termination_message_of_another_set_of_robots(int number_of_robots_to_move, int number_of_robots_to_wait_for_task_termin, ...);
