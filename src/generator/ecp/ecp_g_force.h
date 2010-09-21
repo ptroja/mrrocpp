bc8d6a9856141795c5c681810cc6299cb38c0675
Tomasz Winiarski <tomrobotics@gmail.com>
2010-09-21 21:05:02 +0200

    force gen doc

--- src/generator/ecp/ecp_g_force.h
+++ src/generator/ecp/ecp_g_force.h
@@ -41,40 +41,87 @@ private:
 	static const int USLEEP_TIME = 10000;
 
 	/**
-	 * @brief single macrostep duration in mikrosecond
+	 * @brief weight difference to detect
 	 */
-	double weight_difference; // roznica wagi do wykrycia
+	double weight_difference;
+
+	/**
+	 * @brief cyclic buffer of measured weights
+	 */
 	double weight_in_cyclic_buffer[WEIGHT_MEASSURE_GENERATOR_BUFFER_SIZE];
+
+	/**
+	 * @brief current pointer of cyclic buffer
+	 */
 	int current_buffer_pointer;
-	double initial_weight; // pierwszy zmierzony ciezar
-	bool initial_weight_counted; // czy wyznaczono juz poczatkowy ciezar
-	int catch_lag; // ilosc potwierdzen zmiany masy do zwrocenia false;
-	int initial_catch_lag; // ilosc potwierdzen zmiany masy do zwrocenia false;
-	double catch_time; // czas przez ktory ma byc stwierdzona zmian ci???aru
+
+	/**
+	 * @brief first measured weight
+	 */
+	double initial_weight;
+
+	/**
+	 * @brief flag of fist weight measure done
+	 */
+	bool initial_weight_counted;
+
+	/**
+	 * @brief the current number of macrosteps to execute after desired weight difference is detected before the generator is finished
+	 */
+	int catch_lag;
+
+	/**
+	 * @brief the initial number of macrosteps to execute after desired weight difference is detected before the generator is finished
+	 */
+	int initial_catch_lag;
+
+	/**
+	 * @brief time to wait after desired weight difference is detected before the generator is finished
+	 */
+	double catch_time;
+
+	/**
+	 * @brief set if desired weight difference is detected before the generator is finished
+	 */
 	bool terminate_state_recognized; // wykryto warunek koncowy
 
-	// wstawienie elementu do bufora cyklicznego
+	/**
+	 * @brief insert measured weight to buffer
+	 * @param fx weight
+	 */
 	void insert_in_buffer(const double fx);
 
-	// wyznaczenie sredniej arytmetycznej ciezarow zapisanych w buforze
+	/**
+	 * @brief returns average weight from buffer
+	 * @return average weight from buffer
+	 */
 	double check_average_weight_in_buffer(void) const;
 
-	// czyszczenie bufora cyklicznego
+	/**
+	 * @brief clears cyclic buffer
+	 */
 	void clear_buffer();
 
 public:
 
-	// ustawia nowa roznice wag
+	/**
+	 * @brief sets desired weight difference to detect
+	 */
 	void set_weight_difference(const double _weight_difference);
 
-	// konstruktor
+	/**
+	 * @brief Constructor
+	 * @param _ecp_task ecp task object reference.
+	 * @param _weight_difference desired weight difference (optional)
+	 * @param _catch_time desired catch time to wait after weight difference is detected
+	 */
+
 	weight_meassure(common::task::task& _ecp_task, double _weight_difference = 0.0, double _catch_time = 1.0);
 
 	bool first_step();
 	bool next_step();
 
-}; // end:
-
+};
 
 // --------------------------------------------------------------------------
 // Generator trajektorii dla zadan z wodzeniem za nos w tff ze zmiana orientacji
