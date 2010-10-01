0b7e99d64bb11d442e79537e814e7719566c2721
Tomasz Winiarski <tomrobotics@gmail.com>
2010-10-01 12:05:29 +0200

    compilation fix

--- src/application/haar/ecp_t_haar_irp6ot.h
+++ src/application/haar/ecp_t_haar_irp6ot.h
@@ -9,7 +9,7 @@
 #include "base/lib/sr/srlib.h"
 
 #include "robot/irp6ot_m/ecp_r_irp6ot_m.h"
-#include "robot/irp6ot_m/generator/ecp_g_vis_ib_eih_planar_irp6ot.h"
+#include "generator/ecp/vision/ecp_g_vis_ib_eih_planar_irp6ot.h"
 #include "generator/ecp/force/ecp_g_bias_edp_force.h"
 #include "generator/ecp/force/ecp_g_tff_gripper_approach.h"
 #include "ecp_g_rotate_gripper.h"
