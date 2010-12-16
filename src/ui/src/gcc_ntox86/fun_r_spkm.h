c9116e85bcc8e608f7a97f1a81ea3dd949d88c74
Tomasz Winiarski <tomrobotics@gmail.com>
2010-12-16 20:52:54 +0100

    ui proto.h file added divided into particular for each of the definition file

---
+++ src/ui/src/gcc_ntox86/fun_r_spkm.h
@@ -0,0 +1,5 @@
+/* ../fun_r_spkm.cc */
+int EDP_spkm_create(PtWidget_t *widget, ApInfo_t *apinfo, PtCallbackInfo_t *cbinfo);
+int EDP_spkm_create_int(PtWidget_t *widget, ApInfo_t *apinfo, PtCallbackInfo_t *cbinfo);
+int EDP_spkm_slay(PtWidget_t *widget, ApInfo_t *apinfo, PtCallbackInfo_t *cbinfo);
+int EDP_spkm_synchronise_int(PtWidget_t *widget, ApInfo_t *apinfo, PtCallbackInfo_t *cbinfo);
