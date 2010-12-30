6d464f2f228080bc53850d54d8d109e8e1870eaf
Tomasz Winiarski <tomrobotics@gmail.com>
2010-12-30 23:27:08 +0100

    UI bird_hand simplification

--- src/ui/src/bird_hand/wnd_bird_hand_command_and_status.h
+++ src/ui/src/bird_hand/wnd_bird_hand_command_and_status.h
@@ -52,98 +52,12 @@ public:
 	int
 			get_finger_command(lib::bird_hand::single_joint_command &finger, PtWidget_t *ABW_position, PtWidget_t *ABW_torque, PtWidget_t *ABW_damping);
 	int
-			set_fingerstatus(lib::bird_hand::single_joint_status &finger, PtWidget_t *ABW_position, PtWidget_t *ABW_torque, PtWidget_t *ABW_current, PtWidget_t *ABW_limit_1, PtWidget_t *ABW_limit_2, PtWidget_t *ABW_limit_3, PtWidget_t *ABW_limit_4, PtWidget_t *ABW_limit_5, PtWidget_t *ABW_limit_6, PtWidget_t *ABW_limit_7, PtWidget_t *ABW_limit_8);
+			set_finger_status(lib::bird_hand::single_joint_status &finger, PtWidget_t *ABW_position, PtWidget_t *ABW_torque, PtWidget_t *ABW_current, PtWidget_t *ABW_limit_1, PtWidget_t *ABW_limit_2, PtWidget_t *ABW_limit_3, PtWidget_t *ABW_limit_4, PtWidget_t *ABW_limit_5, PtWidget_t *ABW_limit_6, PtWidget_t *ABW_limit_7, PtWidget_t *ABW_limit_8);
 
 	int
 			copy_finger_command(lib::bird_hand::single_joint_command &finger, PtWidget_t *ABW_current, PtWidget_t *ABW_desired);
 
-	//
-	//
-	// thumb_f_0
-	//
-	//
 
-	int get_thumb_f_0_command();
-	int get_variant_thumb_f_0_command();
-	int set_thumb_f_0_status();
-	int copy_thumb_f_0_command();
-
-	//
-	//
-	// thumb_f_1
-	//
-	//
-
-	int get_thumb_f_1_command();
-	int get_variant_thumb_f_1_command();
-	int set_thumb_f_1_status();
-	int copy_thumb_f_1_command();
-
-	//
-	//
-	// index_f_0
-	//
-	//
-
-	int get_index_f_0_command();
-	int get_variant_index_f_0_command();
-	int set_index_f_0_status();
-	int copy_index_f_0_command();
-
-	//
-	//
-	// index_f_1
-	//
-	//
-
-	int get_index_f_1_command();
-	int get_variant_index_f_1_command();
-	int set_index_f_1_status();
-	int copy_index_f_1_command();
-
-	//
-	//
-	// index_f_2
-	//
-	//
-
-	int get_index_f_2_command();
-	int get_variant_index_f_2_command();
-	int set_index_f_2_status();
-	int copy_index_f_2_command();
-
-	//
-	//
-	// ring_f_0
-	//
-	//
-
-	int get_ring_f_0_command();
-	int get_variant_ring_f_0_command();
-	int set_ring_f_0_status();
-	int copy_ring_f_0_command();
-
-	//
-	//
-	// ring_f_1
-	//
-	//
-
-	int get_ring_f_1_command();
-	int get_variant_ring_f_1_command();
-	int set_ring_f_1_status();
-	int copy_ring_f_1_command();
-
-	//
-	//
-	// ring_f_2
-	//
-	//
-
-	int get_ring_f_2_command();
-	int get_variant_ring_f_2_command();
-	int set_ring_f_2_status();
-	int copy_ring_f_2_command();
 
 };
 
