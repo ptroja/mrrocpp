d1d01a257dd745dc2ab02e524a70474082e9dde7
Piotr Trojanek <piotr.trojanek@gmail.com>
2010-12-31 10:31:30 +0100

    serialized buffers size restored to 500 bytes

--- src/base/lib/com_buf.h
+++ src/base/lib/com_buf.h
@@ -36,8 +36,8 @@
 namespace mrrocpp {
 namespace lib {
 
-#define ECP_EDP_SERIALIZED_COMMAND_SIZE 750
-#define EDP_ECP_SERIALIZED_REPLY_SIZE 750
+#define ECP_EDP_SERIALIZED_COMMAND_SIZE 500
+#define EDP_ECP_SERIALIZED_REPLY_SIZE 500
 
 #if !defined(USE_MESSIP_SRR)
 typedef int fd_client_t;
