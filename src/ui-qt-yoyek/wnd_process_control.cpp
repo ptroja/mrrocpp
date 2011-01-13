cb16514a4cd16da1f335ef7c6daf7ba876788525
Tomasz Winiarski <tomrobotics@gmail.com>
2011-01-14 00:55:01 +0100

    ui-qt process control

--- src/ui-qt-yoyek/wnd_process_control.cpp
+++ src/ui-qt-yoyek/wnd_process_control.cpp
@@ -1,12 +1,33 @@
 #include "wnd_process_control.h"
+#include "interface.h"
 
-wnd_process_control::wnd_process_control(QWidget *parent)
-    : QMainWindow(parent)
+wnd_process_control::wnd_process_control(mrrocpp::ui::common::Interface& _interface, QWidget *parent) :
+	QMainWindow(parent), interface(_interface)
 {
 	ui.setupUi(this);
 }
 
 wnd_process_control::~wnd_process_control()
+{
+
+}
+
+void wnd_process_control::on_mp_start_pushButton_clicked()
+{
+
+}
+
+void wnd_process_control::on_mp_stop_pushButton_clicked()
+{
+
+}
+
+void wnd_process_control::on_mp_pause_pushButton_clicked()
+{
+
+}
+
+void wnd_process_control::on_mp_resume_pushButton_clicked()
 {
 
 }
--- src/ui-qt-yoyek/wnd_process_control.h
+++ src/ui-qt-yoyek/wnd_process_control.h
@@ -4,16 +4,32 @@
 #include <QtGui/QMainWindow>
 #include "ui_wnd_process_control.h"
 
+namespace mrrocpp {
+namespace ui {
+namespace common {
+class Interface;
+}
+}
+}
+
 class wnd_process_control : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    wnd_process_control(QWidget *parent = 0);
-    ~wnd_process_control();
+	wnd_process_control(mrrocpp::ui::common::Interface& _interface, QWidget *parent = 0);
+	~wnd_process_control();
 
 private:
-    Ui::wnd_process_controlClass ui;
+	Ui::wnd_process_controlClass ui;
+	mrrocpp::ui::common::Interface& interface;
+
+private slots:
+	void on_mp_start_pushButton_clicked();
+	void on_mp_stop_pushButton_clicked();
+	void on_mp_pause_pushButton_clicked();
+	void on_mp_resume_pushButton_clicked();
+
 };
 
 #endif // WND_PROCESS_CONTROL_H
