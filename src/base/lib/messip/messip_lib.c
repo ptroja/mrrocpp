7fb30262b51d40e04d6c9e406715b66c587c560b
Piotr Trojanek <piotr.trojanek@gmail.com>
2011-01-18 11:27:00 +0100

    IP framgented message cleanup

--- src/base/lib/messip/messip_lib.c
+++ src/base/lib/messip/messip_lib.c
@@ -1985,14 +1985,15 @@ messip_receive( messip_channel_t * ch,
 		return -1;
 	}
 	if(dcount != (ssize_t) (sizeof( uint32_t ) + len_to_read)) {
+		// Warn if the packet has been fragmented by the TCP/IP stack
 #if defined(__linux__)
-		fprintf(stderr, "LINUX\n");
+		fprintf(stderr, "LINUX: ");
 #elif defined(__QNXNTO__)
-		fprintf(stderr, "QNX\n");
+		fprintf(stderr, "QNX: ");
 #else
-		fprintf(stderr, "OTHER\n");
+		fprintf(stderr, "OTHER: ");
 #endif
-		fprintf(stderr, "MTU test: dcount %zd =? header+data %zd\n", dcount, (ssize_t) (sizeof( uint32_t ) + len_to_read));
+		fprintf(stderr, "IP packet fragmented: %zd of %zd bytes\n", dcount, (ssize_t) (sizeof( uint32_t ) + len_to_read));
 	}
 	assert( dcount >= sizeof( uint32_t ) );
 	ch->datalenr = dcount - sizeof(uint32_t);
