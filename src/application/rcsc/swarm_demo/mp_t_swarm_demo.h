77ff996aa89f09744c5d6be0fd28d20af6a371c0
askubis <adam.skubis@gmail.com>
2011-01-20 14:27:43 +0100

    added and changed

---
+++ src/application/rcsc/swarm_demo/mp_t_swarm_demo.h
@@ -0,0 +1,34 @@
+#if !defined(__MP_T_SWARM_DEMO_H)
+#define __MP_T_SWARM_DEMO_H
+
+namespace mrrocpp {
+namespace mp {
+namespace task {
+
+/**
+ * @defgroup swarm_demo swarm_demo
+ * @ingroup application
+ * A swarm demo application
+ */
+
+class swarm_demo : public task
+{
+protected:
+
+public:
+
+	/**
+	 * Constructor.
+	 */
+	swarm_demo(lib::configurator &_config);
+	/// utworzenie robotow
+	void create_robots(void);
+	void main_task_algorithm(void);
+
+};
+
+} // namespace task
+} // namespace mp
+} // namespace mrrocpp
+
+#endif
