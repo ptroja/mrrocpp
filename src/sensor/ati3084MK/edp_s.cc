803b962f37cf7058f300c3866d453fe5d4abad37
Piotr Trojanek <piotr.trojanek@gmail.com>
2011-01-31 16:41:06 +0100

    Revert "force_sensor_test_mode moved to force() class"
    This reverts commit 7170c69e45a9861aaed97d0899be7e85d2b3913a.

--- src/sensor/ati3084MK/edp_s.cc
+++ src/sensor/ati3084MK/edp_s.cc
@@ -44,7 +44,7 @@ void ATI3084_force::connect_to_hardware(void)
 
 ATI3084_force::~ATI3084_force(void)
 {
-	if (!force_sensor_test_mode) {
+	if (!(master.force_sensor_test_mode)) {
 		disconnect_from_hardware();
 	}
 
