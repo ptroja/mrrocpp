fdb28ef1d0a3b7bd5c1c6ac03f521e69ddeb8b0e
Michal Walecki <wacek@harry.ia.pw.edu.pl>
2011-02-18 16:38:57 +0100

    :-)

--- src/ui-qt-yoyek/sarkofag/wgt_sarkofag_inc.h
+++ src/ui-qt-yoyek/sarkofag/wgt_sarkofag_inc.h
@@ -28,15 +28,40 @@ public:
     wgt_sarkofag_inc(mrrocpp::ui::common::Interface& _interface, mrrocpp::ui::sarkofag::UiRobot& _robot, QWidget *parent = 0);
     ~wgt_sarkofag_inc();
 
+	void synchro_depended_init();
 
 private:
-    Ui::wgt_sarkofag_incClass ui;
+	Ui::wgt_sarkofag_incClass ui;
 	mrrocpp::ui::sarkofag::UiRobot& robot;
+
+	int init();
+	int copy();
+
+	int synchro_depended_widgets_disable(bool _set_disabled);
+
+	int set_single_axis(int axis, QDoubleSpinBox* qdsb_mcur, QDoubleSpinBox* qdsb_cur_p, QAbstractButton* qab_mip);
+	int get_desired_position();
+	int move_it();
+
+	QTimer *timer;
+
+signals:
+	void synchro_depended_init_signal();
 
 private slots:
 
+	void synchro_depended_init_slot();
 
+	void on_timer_slot();
+	void on_pushButton_read_clicked();
+	void on_pushButton_export_clicked();
+	void on_pushButton_import_clicked();
+	void on_pushButton_copy_clicked();
+	void on_pushButton_stop_clicked();
 
+	void on_pushButton_execute_clicked();
+	void on_pushButton_0l_clicked();
+	void on_pushButton_0r_clicked();
 
 };
 
