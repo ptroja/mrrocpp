8aba413989fc728809efeb54d8918451c791907a
twiniars <tmwiniarski@gmail.com>
2011-03-08 19:13:25 -0800

    ui-qt pc in progress

--- src/ui-qt/base/wgt_process_control.cpp
+++ src/ui-qt/base/wgt_process_control.cpp
@@ -84,20 +84,18 @@ void wgt_process_control::on_all_reader_trigger_pushButton_clicked()
 int wgt_process_control::init(bool do_it)
 
 {
+
+	bool wlacz_PtButton_wnd_processes_control_all_reader_start = false;
+	bool wlacz_PtButton_wnd_processes_control_all_reader_stop = false;
+	bool wlacz_PtButton_wnd_processes_control_all_reader_trigger = false;
+
+	// Dla READER'A
+
+	ui.all_reader_start_pushButton->setDisabled(true);
+	ui.all_reader_stop_pushButton->setDisabled(true);
+	ui.all_reader_trigger_pushButton->setDisabled(true);
+
 	/* TR
-
-
-	 if (interface.is_process_control_window_open) {
-
-	 bool wlacz_PtButton_wnd_processes_control_all_reader_start = false;
-	 bool wlacz_PtButton_wnd_processes_control_all_reader_stop = false;
-	 bool wlacz_PtButton_wnd_processes_control_all_reader_trigger = false;
-
-	 // Dla READER'A
-
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_all_reader_start);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_all_reader_stop);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_all_reader_trigger);
 
 	 // Dla irp6_on_track
 
@@ -110,78 +108,75 @@ int wgt_process_control::init(bool do_it)
 	 // Dla conveyor
 
 	 interface.conveyor->process_control_window_conveyor_section_init(wlacz_PtButton_wnd_processes_control_all_reader_start, wlacz_PtButton_wnd_processes_control_all_reader_stop, wlacz_PtButton_wnd_processes_control_all_reader_trigger);
-
-	 // Dla speakera - wylaczone
-
-	 // Dla irp6_mechatronika
-
-
-	 // All reader's pulse buttons
-	 if (wlacz_PtButton_wnd_processes_control_all_reader_start) {
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_all_reader_start);
-	 }
-
-	 if (wlacz_PtButton_wnd_processes_control_all_reader_stop) {
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_all_reader_stop);
-	 }
-
-	 if (wlacz_PtButton_wnd_processes_control_all_reader_trigger) {
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_all_reader_trigger);
-	 }
-
-	 // Dla mp i ecp
-	 if ((interface.mp.state != interface.mp.last_state) || (do_it)) {
-	 interface.process_control_window_renew = false;
-
-	 switch (interface.mp.state)
-	 {
-	 case ui::common::UI_MP_PERMITED_TO_RUN:
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_start);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_stop);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_pause);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_resume);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_trigger);
-
-	 block_all_ecp_trigger_widgets(NULL, NULL, NULL);
-	 break;
-	 case ui::common::UI_MP_WAITING_FOR_START_PULSE:
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_start);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_stop);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_pause);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_resume);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_trigger);
-
-	 block_all_ecp_trigger_widgets(NULL, NULL, NULL);
-	 break;
-	 case ui::common::UI_MP_TASK_RUNNING:
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_start);
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_stop);
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_pause);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_resume);
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_trigger);
-
-	 unblock_all_ecp_trigger_widgets(NULL, NULL, NULL);
-	 break;
-	 case ui::common::UI_MP_TASK_PAUSED:
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_start);
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_stop);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_pause);
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_mp_pulse_resume);
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_mp_pulse_trigger);
-
-	 block_all_ecp_trigger_widgets(NULL, NULL, NULL);
-	 break;
-	 default:
-	 break;
-	 }
-
-	 interface.mp.last_state = interface.mp.state;
-
-	 }
-
-	 PtDamageWidget(ABW_wnd_processes_control);
-	 }
 	 */
+
+	// All reader's pulse buttons
+	if (wlacz_PtButton_wnd_processes_control_all_reader_start) {
+		ui.all_reader_start_pushButton->setDisabled(false);
+
+	}
+
+	if (wlacz_PtButton_wnd_processes_control_all_reader_stop) {
+		ui.all_reader_stop_pushButton->setDisabled(false);
+
+	}
+
+	if (wlacz_PtButton_wnd_processes_control_all_reader_trigger) {
+		ui.all_reader_trigger_pushButton->setDisabled(false);
+
+	}
+
+	// Dla mp i ecp
+	if ((interface.mp.state != interface.mp.last_state) || (do_it)) {
+
+		switch (interface.mp.state)
+		{
+			case ui::common::UI_MP_PERMITED_TO_RUN:
+				ui.mp_start_pushButton->setDisabled(true);
+				ui.mp_stop_pushButton->setDisabled(true);
+				ui.mp_pause_pushButton->setDisabled(true);
+				ui.mp_resume_pushButton->setDisabled(true);
+				ui.mp_trigger_pushButton->setDisabled(true);
+
+				block_all_ecp_trigger_widgets();
+				break;
+			case ui::common::UI_MP_WAITING_FOR_START_PULSE:
+				ui.mp_start_pushButton->setDisabled(false);
+				ui.mp_stop_pushButton->setDisabled(true);
+				ui.mp_pause_pushButton->setDisabled(true);
+				ui.mp_resume_pushButton->setDisabled(true);
+				ui.mp_trigger_pushButton->setDisabled(true);
+
+				block_all_ecp_trigger_widgets();
+				break;
+			case ui::common::UI_MP_TASK_RUNNING:
+
+				ui.mp_start_pushButton->setDisabled(true);
+				ui.mp_stop_pushButton->setDisabled(false);
+				ui.mp_pause_pushButton->setDisabled(false);
+				ui.mp_resume_pushButton->setDisabled(true);
+				ui.mp_trigger_pushButton->setDisabled(false);
+
+				unblock_all_ecp_trigger_widgets();
+				break;
+			case ui::common::UI_MP_TASK_PAUSED:
+
+				ui.mp_start_pushButton->setDisabled(true);
+				ui.mp_stop_pushButton->setDisabled(false);
+				ui.mp_pause_pushButton->setDisabled(true);
+				ui.mp_resume_pushButton->setDisabled(false);
+				ui.mp_trigger_pushButton->setDisabled(true);
+
+				block_all_ecp_trigger_widgets();
+				break;
+			default:
+				break;
+		}
+
+		interface.mp.last_state = interface.mp.state;
+
+	}
+
 	return 1;
 
 }
@@ -201,9 +196,10 @@ int wgt_process_control::block_all_ecp_trigger_widgets()
 	 if (interface.conveyor->state.edp.is_synchronised) {
 	 interface.block_widget(ABW_PtButton_wnd_processes_control_conveyor_ecp_trigger);
 	 }
-
-	 interface.block_widget(ABW_PtButton_wnd_processes_control_all_ecp_trigger);
 	 */
+
+	ui.all_ecp_trigger_pushButton->setDisabled(true);
+
 	return 1;
 }
 
@@ -222,8 +218,8 @@ int wgt_process_control::unblock_all_ecp_trigger_widgets()
 	 if (interface.conveyor->state.edp.is_synchronised) {
 	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_conveyor_ecp_trigger);
 	 }
-
-	 interface.unblock_widget(ABW_PtButton_wnd_processes_control_all_ecp_trigger);
 	 */
+
+	ui.all_ecp_trigger_pushButton->setDisabled(false);
 	return 1;
 }
