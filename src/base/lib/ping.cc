b653bd042d5c870e3e6df5e18985b7063c88aa20
Tomasz Winiarski <tomrobotics@gmail.com>
2011-05-04 20:20:59 +0200

    fix

--- src/base/lib/ping.cc
+++ src/base/lib/ping.cc
@@ -11,7 +11,7 @@ bool ping(const std::string & target)
 	std::string cmd;
 
 	cmd = "ping ";
-	cmd += " -c 1 -W 2 ";
+	cmd += " -c 1 -W 1 ";
 	cmd += target;
 	//cmd += " 1>/dev/null 2>/dev/null";
 
