4df68122d88ce508831a154d5aea68b0030e542e
Tomasz Winiarski <yoyek@ubuntu.(none)>
2011-05-13 01:42:14 -0700

    simplifications (ecp_sections)

--- src/robot/bird_hand/mp_r_bird_hand.cc
+++ src/robot/bird_hand/mp_r_bird_hand.cc
@@ -13,7 +13,7 @@ namespace mp {
 namespace robot {
 
 bird_hand::bird_hand(task::task &mp_object_l) :
-	robot(lib::bird_hand::ROBOT_NAME, lib::bird_hand::ECP_SECTION, mp_object_l, lib::bird_hand::NUM_OF_SERVOS)
+	robot(lib::bird_hand::ROBOT_NAME, mp_object_l, lib::bird_hand::NUM_OF_SERVOS)
 {
 }
 
