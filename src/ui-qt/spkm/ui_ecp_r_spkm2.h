8c08058dfe0f6812a5df3e55f4c3c0d417a4eb05
Tomasz Winiarski <tomrobotics@gmail.com>
2011-06-17 11:41:10 +0200

    ui spkm

--- src/ui-qt/spkm/ui_ecp_r_spkm2.h
+++ src/ui-qt/spkm/ui_ecp_r_spkm2.h
@@ -5,21 +5,21 @@
 // Ostatnia modyfikacja: 16.04.98
 // -------------------------------------------------------------------------
 
-#ifndef _UI_ECP_R_SPKM1_H
-#define _UI_ECP_R_SPKM1_H
+#ifndef _UI_ECP_R_SPKM2_H
+#define _UI_ECP_R_SPKM2_H
 
 #include "../base/ui.h"
 // Konfigurator.
 #include "base/lib/configurator.h"
 #include "base/lib/mrmath/mrmath.h"
 #include "base/ecp/ecp_robot.h"
-#include "robot/spkm/ecp_r_spkm1.h"
+#include "robot/spkm/ecp_r_spkm2.h"
 #include "../base/ui_ecp_robot/ui_ecp_r_data_port.h"
 #include "ui_ecp_r_spkm.h"
 
 namespace mrrocpp {
 namespace ui {
-namespace spkm1 {
+namespace spkm2 {
 
 // ---------------------------------------------------------------
 class EcpRobot : public spkm::EcpRobot
