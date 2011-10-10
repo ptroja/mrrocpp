536425fecc9fe36cfef18fa957f5495368aeafa6
Tomasz Winiarski <tomrobotics@gmail.com>
2011-10-10 17:38:12 +0200

    fix

--- src/robot/smb/festo_and_inputs.cc
+++ src/robot/smb/festo_and_inputs.cc
@@ -24,7 +24,26 @@ festo_and_inputs::festo_and_inputs(effector &_master, boost::shared_ptr <maxon::
 		festo::cpv> _cpv10) :
 		master(_master), epos_di_node(_epos_di_node), cpv10(_cpv10)
 {
-	read_state();
+	if (!(master.robot_test_mode)) {
+		festo::U32 DeviceType = cpv10->readDeviceType();
+		printf("Device type = 0x%08X\n", DeviceType);
+
+		festo::U8 ErrorRegister = cpv10->readErrorRegister();
+		printf("Error register = 0x%02X\n", ErrorRegister);
+
+		festo::U32 ManufacturerStatusRegister = cpv10->readManufacturerStatusRegister();
+		printf("Manufacturer status register = 0x%08X\n", ManufacturerStatusRegister);
+
+		uint8_t NumberOfOutputGroups = cpv10->readNumberOf8OutputGroups();
+		printf("Number of 8-output groups = %d\n", NumberOfOutputGroups);
+
+		uint8_t Outputs07 = cpv10->readOutputs(1);
+		printf("Status of outputs 0..7 = 0x%02x\n", Outputs07);
+
+		master.gateway->SendNMTService(10, canopen::gateway::Start_Remote_Node);
+
+		read_state();
+	}
 }
 
 festo_and_inputs::~festo_and_inputs()
@@ -70,14 +89,16 @@ void festo_and_inputs::set_clean(int leg_number, bool value)
 
 void festo_and_inputs::read_state()
 {
-	epos_inputs = epos_di_node->readDInput();
+	if (!(master.robot_test_mode)) {
+		epos_inputs = epos_di_node->readDInput();
 
-	group_one_current_output = cpv10->readOutputs(1);
-	group_one_desired_output = group_one_current_output;
-	std::cout << "group_one_desired_output 1= " << group_one_desired_output << std::endl;
-	group_two_current_output = cpv10->readOutputs(2);
-	std::cout << "group_two_current_output 1= " << group_two_current_output << std::endl;
-	group_two_desired_output = group_two_current_output;
+		group_one_current_output = cpv10->readOutputs(1);
+		group_one_desired_output = group_one_current_output;
+		std::cout << "group_one_desired_output 1= " << group_one_desired_output << std::endl;
+		group_two_current_output = cpv10->readOutputs(2);
+		std::cout << "group_two_current_output 1= " << group_two_current_output << std::endl;
+		group_two_desired_output = group_two_current_output;
+	}
 }
 
 void festo_and_inputs::execute_command()
