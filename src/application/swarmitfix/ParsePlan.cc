c590782ab150754de2a663fd6ccf34cd98188918
Piotr Trojanek <piotr.trojanek@gmail.com>
2011-11-29 17:47:06 +0100

    plan: parsing of Matlab-like matrices

--- src/application/swarmitfix/ParsePlan.cc
+++ src/application/swarmitfix/ParsePlan.cc
@@ -11,6 +11,7 @@
 #include <boost/thread/thread.hpp>
 
 #include "planner.h"
+#include "base/lib/mrmath/homog_matrix.h"
 
 using namespace std;
 
@@ -23,11 +24,32 @@ int main(int argc, char *argv[])
 	}
 
 	try {
+		const Plan p = *plan(argv[1], xml_schema::Flags::dont_validate);
+
+		cerr << p.hNum() << endl;
+
+		for(Plan::PkmType::ItemConstIterator it = p.pkm().item().begin();
+				it != p.pkm().item().end();
+				++it) {
+
+			const Plan::PkmType::ItemType & pkmCmd = *it;
+
+			// Test only 1st agent
+			if(pkmCmd.agent() != 1)
+				continue;
+
+			mrrocpp::lib::Homog_matrix hm(pkmCmd.pkmToWrist());
+
+			std::cerr << pkmCmd.pkmToWrist() << std::endl;
+			std::cerr << hm << std::endl << std::endl;
+			//std::cerr << "[" << pkmCmd.l1() << "," << pkmCmd.l2() << "," << pkmCmd.l3() << "]" << std::endl;
+		}
+
 		// Create planner object
-		planner pp(argv[1]);
+		//planner pp(argv[1]);
 
 		// Start execution
-		pp.start();
+		//pp.start();
 
 	} catch (const xml_schema::Exception& e) {
 		cerr << e << endl;
--- src/application/swarmitfix/plan/ParsePlan.cc
+++ src/application/swarmitfix/plan/ParsePlan.cc
@@ -10,6 +10,8 @@
 
 #include "plan.hxx"
 
+#include "base/lib/mrmath/homog_matrix.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -22,6 +24,23 @@ int main(int argc, char *argv[])
 		const Plan p = *plan(argv[1], xml_schema::Flags::dont_validate);
 
 		cerr << p.hNum() << endl;
+
+		for(Plan::PkmType::ItemConstIterator it = p.pkm().item().begin();
+				it != p.pkm().item().end();
+				++it) {
+
+			const Plan::PkmType::ItemType & pkmCmd = *it;
+
+			// Test only 1st agent
+			if(pkmCmd.agent() != 1)
+				continue;
+
+			// mrrocpp::lib::Homog_matrix hm(pkmCmd.pkmToWrist());
+			std::cerr << pkmCmd.pkmToWrist() << std::endl;
+
+			// std::cerr << hm << std::endl;
+			//std::cerr << "[" << pkmCmd.l1() << "," << pkmCmd.l2() << "," << pkmCmd.l3() << "]" << std::endl;
+		}
 	} catch (const xml_schema::Exception& e) {
 		cerr << e << endl;
 		return 1;
