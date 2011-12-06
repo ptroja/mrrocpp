fcaefe5811f89589ac5426991650b4cfb7a763cd
Tomasz Winiarski <tomrobotics@gmail.com>
2011-12-06 14:40:32 +0100

    smb up changed to in, down changed to out

--- src/ui-qt/smb/wgt_smb_command.h
+++ src/ui-qt/smb/wgt_smb_command.h
@@ -32,16 +32,16 @@ public:
 			0);
 	~wgt_smb_command();
 
-	QVector <QCheckBox*> checkBox_fl_up_Vector;
-	QVector <QCheckBox*> checkBox_fl_down_Vector;
+	QVector <QCheckBox*> checkBox_fl_in_Vector;
+	QVector <QCheckBox*> checkBox_fl_out_Vector;
 	QVector <QCheckBox*> checkBox_fl_attached_Vector;
 	QVector <QCheckBox*> checkBox_fl_undetachable_Vector;
 
 	QVector <QCheckBox*> checkBox_m_mip_Vector;
 	QVector <QCheckBox*> checkBox_m_no_Vector;
 
-	QVector <QRadioButton*> radioButton_fl_up_Vector;
-	QVector <QRadioButton*> radioButton_fl_down_Vector;
+	QVector <QRadioButton*> radioButton_fl_in_Vector;
+	QVector <QRadioButton*> radioButton_fl_out_Vector;
 
 	QVector <QDoubleSpinBox*> doubleSpinBox_m_current_position_Vector;
 	QVector <QDoubleSpinBox*> doubleSpinBox_m_absolute_Vector;
