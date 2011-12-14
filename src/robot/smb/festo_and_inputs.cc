75af681399ea67a3c82956e7cbb4c6092d968c89
Tomasz Winiarski <tomrobotics@gmail.com>
2011-12-14 15:35:41 +0100

    smb legs state fix

--- src/robot/smb/festo_and_inputs.cc
+++ src/robot/smb/festo_and_inputs.cc
@@ -430,7 +430,7 @@ void festo_and_inputs::command_all_out()
 	switch (current_legs_state)
 	{
 		case lib::smb::ALL_OUT: {
-			BOOST_THROW_EXCEPTION(mrrocpp::edp::smb::nfe_invalid_command_in_given_state() << current_state(current_legs_state) << retrieved_festo_command(lib::smb::ALL_OUT));
+			//	BOOST_THROW_EXCEPTION(mrrocpp::edp::smb::nfe_invalid_command_in_given_state() << current_state(current_legs_state) << retrieved_festo_command(lib::smb::ALL_OUT));
 		}
 			break;
 		case lib::smb::ONE_IN_TWO_OUT: {
@@ -650,8 +650,11 @@ void festo_and_inputs::command_two_in_one_out()
 
 			break;
 		case lib::smb::ONE_IN_TWO_OUT:
-		case lib::smb::TWO_IN_ONE_OUT:
 		case lib::smb::ALL_IN: {
+			BOOST_THROW_EXCEPTION(mrrocpp::edp::smb::nfe_invalid_command_in_given_state()<<current_state(current_legs_state) << retrieved_festo_command(lib::smb::TWO_IN_ONE_OUT));
+		}
+			break;
+		case lib::smb::TWO_IN_ONE_OUT: {
 			BOOST_THROW_EXCEPTION(mrrocpp::edp::smb::nfe_invalid_command_in_given_state()<<current_state(current_legs_state) << retrieved_festo_command(lib::smb::TWO_IN_ONE_OUT));
 		}
 			break;
@@ -712,9 +715,12 @@ void festo_and_inputs::command_all_in()
 
 			break;
 		case lib::smb::ONE_IN_TWO_OUT:
-		case lib::smb::TWO_IN_ONE_OUT:
-		case lib::smb::ALL_IN:
+		case lib::smb::TWO_IN_ONE_OUT: {
 			BOOST_THROW_EXCEPTION(mrrocpp::edp::smb::nfe_invalid_command_in_given_state()<<current_state(current_legs_state) << retrieved_festo_command(lib::smb::ALL_IN));
+		}
+			break;
+		case lib::smb::ALL_IN: {
+		}
 			break;
 		default:
 			break;
