ed622ada4c6a41e519b7446a8aa2b2e7a0c0af41
Tomasz Winiarski <tomrobotics@gmail.com>
2011-12-29 17:19:20 +0100

    ui swarm window added tests needed

--- src/ui-qt/base/ui_ecp_dialogs/wgt_swarm.cpp
+++ src/ui-qt/base/ui_ecp_dialogs/wgt_swarm.cpp
@@ -26,25 +26,52 @@ void wgt_swarm::hideEvent(QHideEvent *event)
 	event->accept();
 }
 
-void wgt_swarm::on_pushButton_yes_clicked()
-{
-	interface.ui_ecp_obj->ui_rep.reply = lib::ANSWER_YES;
-	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
-
-	my_close();
-}
-
-void wgt_swarm::on_pushButton_no_clicked()
-{
-	interface.ui_ecp_obj->ui_rep.reply = lib::ANSWER_NO;
-	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
-
-	my_close();
-}
-
 void wgt_swarm::my_open(bool set_on_top)
 {
 	ui->label_message->setText(interface.ui_ecp_obj->ecp_to_ui_msg.string);
+
+	strcpy(stored_plan_item, interface.ui_ecp_obj->ecp_to_ui_msg.plan_item);
+
+	ui->textEdit->setText(interface.ui_ecp_obj->ecp_to_ui_msg.plan_item);
+
 	wgt_base::my_open(set_on_top);
+}
+
+void wgt_swarm::on_pushButton_prev_clicked()
+{
+	interface.ui_ecp_obj->ui_rep.reply = lib::PLAN_PREV;
+	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
+
+	my_close();
+}
+
+void wgt_swarm::on_pushButton_next_clicked()
+{
+	interface.ui_ecp_obj->ui_rep.reply = lib::PLAN_NEXT;
+	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
+
+	my_close();
+}
+
+void wgt_swarm::on_pushButton_exec_clicked()
+{
+	interface.ui_ecp_obj->ui_rep.reply = lib::PLAN_EXEC;
+
+	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
+	strcpy(interface.ui_ecp_obj->ui_rep.plan_item, ui->textEdit->toPlainText().toStdString().c_str());
+	my_close();
+}
+
+void wgt_swarm::on_pushButton_save_clicked()
+{
+	interface.ui_ecp_obj->ui_rep.reply = lib::PLAN_SAVE;
+	interface.ui_ecp_obj->communication_state = ui::common::UI_ECP_REPLY_READY;
+
+	my_close();
+}
+
+void wgt_swarm::on_pushButton_reload_clicked()
+{
+	ui->textEdit->setText(stored_plan_item);
 }
 
