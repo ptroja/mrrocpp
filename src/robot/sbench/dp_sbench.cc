8d758717a89cfdb08df39531c586054cb6afbb84
yoyek <yoyek@ubuntu>
2012-01-03 23:37:11 +0100

    dp_sbench improovment

--- src/robot/sbench/dp_sbench.cc
+++ src/robot/sbench/dp_sbench.cc
@@ -16,12 +16,26 @@ namespace mrrocpp {
 namespace lib {
 namespace sbench {
 
+// translation table of bench docks to io card ports
 static int translation_table[8][8] =
 		{ { 0, 1, 2, 3, 4, 5, 6, 7 }, { 8, 9, 10, 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20, 21, 22, 23 }, { 24, 25, 26, 27, 28, 29, 30, 31 }, { 32, 33, 34, 35, 36, 37, 38, 39 }, { 40, 41, 42, 43, 44, 45, 46, 47 }, { 48, 49, 50, 51, 52, 53, 54, 55 }, { 56, 57, 58, 59, 60, 61, 62, 63 } };
 
 pins_buffer::pins_buffer()
 {
-	// dodac sprawdzenie czy ktorys indeks sie nie dubluje
+// doubled value check
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+
+			for (int k = 0; k < 8; k++) {
+				for (int g = 0; g < 8; g++) {
+					if ((!((k == i) && (j == g))) && (translation_table[k][g] == translation_table[i][j])) {
+						std::cout << "multiply value at translation_table dp_sbench\n\n\n";
+					}
+				}
+			}
+
+		}
+	}
 
 }
 
