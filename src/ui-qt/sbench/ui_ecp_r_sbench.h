19925ad38680697212036f36aedad4fc0fc76c79
yoyek <yoyek@roma>
2012-01-12 11:04:05 +0100

    sbench kinematics removed

--- src/ui-qt/sbench/ui_ecp_r_sbench.h
+++ src/ui-qt/sbench/ui_ecp_r_sbench.h
@@ -1,15 +1,8 @@
-// -------------------------------------------------------------------------
-//                            robot.h
-// Definicje struktur danych i metod do komunikacji UI z EDP
-//
-// Ostatnia modyfikacja: 16.04.98
-// -------------------------------------------------------------------------
-
 #ifndef _UI_ECP_R_SMB_H
 #define _UI_ECP_R_SMB_H
 
 #include "../base/ui.h"
-// Konfigurator.
+
 #include "base/lib/configurator.h"
 #include "base/lib/mrmath/mrmath.h"
 #include "base/ecp/ecp_robot.h"
