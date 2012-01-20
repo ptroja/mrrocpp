1264b364ad9538c5b84f56c4c1b6b683a38cf2c5
Piotr Trojanek <piotr.trojanek@gmail.com>
2012-01-20 18:07:31 +0100

    swarm: plan executor code formatting

--- src/application/swarmitfix/mp_t_swarmitfix.h
+++ src/application/swarmitfix/mp_t_swarmitfix.h
@@ -30,16 +30,19 @@ namespace task {
  *  @{
  */
 
-typedef struct _IO {
+typedef struct _IO
+{
 
 	//! Composite data type for inter-agent communication subsystem 
-	typedef struct _transmitters {
+	typedef struct _transmitters
+	{
 
 		//! Common input buffers
-		typedef struct _inputs {
-	
+		typedef struct _inputs
+		{
+
 			//! Data type of input buffer
-			typedef InputPtr<lib::notification_t> notification_t;
+			typedef InputPtr <lib::notification_t> notification_t;
 
 			//! InputBuffer: Status of the agent
 			notification_t notification;
@@ -47,53 +50,59 @@ typedef struct _IO {
 		} inputs_t;
 
 		//! Composite data type for transmitter's data buffers  
-		typedef struct _spkm {
-		
+		typedef struct _spkm
+		{
+
 			//! Input buffers
 			inputs_t inputs;
-			
+
 			//! Output buffers
-			struct _outputs {
+			struct _outputs
+			{
 				//! Data type of output buffer
-				typedef OutputPtr<lib::spkm::next_state_t> command_t;
+				typedef OutputPtr <lib::spkm::next_state_t> command_t;
 
 				//! OutputBuffer: Command to execute
 				command_t command;
-			
+
 			} outputs;
-			
+
 		} spkm_t;
-		
+
 		//! Transmitter's data structure itself
-		
+
 		//! Composite data type for transmitter's data buffers  
-		typedef struct _smb {
-		
+		typedef struct _smb
+		{
+
 			//! Input buffers
 			inputs_t inputs;
-			
+
 			//! Output buffers
-			struct _outputs {
+			struct _outputs
+			{
 				//! Data type of output buffer
-				typedef OutputPtr<lib::smb::next_state_t> command_t;
+				typedef OutputPtr <lib::smb::next_state_t> command_t;
 
 				//! OutputBuffer: Command to execute
 				command_t command;
 
 			} outputs;
-			
+
 		} smb_t;
-		
+
 		//! Composite data type for transmitter's data buffers
-		typedef struct _shead {
+		typedef struct _shead
+		{
 
 			//! Input buffers
 			inputs_t inputs;
 
 			//! Output buffers
-			struct _outputs {
+			struct _outputs
+			{
 				//! Data type of output buffer
-				typedef OutputPtr<lib::shead::next_state> command_t;
+				typedef OutputPtr <lib::shead::next_state> command_t;
 
 				//! OutputBuffer: Command to execute
 				command_t command;
@@ -101,17 +110,19 @@ typedef struct _IO {
 			} outputs;
 
 		} shead_t;
-		
+
 		//! Composite data type for transmitter's data buffers
-		typedef struct _sbench {
+		typedef struct _sbench
+		{
 
 			//! Input buffers
 			inputs_t inputs;
 
 			//! Output buffers
-			struct _outputs {
+			struct _outputs
+			{
 				//! Data type of output buffer
-				typedef OutputPtr<lib::sbench::cbuffer> command_t;
+				typedef OutputPtr <lib::sbench::cbuffer> command_t;
 
 				//! OutputBuffer: Command to execute
 				command_t command;
@@ -119,7 +130,7 @@ typedef struct _IO {
 			} outputs;
 
 		} sbench_t;
-		
+
 		//! Transmitter's data structures itself
 		smb_t smb1, smb2;
 		spkm_t spkm1, spkm2;
@@ -127,39 +138,42 @@ typedef struct _IO {
 		sbench_t sbench;
 
 	} transmitters_t;
-	
+
 	//! Inter-agent communication subsystem itself
 	transmitters_t transmitters;
-	
+
 	//! Composite data type for sensory subsystem 
-	typedef struct _sensors {
-	
+	typedef struct _sensors
+	{
+
 		//! Composite data type for transmitter's data buffers  
-		typedef struct _planner {
-		
+		typedef struct _planner
+		{
+
 			//! Input buffers
-			struct _inputs { 
-		
+			struct _inputs
+			{
+
 				//! Data type of input buffer 
-				typedef InputPtr<lib::empty_t> trigger_t;
-	
+				typedef InputPtr <lib::empty_t> trigger_t;
+
 				//! InputBuffer: Trigger execution of the next plan step
 				trigger_t trigger;
-				
+
 			} inputs;
-			
+
 			//! Output buffers
-			struct _outputs {
+			struct _outputs
+			{
 			} outputs;
-			
+
 		} planner_t;
-		
+
 		//! Transmitter's data structure itself
 		planner_t planner;
-		
 
 	} sensors_t;
-	
+
 	//! Sensory subsystem itself
 	sensors_t sensors;
 } IO_t;
@@ -169,48 +183,51 @@ class swarmitfix : public task
 private:
 	// Start of user code Internal memory variables
 //! Type for plan realization status
-typedef enum _PLAN_STATUS { ONGOING, FAILURE } PlanStatus;
+	typedef enum _PLAN_STATUS
+	{
+		ONGOING, FAILURE
+	} PlanStatus;
 
 //! Planner object
-planner pp;
+	planner pp;
 
 // End of user code
-	
-	//! Internal memory variable: Current plan status	
+
+//! Internal memory variable: Current plan status
 	PlanStatus current_plan_status;
 	//! Internal memory variable: Current workers status	
 	WorkersStatus current_workers_status;
 
 	//! Input/Output subsystems
 	IO_t IO;
-	
+
 	//! Initial condition: Execute if the status in non-failure	  
 	bool b1_initial_condition(void) const;
-	
+
 	//! Terminal condition: Terminate in case of failure	  
 	bool b1_terminal_condition(void) const;
-	
+
 	//! Transition function: Progress with the next plan command
 	void b1_plan_progress(void);
-	
+
 	//! Transition function: Record notification message
 	void b1_handle_spkm1_notification(void);
-	
+
 	//! Transition function: Record notification message
 	void b1_handle_spkm2_notification(void);
-	
+
 	//! Transition function: Record notification message
 	void b1_handle_smb1_notification(void);
-	
+
 	//! Transition function: Record notification message
 	void b1_handle_smb2_notification(void);
-	
+
 	//! Initial condition: Execute in case of emergency	  
 	bool b2_initial_condition(void) const;
-	
+
 	//! Terminal condition: Do not terminate (requires operator to restart the task)	  
 	bool b2_terminal_condition(void) const;
-	
+
 	//! Transition function: Send stop commands to all the busy robots
 	void b2_stop_all(void);
 
@@ -236,12 +253,13 @@ planner pp;
 	lib::UI_TO_ECP_REPLY step_mode(Mbase::ItemType & item);
 
 	//! Access to plan items at given index
-	template<typename T>
+	template <typename T>
 	typename T::iterator StateAtInd(int ind, T & items)
 	{
 		typename T::iterator it = items.begin();
 
-		while((it != items.end()) && it->ind() != ind) ++it;
+		while ((it != items.end()) && it->ind() != ind)
+			++it;
 
 		return it;
 	}
@@ -257,9 +275,8 @@ public:
 	void main_task_algorithm(void);
 };
 
-/** @} */// end of swarmitfix
-
-} // namespace task
+/** @} */ // end of swarmitfix
+}// namespace task
 } // namespace mp
 } // namespace mrrocpp
 
