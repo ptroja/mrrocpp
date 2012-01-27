f41a7674fca47fb81b47d1f90074f7e13520d769
Tomasz Winiarski <tomrobotics@gmail.com>
2012-01-27 13:00:47 +0100

    hi_rydz removal in progress

--- src/robot/irp6p_tfg/sg_irp6p_tfg.cc
+++ src/robot/irp6p_tfg/sg_irp6p_tfg.cc
@@ -26,10 +26,9 @@ namespace irp6p_tfg {
 
 // extern uint64_t kk;				  // numer pomiaru od momentu startu pomiarow
 
-
 /*-----------------------------------------------------------------------*/
 servo_buffer::servo_buffer(effector &_master) :
-	common::servo_buffer(_master), master(_master)
+		common::servo_buffer(_master), master(_master)
 {
 	synchro_axis_order[0] = 0;
 
@@ -47,11 +46,10 @@ void servo_buffer::load_hardware_interface(void)
 	// tablica pradow maksymalnych dla poszczegolnych osi
 	//int max_current[lib::irp6p_tfg::NUM_OF_SERVOS] = { AXIS_7_MAX_CURRENT };
 
-	const std::vector <std::string>
-			ports_vector(mrrocpp::lib::irp6p_tfg::ports_strings, mrrocpp::lib::irp6p_tfg::ports_strings
-					+ mrrocpp::lib::irp6p_tfg::LAST_MOXA_PORT_NUM + 1);
-	hi
-			= new hi_moxa::HI_moxa(master, mrrocpp::lib::irp6p_tfg::LAST_MOXA_PORT_NUM, ports_vector, mrrocpp::lib::irp6p_tfg::MAX_INCREMENT);
+	const std::vector <std::string> ports_vector(mrrocpp::lib::irp6p_tfg::ports_strings, mrrocpp::lib::irp6p_tfg::ports_strings
+			+ mrrocpp::lib::irp6p_tfg::LAST_MOXA_PORT_NUM + 1);
+	hi =
+			new hi_moxa::HI_moxa(master, mrrocpp::lib::irp6p_tfg::LAST_MOXA_PORT_NUM, ports_vector, mrrocpp::lib::irp6p_tfg::MAX_INCREMENT);
 	hi->init();
 
 	//Ustawienie zwlocznego ograniczenia pradowego - dlugotrwale przekroczenie ustawionej wartosci
