96cf8668bcc4ad338cbdf3b90fc1d801d934164d
Szymon Piątek <spiatheck@gmail.com>
2012-02-09 15:45:23 +0100

    register subtask and generators in ecp modifications

--- src/application/block_move/ecp_t_block_move.cc
+++ src/application/block_move/ecp_t_block_move.cc
@@ -63,15 +63,15 @@ common::task::task(_config)
 
 	// utworzenie generatorow do uruchamiania dispatcherem
 	//generator_m[ecp_mp::generator::ECP_GEN_BIAS_EDP_FORCE] = new generator::bias_edp_force(*this);
-	register_generator(new common::generator::bias_edp_force(*this));
+	register_sg(new common::generator::bias_edp_force(*this));
 
 	// utworzenie podzadan
 	//subtask_m[ecp_mp::sub_task::ECP_ST_SMOOTH_JOINT_FILE_FROM_MP] =
 	//new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_JOINT, true);
 	//subtask_m[ecp_mp::sub_task::ECP_ST_SMOOTH_ANGLE_AXIS_FILE_FROM_MP] =
 	//new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_XYZ_ANGLE_AXIS, true);
-	register_subtask(new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_JOINT, ecp_mp::sub_task::ECP_ST_SMOOTH_JOINT_FILE_FROM_MP, true));
-	register_subtask(new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_XYZ_ANGLE_AXIS, ecp_mp::sub_task::ECP_ST_SMOOTH_ANGLE_AXIS_FILE_FROM_MP, true));
+	register_sg(new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_JOINT, ecp_mp::sub_task::ECP_ST_SMOOTH_JOINT_FILE_FROM_MP, true));
+	register_sg(new sub_task::sub_task_smooth_file_from_mp(*this, lib::ECP_XYZ_ANGLE_AXIS, ecp_mp::sub_task::ECP_ST_SMOOTH_ANGLE_AXIS_FILE_FROM_MP, true));
 
 	//sensor rpc
 	sr_ecp_msg->message("Creating discode sensor...");
