6fd17a947e29b7b2c91e27343a9711eada1ac44d
yoyek <yoyek@ubuntu>
2012-02-11 00:34:32 +0100

    ecp generators refactor

---
+++ src/generator/ecp/ecp_g_common.h
@@ -0,0 +1,25 @@
+// -------------------------------------------------------------------------
+//                            generator/ecp_g_jarosz.h dla QNX6
+//
+// Modyfikacje:
+// 1. metody wirtualne w kla sie bazowej sensor - ok. 160
+// 2. bonusy do testowania
+//
+// Ostatnia modyfikacja: 25.06.2003
+// autor modyfikacji: tkornuta
+// -------------------------------------------------------------------------
+
+#if !defined(_ECP_GEN_COMMON_H)
+#define _ECP_GEN_COMMON_H
+
+namespace mrrocpp {
+namespace ecp {
+namespace common {
+namespace generator {
+
+} // namespace generator
+} // namespace common
+} // namespace ecp
+} // namespace mrrocpp
+
+#endif
